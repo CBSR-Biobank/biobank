70b422001c44954c4d9b2728cbe1e2a00c39e256
Nelson Loyola <loyola@cs.ualberta.ca>
2009-01-16 16:57:23 -0700

    working on dialog box layout

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/dialogs/SiteDialog.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/dialogs/SiteDialog.java
@@ -13,6 +13,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -20,7 +21,7 @@ import org.eclipse.swt.widgets.Text;
 public class SiteDialog extends TitleAreaDialog {
 	private static final HashMap<String, List<String>> groups = 
 		new HashMap<String, List<String>>() {{
-		put("root", Arrays.asList("Site Name"));
+		put("Site", Arrays.asList("Name"));
 		put("Address", Arrays.asList(
 				"Street 1", "Street 2", "City", "Province", "Telephone",
 				"Fax Number", "Email Address"));
@@ -53,31 +54,27 @@ public class SiteDialog extends TitleAreaDialog {
         Composite contents = new Composite(parentComposite, SWT.NONE);
 		GridData gd = new GridData(GridData.CENTER, GridData.CENTER, true, true);
 		contents.setLayoutData(gd);
-		GridLayout layout = new GridLayout(2, true);
+		GridLayout layout = new GridLayout(1, true);
         layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
         layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
         layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
         layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
 		contents.setLayout(layout);
 		contents.setFont(parentComposite.getFont());
-
 		
 		Iterator<String> it = groups.keySet().iterator();
 		while (it.hasNext()) {
 			String gName = it.next();
 			
-			if (!gName.equals("root")) {
-				Label gLabel = new Label(contents, SWT.NONE);
-				gLabel.setText(gName);
-				GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-				gridData.horizontalSpan = 2;
-				gLabel.setLayoutData(gridData);
-			}
+			Group group = new Group(contents, SWT.NONE);
+			group.setText(gName);
+			GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+			group.setLayoutData(gridData);
 			
 			for (String f : groups.get(gName)) {
-				new Label(contents, SWT.NONE).setText(f + ":");
+				new Label(group, SWT.NONE).setText(f + ":");
 				
-				Text text = new Text(contents, SWT.BORDER | SWT.SINGLE);
+				Text text = new Text(group, SWT.BORDER | SWT.SINGLE);
 				GridData textGridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
 				textGridData.widthHint = 250;
 				text.setLayoutData(textGridData);
