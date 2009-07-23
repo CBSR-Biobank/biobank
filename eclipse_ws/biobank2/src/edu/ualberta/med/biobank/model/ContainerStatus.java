829f2cfa31227e5155c52b58ffe452b32be1d93a
Aaron Young <aaron@acme.(none)>
2009-07-23 13:03:23 -0600

    Container visualizer complete

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/model/ContainerStatus.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/model/ContainerStatus.java
@@ -6,30 +6,28 @@ import org.eclipse.swt.graphics.Color;
 import edu.ualberta.med.biobank.BioBankPlugin;
 
 public enum ContainerStatus {
-	EMPTY(SWT.COLOR_WHITE, "Empty"), FILLED(SWT.COLOR_DARK_GRAY, "Filled"), FREE_POSITIONS(
-			85, 160, 65, "Free positions"), NOT_INITIALIZED(196, 149, 47,
-			"Not initialized");
+    EMPTY(SWT.COLOR_WHITE, "Empty"), FILLED(SWT.COLOR_DARK_GRAY, "Filled");
 
-	private Color color;
-	private String legend;
+    private Color color;
+    private String legend;
 
-	private ContainerStatus(int color, String legend) {
-		this.color = BioBankPlugin.getDefault().getWorkbench().getDisplay()
-				.getSystemColor(color);
-		this.legend = legend;
-	}
+    private ContainerStatus(int color, String legend) {
+        this.color = BioBankPlugin.getDefault().getWorkbench().getDisplay()
+            .getSystemColor(color);
+        this.legend = legend;
+    }
 
-	private ContainerStatus(int red, int green, int blue, String legend) {
-		this.color = new Color(BioBankPlugin.getDefault().getWorkbench()
-				.getDisplay(), red, green, blue);
-		this.legend = legend;
-	}
+    private ContainerStatus(int red, int green, int blue, String legend) {
+        this.color = new Color(BioBankPlugin.getDefault().getWorkbench()
+            .getDisplay(), red, green, blue);
+        this.legend = legend;
+    }
 
-	public Color getColor() {
-		return color;
-	}
+    public Color getColor() {
+        return color;
+    }
 
-	public String getLegend() {
-		return legend;
-	}
+    public String getLegend() {
+        return legend;
+    }
 }
