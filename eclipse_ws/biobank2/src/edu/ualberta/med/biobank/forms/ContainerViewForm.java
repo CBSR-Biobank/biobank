6e878e27c596eb6bbd28637701762337ffd00a7b
Aaron Young <aaron@acme.(none)>
2009-07-31 11:12:40 -0600

    visualization interface now closes parent form

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/ContainerViewForm.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/ContainerViewForm.java
@@ -228,7 +228,7 @@ public class ContainerViewForm extends BiobankViewForm {
 
     private void openFormFor(ContainerPosition pos) {
         ContainerAdapter newAdapter = null;
-
+        ContainerAdapter.closeEditor(new FormInput(containerAdapter));
         if (cells[pos.getPositionDimensionOne()][pos.getPositionDimensionTwo()]
             .getStatus() == ContainerStatus.EMPTY) {
             Container newContainer = new Container();
@@ -255,7 +255,7 @@ public class ContainerViewForm extends BiobankViewForm {
             Assert.isNotNull(newAdapter);
             Node.openForm(new FormInput(newAdapter), ContainerViewForm.ID);
         }
-        ContainerAdapter.closeEditor(new FormInput(containerAdapter));
+
         containerAdapter.performExpand();
     }
 
