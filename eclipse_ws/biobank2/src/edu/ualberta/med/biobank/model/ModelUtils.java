9a13d6a9189c9f76187be15fffe9be42ed6782bf
degrisda <degrisda@cs.ualberta.ca>
2009-08-04 15:14:06 -0600

    ModelUtil.getObjectWithAttr corrected (see issue #85)

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/model/ModelUtils.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/model/ModelUtils.java
@@ -39,16 +39,16 @@ public class ModelUtils {
         return list.get(0);
     }
 
-    public static Object getObjectWithAttr(
-        WritableApplicationService appService, Class<?> classType, String attr,
-        Object value) throws Exception {
+    public static <E> E getObjectWithAttr(
+        WritableApplicationService appService, Class<E> classType, String attr,
+        Class<?> attrType, Object value) throws Exception {
         Constructor<?> constructor = classType.getConstructor();
         Object instance = constructor.newInstance();
         attr = "set" + attr.substring(0, 1).toUpperCase() + attr.substring(1);
-        Method setIdMethod = classType.getMethod(attr, Integer.class);
-        setIdMethod.invoke(instance, value);
+        Method setMethod = classType.getMethod(attr, attrType);
+        setMethod.invoke(instance, value);
 
-        List<?> list = appService.search(classType, instance);
+        List<E> list = appService.search(classType, instance);
         Assert.isTrue(list.size() == 1);
         return list.get(0);
     }
