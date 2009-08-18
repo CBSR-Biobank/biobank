ed3bdd499225bdfac11e06aff96b51cb791f7fc9
degrisda <degrisda@cs.ualberta.ca>
2009-08-18 12:15:53 -0600

    Create the eclipse plugin project

---
+++ src/edu/ualberta/med/sampleProcessingRobot/SampleProcessingRobotPlugin.java
@@ -0,0 +1,55 @@
+package edu.ualberta.med.sampleProcessingRobot;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class SampleProcessingRobotPlugin extends AbstractUIPlugin {
+
+    // The plug-in ID
+    public static final String PLUGIN_ID = "sampleProcessingRobot";
+
+    // The shared instance
+    private static SampleProcessingRobotPlugin plugin;
+
+    /**
+     * The constructor
+     */
+    public SampleProcessingRobotPlugin() {
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static SampleProcessingRobotPlugin getDefault() {
+        return plugin;
+    }
+
+    /**
+     * Returns an image descriptor for the image file at the given plug-in
+     * relative path
+     * 
+     * @param path the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
+}
---
+++ src/edu/ualberta/med/sampleProcessingRobot/perspectives/MainPerspective.java
@@ -0,0 +1,15 @@
+package edu.ualberta.med.sampleProcessingRobot.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class MainPerspective implements IPerspectiveFactory {
+
+    public static final String ID = "edu.ualberta.med.sampleProcessingRobot.perspectives.MainPerspective";
+
+    @Override
+    public void createInitialLayout(IPageLayout layout) {
+
+    }
+
+}
