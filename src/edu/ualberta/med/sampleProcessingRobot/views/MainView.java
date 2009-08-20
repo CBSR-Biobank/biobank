951667b2b8a2ddeb79ea356fe13e37bcab63d202
degrisda <degrisda@cs.ualberta.ca>
2009-08-19 19:22:23 -0600

    Modify the view to show a better one

---
+++ src/edu/ualberta/med/sampleProcessingRobot/views/MainView.java
@@ -0,0 +1,20 @@
+package edu.ualberta.med.sampleProcessingRobot.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MainView extends ViewPart {
+
+    public static final String ID = "edu.ualberta.med.sampleProcessingRobot.views.MainView";
+
+    @Override
+    public void createPartControl(Composite parent) {
+    }
+
+    /**
+     * Passing the focus request to the viewer's control.
+     */
+    @Override
+    public void setFocus() {
+    }
+}
