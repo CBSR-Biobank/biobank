275bea0645d8756e99849a6fea94f2aefff8517d
Aaron <aaron@cs.ualberta.ca>
2009-08-26 13:39:47 -0600

    ?

---
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/dialogs/SampleTypeDialog.java
@@ -0,0 +1,77 @@
+package edu.ualberta.med.biobank.dialogs;
+
+import java.util.HashMap;
+
+import org.eclipse.core.databinding.beans.PojoObservables;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import edu.ualberta.med.biobank.model.SampleType;
+import edu.ualberta.med.biobank.validators.NonEmptyString;
+
+public class SampleTypeDialog extends BiobankDialog {
+
+    private static final String TITLE = " Sample Type ";
+
+    private static final String MSG_NEW_ST_OK = "Create a new sample type.";
+    private static final String MSG_ST_OK = "Edit a sample type.";
+    private static final String MSG_NO_ST_NAME = "Sample type must have a name.";
+    private static final String MSG_NO_ST_SNAME = "Sample type must have a short name.";
+
+    private SampleType sampleType;
+
+    private HashMap<String, SampleType> sampleTypeMap;
+
+    public SampleTypeDialog(Shell parent, SampleType sampleType) {
+        super(parent);
+        Assert.isNotNull(sampleType);
+        this.sampleType = sampleType;
+    }
+
+    @Override
+    protected void configureShell(Shell shell) {
+        super.configureShell(shell);
+        Integer id = sampleType.getId();
+        String title = new String();
+
+        if (id == null) {
+            title = "Add";
+        } else {
+            title = "Edit ";
+        }
+        title += TITLE;
+        shell.setText(title);
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite parentComposite = (Composite) super.createDialogArea(parent);
+        Composite client = new Composite(parentComposite, SWT.NONE);
+        GridLayout layout = new GridLayout(2, false);
+        client.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        layout.horizontalSpacing = 10;
+        client.setLayout(layout);
+        client.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        createBoundWidgetWithLabel(client, Text.class, SWT.NONE, "Name", null,
+            PojoObservables.observeValue(sampleType, "name"),
+            NonEmptyString.class, MSG_NO_ST_NAME);
+
+        createBoundWidgetWithLabel(client, Text.class, SWT.NONE, "Short Name",
+            null, PojoObservables.observeValue(sampleType, "nameShort"),
+            NonEmptyString.class, MSG_NO_ST_SNAME);
+
+        return client;
+    }
+
+    public SampleType getSampleType() {
+        return sampleType;
+    }
+
+}
---
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/SampleTypeEntryWidget.java
@@ -0,0 +1,210 @@
+package edu.ualberta.med.biobank.widgets;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+import edu.ualberta.med.biobank.BioBankPlugin;
+import edu.ualberta.med.biobank.dialogs.SampleTypeDialog;
+import edu.ualberta.med.biobank.model.SampleType;
+import edu.ualberta.med.biobank.widgets.infotables.BiobankCollectionModel;
+import edu.ualberta.med.biobank.widgets.infotables.SampleTypeInfoTable;
+import edu.ualberta.med.biobank.widgets.listener.BiobankEntryFormWidgetListener;
+import edu.ualberta.med.biobank.widgets.listener.MultiSelectEvent;
+
+/**
+ * Displays the current sample storage collection and allows the user to add
+ * additional sample storage to the collection.
+ */
+public class SampleTypeEntryWidget extends BiobankWidget {
+
+    private SampleTypeInfoTable sampleTypeTable;
+
+    private Button addSampleTypeButton;
+
+    private Collection<SampleType> selectedSampleTypes;
+
+    private Collection<SampleType> conflictTypes;
+
+    /**
+     * 
+     * @param parent a composite control which will be the parent of the new
+     *            instance (cannot be null)
+     * @param style the style of control to construct
+     * @param SampleTypeCollection the sample storage already selected and to be
+     *            displayed in the table viewer (can be null).
+     * @param toolkit The toolkit is responsible for creating SWT controls
+     *            adapted to work in Eclipse forms. If widget is not used in a
+     *            form this parameter should be null.
+     */
+    public SampleTypeEntryWidget(Composite parent, int style,
+        Collection<SampleType> sampleTypeCollection,
+        Collection<SampleType> conflictTypes, FormToolkit toolkit) {
+        super(parent, style);
+        Assert.isNotNull(toolkit, "toolkit is null");
+
+        this.conflictTypes = conflictTypes;
+
+        if (sampleTypeCollection == null) {
+            selectedSampleTypes = new HashSet<SampleType>();
+        } else {
+            selectedSampleTypes = sampleTypeCollection;
+        }
+
+        setLayout(new GridLayout(1, false));
+        setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        sampleTypeTable = new SampleTypeInfoTable(parent, selectedSampleTypes);
+        sampleTypeTable.adaptToToolkit(toolkit, true);
+
+        addTableMenu();
+        sampleTypeTable
+            .addSelectionChangedListener(new BiobankEntryFormWidgetListener() {
+                @Override
+                public void selectionChanged(MultiSelectEvent event) {
+                    SampleTypeEntryWidget.this.notifyListeners();
+                }
+            });
+
+        addSampleTypeButton = toolkit.createButton(parent, "Add Sample Type",
+            SWT.PUSH);
+        addSampleTypeButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                addOrEditSampleType(true, new SampleType(),
+                    getRestrictedTypes());
+            }
+        });
+    }
+
+    private void addOrEditSampleType(boolean add, SampleType sampleType,
+        Set<SampleType> restrictedTypes) {
+        SampleTypeDialog dlg = new SampleTypeDialog(PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getShell(), sampleType);
+        if (dlg.open() == Dialog.OK) {
+            if (addEditOk(sampleType, restrictedTypes)) {
+                if (add) {
+
+                    // only add to the collection when adding and not editing
+                    selectedSampleTypes.add(dlg.getSampleType());
+                }
+                sampleTypeTable.setCollection(selectedSampleTypes);
+            } else {
+                BioBankPlugin.openAsyncError("Name Problem",
+                    "A type with the same name or short name already exists.");
+            }
+        }
+    }
+
+    // need sample types that have not yet been selected in sampleStorageTable
+    private Set<SampleType> getRestrictedTypes() {
+
+        Set<SampleType> restrictedTypes = new HashSet<SampleType>(conflictTypes);
+        Collection<SampleType> currentSampleTypes = sampleTypeTable
+            .getCollection();
+
+        for (SampleType ss : currentSampleTypes) {
+            restrictedTypes.add(ss);
+        }
+        return restrictedTypes;
+    }
+
+    private void addTableMenu() {
+        Menu menu = new Menu(PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getShell(), SWT.NONE);
+        sampleTypeTable.getTableViewer().getTable().setMenu(menu);
+
+        MenuItem item = new MenuItem(menu, SWT.PUSH);
+        item.setText("Edit");
+        item.addSelectionListener(new SelectionListener() {
+            public void widgetSelected(SelectionEvent event) {
+
+                IStructuredSelection stSelection = (IStructuredSelection) sampleTypeTable
+                    .getTableViewer().getSelection();
+
+                BiobankCollectionModel item = (BiobankCollectionModel) stSelection
+                    .getFirstElement();
+                SampleType pvss = (SampleType) item.o;
+
+                Set<SampleType> restrictedTypes = getRestrictedTypes();
+                restrictedTypes.remove(pvss);
+                addOrEditSampleType(false, pvss, restrictedTypes);
+            }
+
+            public void widgetDefaultSelected(SelectionEvent e) {
+            }
+        });
+
+        item = new MenuItem(menu, SWT.PUSH);
+        item.setText("Delete");
+        item.addSelectionListener(new SelectionListener() {
+            public void widgetSelected(SelectionEvent event) {
+                IStructuredSelection stSelection = (IStructuredSelection) sampleTypeTable
+                    .getTableViewer().getSelection();
+
+                BiobankCollectionModel item = (BiobankCollectionModel) stSelection
+                    .getFirstElement();
+                SampleType sampleType = (SampleType) item.o;
+
+                boolean confirm = MessageDialog.openConfirm(PlatformUI
+                    .getWorkbench().getActiveWorkbenchWindow().getShell(),
+                    "Delete Sample Type",
+                    "Are you sure you want to delete sample type \""
+                        + sampleType.getName() + "\"?");
+
+                if (confirm) {
+                    Collection<SampleType> stToDelete = new HashSet<SampleType>();
+                    for (SampleType st : selectedSampleTypes) {
+                        if (st.getName().equals(sampleType.getName()))
+                            stToDelete.add(st);
+                    }
+
+                    for (SampleType st : stToDelete) {
+                        selectedSampleTypes.remove(st);
+                    }
+
+                    sampleTypeTable.setCollection(selectedSampleTypes);
+                }
+            }
+
+            public void widgetDefaultSelected(SelectionEvent e) {
+            }
+        });
+    }
+
+    private boolean addEditOk(SampleType type, Set<SampleType> restrictedTypes) {
+        for (SampleType st : restrictedTypes) {
+            if (st.getName().equals(type.getName())
+                || st.getNameShort().equals(type.getNameShort())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Collection<SampleType> getTableSampleTypes() {
+        return sampleTypeTable.getCollection();
+    }
+
+    @Override
+    public boolean setFocus() {
+        return addSampleTypeButton.setFocus();
+    }
+}
