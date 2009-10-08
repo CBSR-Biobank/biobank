8a1f403688ce6ae6adee566524fcc9023ef4a9d6
Nelson Loyola <loyola@cs.ualberta.ca>
2009-10-08 15:21:08 -0600

    Forgot to add SiteManager. Implemented CapacityWrapper.

---
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/wrappers/CapacityWrapper.java
@@ -0,0 +1,77 @@
+package edu.ualberta.med.biobank.common.wrappers;
+
+import edu.ualberta.med.biobank.common.BiobankCheckException;
+import edu.ualberta.med.biobank.model.Capacity;
+import gov.nih.nci.system.applicationservice.WritableApplicationService;
+
+public class CapacityWrapper extends ModelWrapper<Capacity> implements
+    Comparable<CapacityWrapper> {
+
+    public CapacityWrapper(WritableApplicationService appService) {
+        super(appService);
+    }
+
+    public CapacityWrapper(WritableApplicationService appService,
+        Capacity wrappedObject) {
+        super(appService, wrappedObject);
+    }
+
+    @Override
+    protected String[] getPropertyChangesNames() {
+        return new String[] { "rowCapacity", "colCapacity" };
+    }
+
+    public void setRow(Integer rowCapacity) {
+        Integer oldRowCapacity = wrappedObject.getRowCapacity();
+        wrappedObject.setRowCapacity(rowCapacity);
+        propertyChangeSupport.firePropertyChange("row", oldRowCapacity,
+            rowCapacity);
+    }
+
+    public Integer getRowCapacity() {
+        return wrappedObject.getRowCapacity();
+    }
+
+    public void setCol(Integer colCapacity) {
+        Integer oldColCapacity = wrappedObject.getColCapacity();
+        wrappedObject.setColCapacity(colCapacity);
+        propertyChangeSupport.firePropertyChange("col", oldColCapacity,
+            colCapacity);
+    }
+
+    public Integer getColCapacity() {
+        return wrappedObject.getColCapacity();
+    }
+
+    @Override
+    protected void deleteChecks() throws BiobankCheckException, Exception {
+        // do nothing
+    }
+
+    @Override
+    public Class<Capacity> getWrappedClass() {
+        return Capacity.class;
+    }
+
+    @Override
+    protected void persistChecks() throws BiobankCheckException, Exception {
+        // do nothing
+    }
+
+    @Override
+    public int compareTo(CapacityWrapper cap) {
+        Integer rowCapacity1 = getRowCapacity();
+        Integer rowCapacity2 = cap.getRowCapacity();
+        int compare = rowCapacity1.compareTo(rowCapacity2);
+        if (compare == 0) {
+            Integer colCapacity1 = getColCapacity();
+            Integer colCapacity2 = cap.getColCapacity();
+
+            return ((colCapacity1.compareTo(colCapacity2) > 0) ? 1
+                : (colCapacity1.equals(colCapacity2) ? 0 : -1));
+
+        }
+        return (compare > 0) ? 1 : -1;
+    }
+
+}
