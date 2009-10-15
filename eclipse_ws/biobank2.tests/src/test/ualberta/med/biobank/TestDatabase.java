1584752c8b0fd40fbf62e1727687d56ff136fd12
Nelson Loyola <nloyola@gmail.com>
2009-10-15 00:06:39 -0600

    moved get application service to suite class

--- eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/TestDatabase.java
+++ eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/TestDatabase.java
@@ -1,26 +1,15 @@
 package test.ualberta.med.biobank;
 
-import gov.nih.nci.system.applicationservice.ApplicationException;
 import gov.nih.nci.system.applicationservice.WritableApplicationService;
-import gov.nih.nci.system.client.ApplicationServiceProvider;
 
 import org.junit.Before;
-import org.springframework.remoting.RemoteAccessException;
 
 public class TestDatabase {
     protected WritableApplicationService appService;
 
     @Before
-    public void setUp() throws Exception {
-        try {
-            appService = (WritableApplicationService) ApplicationServiceProvider
-                .getApplicationServiceFromUrl("http://localhost:8080/biobank2",
-                    "testuser", "test");
-        } catch (ApplicationException exp) {
-            System.out.println("Login failed - error authenticating user");
-        } catch (RemoteAccessException exp) {
-            System.out.println("Login Failed - Remote Access Exception:");
-        }
+    public void setUp() {
+        appService = AllTests.appService;
     }
 
 }
