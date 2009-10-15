c8ee3775dbbda3e87dc152d95b23ee04d1860799
Delphine Degris-Dard <degrisda@cs.ualberta.ca>
2009-10-15 10:19:45 -0600

    Site test case add

---
+++ eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/TestSite.java
@@ -0,0 +1,279 @@
+package test.ualberta.med.biobank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSite {
+
+	@Test
+	public void testReload() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSiteWrapperWritableApplicationServiceSite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSiteWrapperWritableApplicationService() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActivityStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetActivityStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStreet1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStreet1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStreet2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStreet2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProvince() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProvince() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPostalCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPostalCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWrappedClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStudyCollectionBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStudyCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClinicCollectionBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClinicCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetContainerTypeCollectionBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetContainerTypeCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetContainerTypeCollectionCollectionOfContainerTypeBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetContainerTypeCollectionListOfContainerTypeWrapper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetContainerCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetContainerCollectionCollectionOfContainerBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetContainerCollectionListOfContainerWrapper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTopContainerCollectionBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTopContainerCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearTopContainerCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSampleTypeCollectionBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSampleTypeCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSampleTypeCollectionCollectionOfSampleTypeBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSampleTypeCollectionListOfSampleTypeWrapper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPvInfoPossibleCollectionBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPvInfoPossibleCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPvInfoPossibleCollectionCollectionOfPvInfoPossibleBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPvInfoPossibleCollectionListOfPvInfoPossibleWrapper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPvInfoPossibleLabels() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPvInfoPossible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPvInfoPossible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllSites() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSites() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPersist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckIntegrity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
