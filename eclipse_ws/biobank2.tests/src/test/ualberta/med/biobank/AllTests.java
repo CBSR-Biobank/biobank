b13d5c0f246c2e8c40548275635ef1a54f5dce27
Aaron <aaron@cs.ualberta.ca>
2009-10-19 13:28:36 -0600

    oops, wrong branch

--- eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/AllTests.java
+++ eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/AllTests.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses( { TestContainer.class, TestSite.class })
+@SuiteClasses( { TestContainer.class, TestSite.class, TestContact.class, TestContainerLabelingScheme.class, TestPvSampleSource.class, TestSampleWrapper.java })
 public class AllTests {
 	public static WritableApplicationService appService = null;
 
