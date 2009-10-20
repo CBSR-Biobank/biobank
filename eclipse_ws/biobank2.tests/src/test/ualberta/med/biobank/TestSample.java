87941036945fd1ae9714e587873c0f8c82c94121
Aaron <aaron@cs.ualberta.ca>
2009-10-20 16:33:45 -0600

    minor updates

--- eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/TestSample.java
+++ eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/TestSample.java
@@ -2,113 +2,124 @@ package test.ualberta.med.biobank;
 
 import java.util.Collection;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import test.ualberta.med.biobank.internal.ClinicHelper;
+import test.ualberta.med.biobank.internal.ContainerHelper;
+import test.ualberta.med.biobank.internal.PatientHelper;
+import test.ualberta.med.biobank.internal.PatientVisitHelper;
+import test.ualberta.med.biobank.internal.SampleHelper;
+import test.ualberta.med.biobank.internal.SiteHelper;
+import test.ualberta.med.biobank.internal.StudyHelper;
 import edu.ualberta.med.biobank.common.BiobankCheckException;
 import edu.ualberta.med.biobank.common.RowColPos;
-import edu.ualberta.med.biobank.common.wrappers.ContainerTypeWrapper;
+import edu.ualberta.med.biobank.common.wrappers.ClinicWrapper;
 import edu.ualberta.med.biobank.common.wrappers.ContainerWrapper;
 import edu.ualberta.med.biobank.common.wrappers.PatientVisitWrapper;
+import edu.ualberta.med.biobank.common.wrappers.PatientWrapper;
 import edu.ualberta.med.biobank.common.wrappers.SampleTypeWrapper;
 import edu.ualberta.med.biobank.common.wrappers.SampleWrapper;
 import edu.ualberta.med.biobank.common.wrappers.SiteWrapper;
-import edu.ualberta.med.biobank.model.Container;
-import edu.ualberta.med.biobank.model.ContainerType;
+import edu.ualberta.med.biobank.common.wrappers.StudyWrapper;
 import edu.ualberta.med.biobank.model.PatientVisit;
-import edu.ualberta.med.biobank.model.Sample;
 import edu.ualberta.med.biobank.model.SampleType;
-import edu.ualberta.med.biobank.model.Site;
 
 public class TestSample extends TestDatabase {
 
-    SampleWrapper sw;
-    
+    SampleWrapper sample;
+
+    @Override
     @Before
     public void setUp() throws Exception {
-    	super.setUp();
-    	SampleTypeWrapper sampleTypeWrapper = TestSampleType.addSampleTypeWrapper();
-        sw = newSampleWrapper(sampleTypeWrapper);
-    }
-    
-   
-    private SampleWrapper newSampleWrapper(SampleTypeWrapper sampleType) {
-    	SampleWrapper sw=new SampleWrapper(appService, new Sample());
-    	sw.setSampleType(sampleType);
-		return sw;
-	}
-
-
-	@Test(expected=BiobankCheckException.class)
-    public void TestPersistChecks() throws BiobankCheckException, Exception {
-    	sw.persist();
+        super.setUp();
+        SampleTypeWrapper sampleTypeWrapper = TestSampleType
+            .addSampleTypeWrapper();
+        SiteHelper.deletedCreatedSites();
+        SiteWrapper site = SiteHelper.addSite("sitename", true);
+        ContainerWrapper container = ContainerHelper.addContainerRandom(site,
+            "newcontainer");
+        StudyWrapper study = StudyHelper.addStudy(site, "studyname");
+        PatientWrapper patient = PatientHelper.addPatient("5684", study);
+        ClinicWrapper clinic = ClinicHelper.addClinic(site, "clinicname");
+        PatientVisitWrapper pv = PatientVisitHelper.addPatientVisit(patient,
+            clinic, null, null, null);
+        sample = SampleHelper.newSample(sampleTypeWrapper, container, pv, 3, 3);
     }
 
-    @Test(expected=BiobankCheckException.class)
-    public void TestCheckInventoryIdUnique() throws BiobankCheckException, Exception {
-        SampleWrapper duplicate = new SampleWrapper(appService, new Sample());
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        SiteHelper.deletedCreatedSites();
+    }
+
+    @Test(expected = BiobankCheckException.class)
+    public void TestCheckInventoryIdUnique() throws BiobankCheckException,
+        Exception {
+        SampleWrapper duplicate = SampleHelper.addSample(
+            sample.getSampleType(), sample.getParent(), sample
+                .getPatientVisit(), 3, 3);
+        // should be allowed same position?
         duplicate.persist();
-        sw.setInventoryId(duplicate.getInventoryId());
-        sw.checkInventoryIdUnique();
+        sample.setInventoryId(duplicate.getInventoryId());
+        sample.checkInventoryIdUnique();
+
     }
 
     @Test
     public void TestGetSetPatientVisit() {
-       PatientVisitWrapper pvw = new PatientVisitWrapper(appService, new PatientVisit());
-       sw.setPatientVisit(pvw.getWrappedObject());
-       Assert.assertTrue(sw.getPatientVisit().getId()==pvw.getId());
+        PatientVisitWrapper pvw = new PatientVisitWrapper(appService,
+            new PatientVisit());
+        sample.setPatientVisit(pvw.getWrappedObject());
+        Assert.assertTrue(sample.getPatientVisit().getId() == pvw.getId());
     }
 
     @Test
     public void TestSetSamplePositionFromString() throws Exception {
-    	SiteWrapper site = new SiteWrapper(appService, new Site());
-    	site.setStreet1("stree1");
-    	site.persist();
-    	ContainerWrapper container = new ContainerWrapper(appService, new Container());
-    	container.setSite(site);
-    	ContainerTypeWrapper containerType = new ContainerTypeWrapper(appService, new ContainerType());
-    	containerType.setSite(site);
-    	containerType.persist();
-    	container.setContainerType(containerType);
-    	container.persist();
-    	sw.setSamplePositionFromString("01AA", container);
-    	Assert.assertTrue(sw.getPositionString().equals("01AA"));
-    	RowColPos pos=sw.getPosition();
-    	Assert.assertTrue(pos.col==0&&pos.row==0);
+        sample.setSamplePositionFromString("01AA", sample.getParent());
+        Assert.assertTrue(sample.getPositionString().equals("01AA"));
+        RowColPos pos = sample.getPosition();
+        Assert.assertTrue((pos.col == 0) && (pos.row == 0));
     }
 
     @Test
     public void TestGetSetPosition() {
         RowColPos position = new RowColPos();
-        position.row=1;
-        position.col=3;
-        sw.setPosition(position);
-        RowColPos newPosition = sw.getPosition();
-        Assert.assertTrue(newPosition.row==position.row&&newPosition.col==position.col);
+        position.row = 1;
+        position.col = 3;
+        sample.setPosition(position);
+        RowColPos newPosition = sample.getPosition();
+        Assert.assertTrue((newPosition.row == position.row)
+            && (newPosition.col == position.col));
     }
-    
+
     @Test
     public void TestGetSetParent() throws Exception {
-       Assert.assertTrue(sw.getParent()==null);
-       ContainerWrapper parent = new ContainerWrapper(appService, new Container());
-       sw.setParent(parent);
-       Assert.assertTrue(sw.getParent()!=null);
-       Collection<SampleWrapper> sampleWrappers = parent.getSamples();
-       boolean found=false;
-       for (SampleWrapper sampleWrapper:sampleWrappers) {
-           if (sampleWrapper.getId()==sw.getId()) found=true;
-       }
-       Assert.assertTrue(found);
+        Assert.assertTrue(sample.getParent() == null);
+        ContainerWrapper parent = ContainerHelper.addContainer("newcontainer",
+            "barcode", sample.getParent(), sample.getSite(), sample.getParent()
+                .getContainerType());
+        sample.setParent(parent);
+        Assert.assertTrue(sample.getParent() != null);
+        Collection<SampleWrapper> sampleWrappers = parent.getSamples();
+        boolean found = false;
+        for (SampleWrapper sampleWrapper : sampleWrappers) {
+            if (sampleWrapper.getId() == sample.getId())
+                found = true;
+        }
+        Assert.assertTrue(found);
     }
-    
+
     @Test
     public void TestGetSetSampleType() {
-    	SampleTypeWrapper stw=sw.getSampleType();
-    	SampleTypeWrapper newStw= new SampleTypeWrapper(appService, new SampleType());
-    	Assert.assertTrue(stw.getId()!=newStw.getId());
-    	sw.setSampleType(newStw);
-    	Assert.assertTrue(stw.getId()==sw.getSampleType().getId());
+        SampleTypeWrapper stw = sample.getSampleType();
+        SampleTypeWrapper newStw = new SampleTypeWrapper(appService,
+            new SampleType());
+        Assert.assertTrue(stw.getId() != newStw.getId());
+        sample.setSampleType(newStw);
+        Assert.assertTrue(stw.getId() == sample.getSampleType().getId());
     }
 
     @Test
@@ -117,12 +128,12 @@ public class TestSample extends TestDatabase {
 
     @Test
     public void TestGetSetQuantityFromType() {
-        
+
     }
 
     @Test
     public void TestLoadAttributes() {
-        
+
     }
 
     @Test
@@ -132,6 +143,6 @@ public class TestSample extends TestDatabase {
 
     @Test
     public void TestCompareTo() {
-        
+
     }
 }
