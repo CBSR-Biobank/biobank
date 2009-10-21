621c5e2123882467878a92a5b8e38c4facf493ec
Aaron <aaron@cs.ualberta.ca>
2009-10-21 12:51:57 -0600

    pvsamplesource tests updated with fixed date

--- eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/Utils.java
+++ eclipse_ws/biobank2.tests/src/test/ualberta/med/biobank/Utils.java
@@ -24,7 +24,7 @@ public class Utils {
     }
 
     public static Date getRandomDate() throws ParseException {
-        String dateStr = String.format("%04-%02-%02 %02:%02", 2000 + r
+        String dateStr = String.format("%04d-%02d-%02d %02d:%02d", 2000 + r
             .nextInt(40), r.nextInt(12) + 1, r.nextInt(30) + 1,
             r.nextInt(24) + 1, r.nextInt(60) + 1);
         return DateFormatter.dateFormatter.parse(dateStr);
