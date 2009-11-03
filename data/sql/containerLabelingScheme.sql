2020cf67b252dff1caea57a6e88adaa89f0dcfc5
Delphine Degris-Dard <degrisda@cs.ualberta.ca>
2009-11-03 16:49:22 -0700

    remove N/A labeling scheme

--- data/sql/containerLabelingScheme.sql
+++ data/sql/containerLabelingScheme.sql
@@ -2,6 +2,5 @@ LOCK TABLES `CONTAINER_LABELING_SCHEME` WRITE;
 INSERT INTO `CONTAINER_LABELING_SCHEME` (ID, NAME) VALUES
 ( 1, "SBS Standard"),
 ( 2, "CBSR 2 char alphabetic"),
-( 3, "2 char numeric"),
-( 4, "N/A");
+( 3, "2 char numeric");
 UNLOCK TABLES;
