b1d0a4ab4c9d8d4ca1d3fda59e1d5f40556cd60b
Delphine Degris-Dard <degrisda@ualberta.ca>
2009-12-08 17:50:30 -0700

    add jasperreport file for scan printing

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/AbstractPatientAdminForm.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/AbstractPatientAdminForm.java
@@ -3,15 +3,9 @@ package edu.ualberta.med.biobank.forms;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-import net.sf.jasperreports.engine.JRException;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.printing.PrintDialog;
-import org.eclipse.swt.printing.Printer;
-import org.eclipse.swt.printing.PrinterData;
 import org.eclipse.ui.PlatformUI;
 
 import edu.ualberta.med.biobank.BioBankPlugin;
@@ -61,56 +55,16 @@ public abstract class AbstractPatientAdminForm extends BiobankEntryForm {
     }
 
     protected void print() {
-        if (appender != null) {
-            List<LogInfo> logsList = appender.getLogsList();
-            PrinterData data = null;
-            // if (!BioBankPlugin.getDefault().isDebugging()) {
-            // data = Printer.getDefaultPrinterData();
-            // }
-            if (data == null) {
-                PrintDialog dialog = new PrintDialog(PlatformUI.getWorkbench()
-                    .getActiveWorkbenchWindow().getShell(), SWT.NONE);
-                data = dialog.open();
-            }
-            if (data == null)
-                return;
-
-            // // use existing print functionality of swt styled texts
-            // StyledText sText = new StyledText(form, SWT.NONE);
-            // sText.setText(sb.toString());
-            //
-            // FontData printerFd = new FontData("Sans", 8, SWT.NORMAL);
-            // final Font font = new Font(Display.getCurrent(), printerFd);
-            // sText.setFont(font);
-            // final Printer printer = new Printer(data);
-            // StyledTextPrintOptions options = new StyledTextPrintOptions();
-            // options.footer = "Printed on " + dateFormat.format(new Date())
-            // + StyledTextPrintOptions.SEPARATOR
-            // + StyledTextPrintOptions.SEPARATOR
-            // + StyledTextPrintOptions.PAGE_TAG;
-            // options.header = "Biobank2 - Activity Report"
-            // + StyledTextPrintOptions.SEPARATOR
-            // + StyledTextPrintOptions.SEPARATOR + "USER:"
-            // + SessionManager.getInstance().getSession().getUserName();
-            // options.jobName = "scannedLinkedActivity";
-            // final Runnable styledTextPrinter = sText.print(printer, options);
-            // styledTextPrinter.run();
-            // 
-            // font.dispose();
-
-            try {
-                LogInfo.printLogReport(SessionManager.getInstance()
-                    .getSession().getUserName(), logsList);
-            } catch (JRException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            Printer printer = new Printer(data);
-            printer.dispose();
-
-            return;
+        if (appender == null) {
+            BioBankPlugin.openError("Print error", "Can't print: log error.");
         }
-        BioBankPlugin.openError("Print error", "Can't print: log error.");
+        try {
+            List<LogInfo> logsList = appender.getLogsList();
+            LogInfo.printLogReport(SessionManager.getInstance().getSession()
+                .getUserName(), logsList);
+        } catch (Exception e) {
+            BioBankPlugin.openAsyncError("Print error", e);
+        }
     }
 
     protected abstract String getActivityTitle();
