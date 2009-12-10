89c22f9dd3fae590863b93513849ac057303e3c1
Delphine Degris-Dard <degrisda@ualberta.ca>
2009-12-10 12:26:35 -0700

    ssl configuration modification

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/ServiceConnection.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/ServiceConnection.java
@@ -1,26 +1,33 @@
 package edu.ualberta.med.biobank.common;
 
-import java.net.URL;
-
 import gov.nih.nci.system.applicationservice.WritableApplicationService;
 import gov.nih.nci.system.client.ApplicationServiceProvider;
 
+import java.net.URL;
+
 public class ServiceConnection {
 
-    static {
-        URL url = ServiceConnection.class
-            .getResource("cert/localhost.keystore");
-        System.setProperty("javax.net.ssl.trustStore", url.getFile());
-    }
+    private static WritableApplicationService appService;
 
     public static WritableApplicationService getAppService(String serverUrl,
         String userName, String password) throws Exception {
-        if (userName == null) {
-            return (WritableApplicationService) ApplicationServiceProvider
-                .getApplicationServiceFromUrl(serverUrl);
+        if (appService == null) {
+            if (serverUrl.startsWith("https")) {
+                String name = serverUrl.substring("https://".length());
+                name = name.substring(0, name.indexOf(":"));
+                URL url = ServiceConnection.class.getResource("cert/" + name
+                    + ".keystore");
+                System.setProperty("javax.net.ssl.trustStore", url.getFile());
+            }
+            if (userName == null) {
+                appService = (WritableApplicationService) ApplicationServiceProvider
+                    .getApplicationServiceFromUrl(serverUrl);
+            } else {
+                appService = (WritableApplicationService) ApplicationServiceProvider
+                    .getApplicationServiceFromUrl(serverUrl, userName, password);
+            }
         }
-        return (WritableApplicationService) ApplicationServiceProvider
-            .getApplicationServiceFromUrl(serverUrl, userName, password);
+        return appService;
     }
 
     public static WritableApplicationService getAppService(String serverUrl)
