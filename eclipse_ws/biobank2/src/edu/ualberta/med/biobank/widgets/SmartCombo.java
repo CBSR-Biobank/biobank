940d55910d7d30bd1a391fea7af35a56f2cf248f
Aaron <aaron.young@ualberta.ca>
2010-01-28 14:15:27 -0700

    removed smartcombo code for now

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/SmartCombo.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/SmartCombo.java
@@ -1,8 +1,10 @@
 package edu.ualberta.med.biobank.widgets;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 
@@ -16,13 +18,37 @@ public class SmartCombo {
         combo.setItems(args);
         items = args;
         combo.select(0);
-        combo.addModifyListener(new ModifyListener() {
+        combo.addKeyListener(new KeyListener() {
+
             @Override
-            public void modifyText(ModifyEvent e) {
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
                 if (items != null) {
                     refineList(combo.getText());
                     combo.setListVisible(true);
                 }
+            }
+        });
+        combo.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseDoubleClick(MouseEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void mouseDown(MouseEvent e) {
+                resetList();
+            }
+
+            @Override
+            public void mouseUp(MouseEvent e) {
+
             }
         });
     }
@@ -34,5 +60,16 @@ public class SmartCombo {
             if (item.startsWith(text))
                 combo.add(item);
         }
+        combo.computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
     }
+
+    protected void resetList() {
+        combo.remove(0, combo.getItemCount() - 1);
+        for (int i = 0; i < items.length; i++) {
+            String item = items[i];
+            combo.add(item);
+        }
+        combo.computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
+    }
+
 }
