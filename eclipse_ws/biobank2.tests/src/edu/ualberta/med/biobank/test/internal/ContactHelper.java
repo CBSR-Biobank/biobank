92358581672c5f485576acd7f3f120bb07f3869b
Nelson Loyola <nloyola@gmail.com>
2010-02-20 21:14:51 -0700

    renamed test package from 'test.ualberta.med.biobank' to 'edu.ualberta.med.biobank.test'

---
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/internal/ContactHelper.java
@@ -0,0 +1,61 @@
+package edu.ualberta.med.biobank.test.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ualberta.med.biobank.common.wrappers.ClinicWrapper;
+import edu.ualberta.med.biobank.common.wrappers.ContactWrapper;
+import edu.ualberta.med.biobank.common.wrappers.SiteWrapper;
+import edu.ualberta.med.biobank.common.wrappers.StudyWrapper;
+
+public class ContactHelper extends DbHelper {
+
+    public static ContactWrapper newContact(ClinicWrapper clinic, String name) {
+        ContactWrapper contact = new ContactWrapper(appService);
+        contact.setClinic(clinic);
+        contact.setName(name + r.nextInt());
+        contact.setEmailAddress("toto@gmail.com");
+        return contact;
+    }
+
+    public static ContactWrapper addContact(ClinicWrapper clinic, String name)
+        throws Exception {
+        ContactWrapper contact = newContact(clinic, name);
+        contact.persist();
+        return contact;
+    }
+
+    public static int addContactsToStudy(StudyWrapper study, String name)
+        throws Exception {
+        SiteWrapper site = study.getSite();
+        ClinicHelper.addClinics(site, name, r.nextInt(15) + 3, true);
+        List<ClinicWrapper> clinics = site.getClinicCollection();
+        int nber = r.nextInt(clinics.size() - 2) + 1;
+        List<ContactWrapper> contacts = new ArrayList<ContactWrapper>();
+        for (int i = 0; i < nber; i++) {
+            ClinicWrapper clinic = clinics.get(i);
+            ContactWrapper contact = chooseRandomlyInList(clinic
+                .getContactCollection());
+            contacts.add(contact);
+        }
+        study.addContacts(contacts);
+        study.persist();
+        return nber;
+    }
+
+    public static int addContactsToClinic(ClinicWrapper clinic, String name,
+        int min, int max) throws Exception {
+        int nber = r.nextInt(max - min + 1) + min;
+        for (int i = 0; i < nber; i++) {
+            ContactHelper.addContact(clinic, name + i);
+        }
+        clinic.reload();
+        return nber;
+    }
+
+    public static int addContactsToClinic(ClinicWrapper clinic, String name)
+        throws Exception {
+        return addContactsToClinic(clinic, name, 1, 5);
+    }
+
+}
---
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/wrappers/TestSampleSource.java
@@ -0,0 +1,86 @@
+package edu.ualberta.med.biobank.test.wrappers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ualberta.med.biobank.common.wrappers.SampleSourceWrapper;
+import edu.ualberta.med.biobank.common.wrappers.SiteWrapper;
+import edu.ualberta.med.biobank.common.wrappers.StudyWrapper;
+import edu.ualberta.med.biobank.test.TestDatabase;
+import edu.ualberta.med.biobank.test.internal.SampleSourceHelper;
+import edu.ualberta.med.biobank.test.internal.SiteHelper;
+import edu.ualberta.med.biobank.test.internal.StudyHelper;
+import gov.nih.nci.system.applicationservice.ApplicationException;
+
+public class TestSampleSource extends TestDatabase {
+
+    SampleSourceWrapper ssw;
+    SiteWrapper defaultSite;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        ssw = SampleSourceHelper.addSampleSource("SampleSourceName");
+        defaultSite = SiteHelper.addSite("Default");
+    }
+
+    @Test
+    public void testGetStudyCollection() throws Exception {
+        List<StudyWrapper> studies = new ArrayList<StudyWrapper>();
+        for (int i = 0; i < 3; i++) {
+            StudyWrapper newStudy = StudyHelper.newStudy(defaultSite,
+                "newStudy" + i);
+            newStudy.addSampleSources(Arrays.asList(ssw));
+            newStudy.persist();
+            studies.add(newStudy);
+        }
+        ssw.reload();
+        List<StudyWrapper> foundStudies = new ArrayList<StudyWrapper>(ssw
+            .getStudyCollection(false));
+        Assert.assertEquals(studies.size(), foundStudies.size());
+        for (StudyWrapper study : studies) {
+            Assert.assertTrue(foundStudies.contains(study));
+        }
+    }
+
+    @Test
+    public void testCompareTo() throws Exception {
+        SampleSourceWrapper newSampleSource = SampleSourceHelper
+            .addSampleSource(ssw.getName() + "1");
+        Assert.assertTrue(newSampleSource.compareTo(ssw) > 0);
+        Assert.assertTrue(ssw.compareTo(newSampleSource) < 0);
+        newSampleSource.setName(ssw.getName());
+        Assert.assertTrue(newSampleSource.compareTo(ssw) == 0);
+    }
+
+    @Test
+    public void testResetAlreadyInDatabase() throws Exception {
+        String old = ssw.getName();
+        ssw.setName("toto");
+        ssw.reset();
+        Assert.assertEquals(old, ssw.getName());
+    }
+
+    @Test
+    public void testResetNew() throws Exception {
+        SampleSourceWrapper ssw = SampleSourceHelper
+            .newSampleSource("testResetNew");
+        ssw.setName("toto");
+        ssw.reset();
+        Assert.assertEquals(null, ssw.getName());
+    }
+
+    @Test
+    public void testGetAllSampleSources() throws ApplicationException {
+        List<SampleSourceWrapper> list = SampleSourceWrapper
+            .getAllSampleSources(appService);
+        Assert.assertTrue(list.contains(ssw));
+
+    }
+}
