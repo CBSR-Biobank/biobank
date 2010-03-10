2fcd8c182328d5d6b96a71b0ec072e9d3d6fbbad
Nelson Loyola <nloyola@gmail.com>
2010-03-09 20:17:55 -0700

    working on import test scripts

--- scripts/import/300_aliquots.php
+++ scripts/import/300_aliquots.php
@@ -309,6 +309,7 @@ select *
 from aliquot
 join patient_visit on patient_visit.id=aliquot.PATIENT_VISIT_ID
 join patient on patient.id=patient_visit.PATIENT_ID
+join pv_source_vessel on pv_source_vessel.patient_visit_id=patient_visit.id
 join pv_attr on pv_attr.PATIENT_VISIT_ID=patient_visit.id
 join study_pv_attr on study_pv_attr.id=pv_attr.STUDY_PV_ATTR_ID
 join aliquot_position on aliquot_position.aliquot_ID=aliquot.id
@@ -322,6 +323,7 @@ $headings = array('patient_nr', 'visit_nr', 'date_taken', 'date_received',
     'worksheet', 'fnum', 'rack', 'box', 'cell', 'inventory_id');
 
 $chr2patientNr = getPatientNrs();
+$chr2visitNr = getVisitNrs();
 
 mysql_connect("localhost", "dummy", "ozzy498") or die(mysql_error());
 mysql_select_db("biobank2") or die(mysql_error());
@@ -337,19 +339,20 @@ foreach ($inventory_ids as $inv_id) {
         
         $data = array(
             'patient_nr' => $chr2patientNr[$row['PNUMBER']], 
-            'visit_nr' => '', 
-            'date_taken' => '', 
+            'visit_nr' => $chr2visitNr[$row['INVENTORY_ID']], 
+            'date_taken' => date('d-M-y', strtotime($row['DATE_DRAWN'])), 
             'date_received'=> date('d-M-y', strtotime($row['DATE_PROCESSED'])),
-            'worksheet' => $row['VALUE'], 
+            'worksheet' => '"' . $row['VALUE'] . '"', 
             'fnum' => intval(substr($row['LABEL'], 0, 2)), 
-            'rack' => substr($row['LABEL'], 2, 2), 
+            'rack' => '"' . substr($row['LABEL'], 2, 2) . '"', 
             'box' => intval(substr($row['LABEL'], 4, 2)), 
-            'cell' => sprintf("%c%d", $row['ROW'] + 65,  $row['COL'] + 1),  
-            'inventory_id' => $row['INVENTORY_ID']
+            'cell' => sprintf("\"%c%d\"", $row['ROW'] + 65,  $row['COL'] + 1),  
+            'inventory_id' => '"' . $row['INVENTORY_ID'] . '"'
         );
         echo implode(",", $data), "\n";
     }
 }
+mysql_close();
 
 function getPatientNrs() {
     mysql_connect("localhost", "dummy", "ozzy498") or die(mysql_error());
@@ -363,6 +366,27 @@ function getPatientNrs() {
     }
     mysql_close();
     return $chr2patientNr;
+}
+
+function getVisitNrs() {
+    global $inventory_ids;
+    
+    mysql_connect("localhost", "dummy", "ozzy498") or die(mysql_error());
+    mysql_select_db("bbpdb") or die(mysql_error());
+
+    $chr2visitNr = array();    
+    foreach ($inventory_ids as $inv_id) {
+        $result = mysql_query("
+            select visit_nr, inventory_id from freezer
+            where inventory_id like binary '$inv_id'") 
+                or die(mysql_error());    
+
+        while($row = mysql_fetch_array($result)) {
+            $chr2visitNr[$row['inventory_id']] = $row['visit_nr'];
+        }
+    }
+    mysql_close();
+    return $chr2visitNr;
 }
 
 ?>
