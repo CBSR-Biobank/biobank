a5b00cd215e54f38ac7595a4a016a323bf15f3d8
Nelson Loyola <nloyola@gmail.com>
2010-03-10 17:20:32 -0700

    fix for freezer 99

--- scripts/import/300_aliquots.php
+++ scripts/import/300_aliquots.php
@@ -1,310 +1,11 @@
 #!/usr/bin/env php
 <?php
 
-$cbsr = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
+  /**
+   * this script is used to test the information for samples selected at random.
+   */
 
-$inventory_id = array(
-   "NUBT585236",
-   "1fis",
-   "bbjv",
-   "AWTY",
-   "bajd",
-   "bafu",
-   "bbyv",
-   "AXUX",
-   "bcsk",
-   "bdgy",
-   "bdxq",
-   "bgbu",
-   "BAAN",
-   "bhwy",
-   "bhqk",
-   "BCFN",
-   "BCZV",
-   "blav",
-   "BFLP",
-   "BEMP",
-   "BENC",
-   "bnad",
-   "ccmo",
-   "cdjg",
-   "cfcr",
-   "cger",
-   "1qsn",
-   "BLYK",
-   "cjho",
-   "ckit",
-   "cmty",
-   "cphs",
-   "1uiv",
-   "1vwr",
-   "BTAB",
-   "1xqr",
-   "BUDF",
-   "cwch",
-   "BWVV",
-   "BYBL",
-   "dbhe",
-   "BWOZ",
-   "ddbu",
-   "1EHB",
-   "cxgd",
-   "1EYT",
-   "NUAT643735",
-   "NUBS368012",
-   "NUBR331756",
-   "NUBS697808",
-   "NUBQ840707",
-   "NUBQ910662",
-   "NUBQ911102",
-   "NUBT479786",
-   "NUBR010455",
-   "NUBS912929",
-   "NUBR050259",
-   "NUBT051526",
-   "NUBT487651",
-   "NUBT521964",
-   "NUBT217724",
-   "NUBT243251",
-   "NUBT276453",
-   "NUBT273438",
-   "NUBT273845",
-   "NUBT211845",
-   "NUBT207295",
-   "NUBT207523",
-   "NUBS446480",
-   "NUBT312658",
-   "NUBT312490",
-   "NUBS558138",
-   "NUBT428029",
-   "NUBT315619",
-   "NUBT530962",
-   "NUBT629479",
-   "NUBS615859",
-   "NUBS616779",
-   "NUBT389119",
-   "NUAD495996",
-   "NUAI268379",
-   "NUAI464946",
-   "NUAI269156",
-   "NUAU426913",
-   "NUAU431650",
-   "NUAU091285",
-   "NUAT697286",
-   "NUBQ938756",
-   "NUBQ821410",
-   "NUBR286775",
-   "NUBT326916",
-   "NUBT025293",
-   "NUBT453247",
-   "NUBR193280",
-   "AANX",
-   "afqt",
-   "AELK",
-   "AEIA",
-   "ACLI",
-   "ahar",
-   "afgq",
-   "afcj",
-   "akgh",
-   "akrm",
-   "abxr",
-   "aliy",
-   "amkv",
-   "adel",
-   "1dki",
-   "1cxk",
-   "ANVY",
-   "ANLE",
-   "1ain",
-   "aoop",
-   "apyz",
-   "apyk",
-   "1bod",
-   "AUCF",
-   "avfb",
-   "1gcp",
-   "1gbv",
-   "ATLZ",
-   "1hft",
-   "atrq",
-   "asys",
-   "azkf",
-   "AHRP",
-   "1cyz",
-   "NUAT688057",
-   "NUBJ009107",
-   "NUAU589975",
-   "1fqh",
-   "bect",
-   "BCPF",
-   "3ard",
-   "0abs",
-   "1ntx",
-   "1nml",
-   "cegi",
-   "cedy",
-   "0bgd",
-   "0bjs",
-   "ciiy",
-   "1tjd",
-   "cmld",
-   "cmke",
-   "cnbk",
-   "1trt",
-   "cnrm",
-   "1ued",
-   "BOUC",
-   "1vlu",
-   "0cei",
-   "1wqz",
-   "BWQI",
-   "5jql",
-   "dajr",
-   "1DOB",
-   "dcwv",
-   "1EAE",
-   "3cor",
-   "0ecn",
-   "1FTX",
-   "NUAC898512",
-   "NUAC929821",
-   "NUAC798850",
-   "NUAC803754",
-   "NUAC885095",
-   "NUAC955941",
-   "NUAT674346",
-   "NUAI166576",
-   "NUAI294268",
-   "NUAW588439",
-   "NUBR020489",
-   "NUAW780532",
-   "NUAW780657",
-   "NUAW520860",
-   "NUAW530636",
-   "NUAW530973",
-   "NUBR175336",
-   "NUBR073621",
-   "NUBR178670",
-   "NUBQ933715",
-   "NUBR058844",
-   "NUBS678238",
-   "NUBR186004",
-   "NUBR187410",
-   "NUBR191857",
-   "NUBQ909347",
-   "NUBR216961",
-   "NUBS369747",
-   "NUBR364408",
-   "NUBT546185",
-   "NUBS505288",
-   "NUBR310771",
-   "NUBT485565",
-   "NUAW591460",
-   "NUBR088313",
-   "NUBS914307",
-   "NUBS914051",
-   "NUBT409741",
-   "NUBT634772",
-   "NUBT641480",
-   "NUBR513624",
-   "NUBR256631",
-   "NUBT474523",
-   "NUBT473348",
-   "NUBT470138",
-   "NUBT489613",
-   "NUBR342585",
-   "NUBT329302",
-   "NUBT332324",
-   "NUBT227262",
-   "NUBT027583",
-   "NUBT030684",
-   "NUBT254239",
-   "NUBS577203",
-   "NUBT586846",
-   "NUBS574109",
-   "NUBT284638",
-   "NUBT557400",
-   "NUBT568956",
-   "NUBT311303",
-   "NUBT449606",
-   "NUBT011421",
-   "NUBS619679",
-   "NUBS616432",
-   "NUBS972532",
-   "NUBT384530",
-   "csgj",
-   "0clw",
-   "5iqn",
-   "ALXV",
-   "NUBR318931",
-   "NUBT201811",
-   "NUBT338300",
-   "NUBT260841",
-   "cctb",
-   "cfrl",
-   "ckdp",
-   "cwph",
-   "NUAI444711",
-   "NUAW691539",
-   "NUAW692592",
-   "NUBR152104",
-   "clhg",
-   "clhu",
-   "5ayu",
-   "5gyp",
-   "cpnf",
-   "cplj",
-   "csde",
-   "csih",
-   "BXJG",
-   "BYGP",
-   "CBGT",
-   "NUAD489515",
-   "NUAC956199",
-   "NUAI171912",
-   "NUAU114328",
-   "NUAU349270",
-   "NUAW681268",
-   "NUAU346927",
-   "NUAU343629",
-   "NUAW480049",
-   "NUAW690044",
-   "NUBQ877853",
-   "NUAD256555",
-   "NUAU563773",
-   "NUBJ005828",
-   "NUBQ817806",
-   "NUBS366494",
-   "NUBS693705",
-   "NUBR256598",
-   "NUBT476123",
-   "NUBR370524",
-   "NUBT302129",
-   "NUBS718457",
-   "NUBS441777",
-   "NUBT621936",
-   "NUBT419377",
-   "NUBR041905",
-   "NUBQ910431",
-   "NUBS504906",
-   "NUBT637168",
-   "NUBT377345",
-   "NUBS800068",
-   "BADY",
-   "5cdi",
-   "cakp",
-   "0bqq",
-   "ctva",
-   "BTSH",
-   "3cge",
-   "1xwt",
-   "dfcn",
-   "NUBQ997999",
-   "NUBR215245",
-   "NUBR365522",
-   "5abc"
-   );
+$cbsr = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
 
 function main() {
    $cbsr = $GLOBALS['cbsr'];
@@ -327,15 +28,22 @@ and study_pv_attr.label='Worksheet'
    $headings = array('patient_nr', 'visit_nr', 'date_taken', 'date_received',
                      'worksheet', 'fnum', 'rack', 'box', 'cell', 'inventory_id');
 
+   if ($GLOBALS['argc'] != 2) {
+      die("invalid arguments");
+   }
+
+   $csvfile = $GLOBALS['argv'][1];
+   $inventory_ids = getInventoryIds($csvfile);
    $chr2patientNr = getPatientNrs();
-   $chr2visitNr = getVisitNrs();
+   $chr2visitNr = getVisitNrs($inventory_ids);
 
    mysql_connect("localhost", "dummy", "ozzy498") or die(mysql_error());
    mysql_select_db("biobank2") or die(mysql_error());
 
    echo implode(",", $headings), "\n";
 
-   foreach ($GLOBALS['inventory_id'] as $inv_id) {
+
+   foreach ($inventory_ids as $inv_id) {
       $query = str_replace(array('{inv_id}'), array($inv_id), $default_query);
       $result = mysql_query($query)  or die(mysql_error());
 
@@ -343,7 +51,7 @@ and study_pv_attr.label='Worksheet'
          //print_r($row);
 
          $label = $row['LABEL'];
-         if (strpos('Sent Samples', $label) !== false) {
+         if (strpos($label, 'Sent Samples') !== FALSE) {
             $label = str_replace('Sent Samples', '99', $label);
          }
 
@@ -375,6 +83,24 @@ and study_pv_attr.label='Worksheet'
    }
    mysql_close();
 }
+
+function getInventoryIds($filename) {
+   if (($handle = fopen($filename, "r")) === FALSE) return null;
+
+   $inventory_ids = array();
+   $row = 1;
+   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+      $num = count($data);
+      if ($num != 10) {
+         throw new Exception("invalid field count at line $row");
+      }
+      $inventory_ids[] = $data[9];
+      $row++;
+   }
+   fclose($handle);
+   return $inventory_ids;
+}
+
 
 function getFrz2OldLabel($label) {
    $cbsr = $GLOBALS['cbsr'];
@@ -483,12 +209,12 @@ function getPatientNrs() {
    return $chr2patientNr;
 }
 
-function getVisitNrs() {
+function getVisitNrs($inventory_ids) {
    mysql_connect("localhost", "dummy", "ozzy498") or die(mysql_error());
    mysql_select_db("bbpdb") or die(mysql_error());
 
    $chr2visitNr = array();
-   foreach ($GLOBALS['inventory_id'] as $inv_id) {
+   foreach ($inventory_ids as $inv_id) {
       $result = mysql_query("
             select visit_nr, inventory_id from freezer
             where inventory_id like binary '$inv_id'")
