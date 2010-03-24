7234aa51b57eac144557d3d4f66200ef29140c32
Nelson Loyola <loyola@cs.ualberta.ca>
2010-03-24 16:58:46 -0600

    ruby scripts

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/ReportsEditor.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/ReportsEditor.java
@@ -66,12 +66,12 @@ import edu.ualberta.med.biobank.common.reports.FreezerCAliquots;
 import edu.ualberta.med.biobank.common.reports.FreezerDAliquots;
 import edu.ualberta.med.biobank.common.reports.FreezerSAliquots;
 import edu.ualberta.med.biobank.common.reports.FvLPatientVisits;
-import edu.ualberta.med.biobank.common.reports.PVsByStudy;
 import edu.ualberta.med.biobank.common.reports.NewPVsByStudyClinic;
-import edu.ualberta.med.biobank.common.reports.PsByStudy;
 import edu.ualberta.med.biobank.common.reports.NewPsByStudyClinic;
+import edu.ualberta.med.biobank.common.reports.PVsByStudy;
 import edu.ualberta.med.biobank.common.reports.PatientVisitSummary;
 import edu.ualberta.med.biobank.common.reports.PatientWBC;
+import edu.ualberta.med.biobank.common.reports.PsByStudy;
 import edu.ualberta.med.biobank.common.reports.QACabinetAliquots;
 import edu.ualberta.med.biobank.common.reports.QAFreezerAliquots;
 import edu.ualberta.med.biobank.common.reports.QueryObject;
@@ -128,8 +128,8 @@ public class ReportsEditor extends EditorPart {
         aMap.put(FvLPatientVisits.class, new int[] { 100, 100, 100, 100 });
         aMap.put(NewPsByStudyClinic.class, new int[] { 100, 100, 100, 100 });
         aMap.put(NewPVsByStudyClinic.class, new int[] { 100, 100, 100, 100 });
-        aMap.put(NewPsByStudy.class, new int[] { 100, 100, 100 });
-        aMap.put(NewPVsByStudy.class, new int[] { 100, 100, 100 });
+        aMap.put(PsByStudy.class, new int[] { 100, 100, 100 });
+        aMap.put(PVsByStudy.class, new int[] { 100, 100, 100 });
         aMap.put(PatientVisitSummary.class, new int[] { 100, 100, 100, 100,
             100, 100, 100, 100, 100 });
         aMap.put(PatientWBC.class, new int[] { 100, 100, 100, 100, 100 });
