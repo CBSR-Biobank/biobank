a11d69f2a19988f4d67b6ef48903d69cf6bf4ab3
Nelson Loyola <loyola@cs.ualberta.ca>
2010-04-08 20:12:06 -0600

    refactoring date and time widget

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/DateTimeWidget.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/DateTimeWidget.java
@@ -1,16 +1,21 @@
 package edu.ualberta.med.biobank.widgets;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-import org.eclipse.nebula.widgets.cdatetime.CDT;
-import org.eclipse.nebula.widgets.cdatetime.CDateTime;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-
-import edu.ualberta.med.biobank.common.formatters.DateFormatter;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * Wrapper around Nebula's CDateTime widget.
@@ -25,7 +30,9 @@ import edu.ualberta.med.biobank.common.formatters.DateFormatter;
  */
 public class DateTimeWidget extends BiobankWidget {
 
-    CDateTime cdt;
+    private DateTime dateEntry;
+
+    private DateTime timeEntry;
 
     /**
      * Allow date to be null.
@@ -33,7 +40,7 @@ public class DateTimeWidget extends BiobankWidget {
     public DateTimeWidget(Composite parent, int style, Date date) {
         super(parent, style);
 
-        GridLayout layout = new GridLayout(1, false);
+        GridLayout layout = new GridLayout(3, false);
         layout.horizontalSpacing = 0;
         layout.marginWidth = 0;
         layout.verticalSpacing = 0;
@@ -41,33 +48,58 @@ public class DateTimeWidget extends BiobankWidget {
 
         setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
 
-        cdt = new CDateTime(this, CDT.BORDER | CDT.COMPACT | CDT.DROP_DOWN
-            | CDT.DATE_LONG | CDT.TIME_MEDIUM);
-        cdt.setPattern(DateFormatter.DATE_TIME_FORMAT);
-        cdt.setSelection(date);
-        cdt.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+        dateEntry = new DateTime(this, SWT.DATE);
+        Button open = new Button(this, SWT.PUSH);
+        timeEntry = new DateTime(this, SWT.TIME | SWT.SHORT);
+
+        open.setText("Select Date");
+        open.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                final Shell dialog = new Shell(PlatformUI.getWorkbench()
+                    .getActiveWorkbenchWindow().getShell(), SWT.DIALOG_TRIM);
+                dialog.setLayout(new GridLayout(3, false));
+
+                final DateTime calendar = new DateTime(dialog, SWT.CALENDAR
+                    | SWT.BORDER);
+
+                new Label(dialog, SWT.NONE);
+                Button ok = new Button(dialog, SWT.PUSH);
+                ok.setText("OK");
+                ok.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+                    false));
+                ok.addSelectionListener(new SelectionAdapter() {
+                    public void widgetSelected(SelectionEvent e) {
+                        dialog.close();
+                    }
+                });
+                dialog.setDefaultButton(ok);
+                dialog.pack();
+                dialog.open();
+            }
+        });
+
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+
+        dateEntry.setDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal
+            .get(Calendar.DAY_OF_MONTH));
+        timeEntry.setTime(cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), cal
+            .get(Calendar.SECOND));
     }
 
     public void addSelectionListener(SelectionListener listener) {
-        cdt.addSelectionListener(listener);
+        dateEntry.addSelectionListener(listener);
+        timeEntry.addSelectionListener(listener);
     }
 
     public String getText() {
-        String text = cdt.getText();
-        if (text.equals("<choose date>"))
-            return null;
-        return text;
+        return null;
     }
 
     public Date getDate() {
-        String text = cdt.getText();
-        if (text.equals("<choose date>"))
-            return null;
-
-        return DateFormatter.parseToDateTime(cdt.getText());
+        return null;
     }
 
     public void setDate(Date date) {
-        cdt.setSelection(date);
     }
 }
