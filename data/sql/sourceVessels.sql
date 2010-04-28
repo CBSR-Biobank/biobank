59b32679acc3c6a1539108ae055177b9618d2f62
Nelson Loyola <loyola@cs.ualberta.ca>
2010-04-28 11:31:12 -0600

    new source vessel

--- data/sql/sourceVessels.sql
+++ data/sql/sourceVessels.sql
@@ -33,6 +33,7 @@ INSERT INTO `SOURCE_VESSEL` (ID, NAME) VALUES
 (31, "ERCIN Urine processing pallet"),
 (32, "AHFEM processing pallet "),
 (33, "8.5ml P100 orange top tube"),
-(34, "9ml CPDA yellow top tube");
+(34, "9ml CPDA yellow top tube"),
+(35, "10ml green top Lithium Heparin tube");
 UNLOCK TABLES;
 
