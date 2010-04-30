b51a56aa3aeaf0f0f567ea998accebfa8911ff8a
Nelson Loyola <loyola@cs.ualberta.ca>
2010-04-30 15:27:16 -0600

    added activity status to aliquot class

--- data/sql/activityStatus.sql
+++ data/sql/activityStatus.sql
@@ -2,5 +2,6 @@ LOCK TABLES `ACTIVITY_STATUS` WRITE;
 INSERT INTO `ACTIVITY_STATUS` (ID, NAME) VALUES
 ( 1, "Active"),
 ( 2, "Closed"),
-( 3, "Disabled");
+( 3, "Disabled"),
+( 4, "Flagged");
 UNLOCK TABLES;
