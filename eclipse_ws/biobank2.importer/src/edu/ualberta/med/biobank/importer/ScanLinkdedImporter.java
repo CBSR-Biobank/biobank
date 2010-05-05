376500e3f2a74cc57325e5f5492b40ea57e4ce03
Nelson Loyola <loyola@cs.ualberta.ca>
2010-05-05 16:21:35 -0600

    working on importing aliquots from freezer_link table

--- eclipse_ws/biobank2.importer/src/edu/ualberta/med/biobank/importer/ScanLinkdedImporter.java
+++ eclipse_ws/biobank2.importer/src/edu/ualberta/med/biobank/importer/ScanLinkdedImporter.java
@@ -3,11 +3,14 @@ package edu.ualberta.med.biobank.importer;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.springframework.util.Assert;
 
 import edu.ualberta.med.biobank.common.wrappers.ActivityStatusWrapper;
 import edu.ualberta.med.biobank.common.wrappers.AliquotWrapper;
@@ -18,6 +21,21 @@ public class ScanLinkdedImporter {
 
     private static final Logger logger = Logger
         .getLogger(ScanLinkdedImporter.class.getName());
+
+    private static List<String> invIdNotImported;
+    static {
+        List<String> aList = new ArrayList<String>();
+        aList.add("NUBB681914");
+        aList.add("NUBB681950");
+        aList.add("NUBB682490");
+        aList.add("NUBB682506");
+        aList.add("NUBB685789");
+        aList.add("NUBB685798");
+        aList.add("NUBB691489");
+        aList.add("NUBB692008");
+
+        invIdNotImported = Collections.unmodifiableList(aList);
+    };
 
     private static Map<String, String> dupInvIdErrFix;
     static {
@@ -66,6 +84,7 @@ public class ScanLinkdedImporter {
 
     public ScanLinkdedImporter(WritableApplicationService appService,
         Connection con, final SiteWrapper site) throws Exception {
+        this.appService = appService;
         this.con = con;
         this.site = site;
         sampleImportCount = 0;
@@ -75,13 +94,17 @@ public class ScanLinkdedImporter {
     public void doImport() throws Exception {
         logger.info("importing scan assigned only aliquots ...");
 
-        String qryPart = "from freezer_link "
-            + "join sample_list on sample_list.sample_nr=freezer_link.sample_nr "
-            + "join patient_visit on patient_visit.visit_nr=freezer_link.visit_nr "
+        // String freezerLinkTable = "freezer_link";
+        String freezerLinkTable = "freezer_link_multiple";
+
+        String qryPart = "from " + freezerLinkTable
+            + " join sample_list on sample_list.sample_nr=" + freezerLinkTable
+            + ".sample_nr " + "join patient_visit on patient_visit.visit_nr="
+            + freezerLinkTable + ".visit_nr "
             + "join patient on patient.patient_nr=patient_visit.patient_nr "
             + "join study_list on study_list.study_nr=patient_visit.study_nr "
-            + "left join freezer on freezer.inventory_id=freezer_link.inventory_id "
-            + "where fnum is null order by link_date desc";
+            + "left join freezer on freezer.inventory_id=" + freezerLinkTable
+            + ".inventory_id where fnum is null order by link_date desc";
 
         Statement s = con.createStatement();
         s.execute("select count(*) " + qryPart);
@@ -92,8 +115,8 @@ public class ScanLinkdedImporter {
         s.execute("select study_name_short,dec_chr_nr,"
             + "patient_visit.bb2_pv_id,"
             + "patient_visit.date_received,patient_visit.date_taken,"
-            + "sample_name_short,freezer_link.link_date,"
-            + "freezer_link.inventory_id " + qryPart);
+            + "sample_name_short," + freezerLinkTable + ".link_date,"
+            + freezerLinkTable + ".inventory_id " + qryPart);
 
         rs = s.getResultSet();
         if (rs == null) {
@@ -120,26 +143,60 @@ public class ScanLinkdedImporter {
             linkDateStr = rs.getString(7);
             inventoryId = rs.getString(8);
 
-            String dupInvIdErr = dupInvIdErrFix.get(inventoryId);
-            if ((dupInvIdErr != null) && !dupInvIdErr.equals(patientNr)) {
-                // this is an invalid aliquot
+            // if (inventoryId.equals("NUAW522752")) {
+            if (inventoryId.equals("NUAW522752")) {
+                System.out.println("here");
+            }
+
+            if (invIdNotImported.contains(inventoryId)) {
+                logger.info("duplicate kit, not importing inventory id: "
+                    + "inventoryId/" + inventoryId);
                 continue;
             }
 
-            AliquotWrapper aliquot = Importer.createAliquot(site,
-                studyNameShort, patientNr, visitId, dateProcessedStr,
-                dateTakenStr, inventoryId, sampleTypeNameShort, linkDateStr);
+            AliquotWrapper aliquot = isDuplicateInventoryId(inventoryId,
+                visitId, patientNr, sampleTypeNameShort);
 
-            if (aliquot == null) {
-                return;
+            if (aliquot != null) {
+                aliquot.setActivityStatus(ActivityStatusWrapper
+                    .getActivityStatus(appService, "Flagged"));
+                logger.info("flagging duplicate inventory id: "
+                    + "inventoryId/" + inventoryId + " visitId/" + visitId
+                    + " patientNr/" + patientNr + " sampleTypeNameShort/"
+                    + sampleTypeNameShort);
+
+                aliquot.persist();
+                continue;
             }
 
+            String dupInvIdErr = dupInvIdErrFix.get(inventoryId);
+            if ((dupInvIdErr != null) && !dupInvIdErr.equals(patientNr)) {
+                logger.info("ignoring duplicate inventory id: "
+                    + "inventoryId/" + inventoryId + " visitId/" + visitId
+                    + " patientNr/" + patientNr + " sampleTypeNameShort/"
+                    + sampleTypeNameShort);
+                continue;
+            }
+
+            aliquot = Importer.createAliquot(site, studyNameShort, patientNr,
+                visitId, dateProcessedStr, dateTakenStr, inventoryId,
+                sampleTypeNameShort, linkDateStr);
+
+            if (aliquot == null) {
+                logger
+                    .error("could not add aliquot: inventoryId/" + inventoryId
+                        + " visitId/" + visitId + " patientNr/" + patientNr
+                        + " sampleTypeNameShort/" + sampleTypeNameShort);
+                continue;
+            }
             if (dupInvIdErr != null) {
                 aliquot.setActivityStatus(ActivityStatusWrapper
                     .getActivityStatus(appService, "Flagged"));
+                logger.info("flagging duplicate inventory id: "
+                    + "inventoryId/" + inventoryId + " visitId/" + visitId
+                    + " patientNr/" + patientNr + " sampleTypeNameShort/"
+                    + sampleTypeNameShort);
             }
-
-            aliquot.persist();
 
             ++count;
 
@@ -147,7 +204,42 @@ public class ScanLinkdedImporter {
                 "importing scan linked only aliquot %s (%d/%d)", inventoryId,
                 count, numSamples));
             ++sampleImportCount;
+
+            aliquot.persist();
         }
+    }
+
+    private AliquotWrapper isDuplicateInventoryId(String inventoryId,
+        int visitId, String patientNr, String sampleTypeNameShort)
+        throws Exception {
+        List<AliquotWrapper> aliquots = AliquotWrapper.getAliquotsInSite(
+            appService, inventoryId, site);
+        if (aliquots.size() > 0) {
+            // check if this is a duplicate
+            for (AliquotWrapper a : aliquots) {
+                if (a.getPatientVisit().getId().equals(visitId)
+                    && a.getPatientVisit().getPatient().getPnumber().equals(
+                        patientNr)
+                    && a.getSampleType().getNameShort().equals(
+                        sampleTypeNameShort)) {
+                    return a;
+                }
+            }
+            logger.error("duplicate inventory id found: inventoryId/"
+                + inventoryId + " visitId/" + visitId + " patientNr/"
+                + patientNr + " sampleTypeNameShort/" + sampleTypeNameShort);
+        }
+        return null;
+    }
+
+    private AliquotWrapper getAliquot(String inventoryId) throws Exception {
+        List<AliquotWrapper> aliquots = AliquotWrapper.getAliquotsInSite(
+            appService, inventoryId, site);
+        Assert.isTrue(aliquots.size() <= 1);
+        if (aliquots.size() == 1) {
+            return aliquots.get(0);
+        }
+        return null;
     }
 
     public int getSamplesImported() {
