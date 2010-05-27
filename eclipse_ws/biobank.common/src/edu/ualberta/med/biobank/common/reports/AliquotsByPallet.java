831aa409a682c24f3ecb0aa7dc5e7d1da9e0900a
Aaron <aaron.young@ualberta.ca>
2010-05-27 11:38:18 -0600

    AliquotsByPallet.java now sorts by location (and has it in the first column)

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/reports/AliquotsByPallet.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/reports/AliquotsByPallet.java
@@ -1,6 +1,8 @@
 package edu.ualberta.med.biobank.common.reports;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import edu.ualberta.med.biobank.common.LabelingScheme;
@@ -14,20 +16,17 @@ public class AliquotsByPallet extends QueryObject {
     protected static final String NAME = "Aliquots by Pallet";
 
     public AliquotsByPallet(String op, Integer siteId) {
-        super(
-            "Given a pallet label, generate a list of aliquots.",
-            "select s from "
-                + Aliquot.class.getName()
-                + " s where s.patientVisit.patient.study.site "
-                + op
-                + siteId
+        super("Given a pallet label, generate a list of aliquots.",
+            "select s from " + Aliquot.class.getName()
+                + " s where s.patientVisit.patient.study.site " + op + siteId
                 + " and s.aliquotPosition.container.id "
                 + "in (select path1.container.id from "
+                + ContainerPath.class.getName() + " as path1, "
                 + ContainerPath.class.getName()
-                + " as path1, "
-                + ContainerPath.class.getName()
-                + " as path2 where locate(path2.path, path1.path) > 0 and path2.container.containerType.nameShort like ?) and s.aliquotPosition.container.label = ?",
-            new String[] { "Patient", "Inventory ID", "Location", "Type" });
+                + " as path2 where locate(path2.path, path1.path) > 0 "
+                + "and path2.container.containerType.nameShort like ?) "
+                + "and s.aliquotPosition.container.label = ?", new String[] {
+                "Location", "Inventory ID", "Patient", "Type" });
         addOption("Top Container Type", String.class, "");
         addOption("Pallet Label", String.class, "");
     }
@@ -47,9 +46,17 @@ public class AliquotsByPallet extends QueryObject {
                 new RowColPos(aliquotPosition.getRow(), aliquotPosition
                     .getCol()), aliquotPosition.getContainer()
                     .getContainerType());
-            modifiedResults.add(new Object[] { pnumber, inventoryId,
-                containerLabel + aliquotLabel, stName });
+            modifiedResults.add(new Object[] { containerLabel + aliquotLabel,
+                inventoryId, pnumber, stName });
         }
+        Collections.sort(modifiedResults, new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Object[] castOb1 = ((Object[]) o1);
+                Object[] castOb2 = ((Object[]) o2);
+                return castOb1[0].toString().compareTo(castOb2[0].toString());
+            }
+        });
         return modifiedResults;
     }
 
