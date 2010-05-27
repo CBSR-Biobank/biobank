e717f05b56291433389c1ac3a6eb5ce345ef0b39
Delphine Degris-Dard <degrisda@ualberta.ca>
2010-05-27 14:19:27 -0600

    add sql script to add the protection element of site CBSR with ID (only for development, in case the schemaSecurity script has been launch)

---
+++ security-logs/addCBSRSiteSecurity.sql
@@ -0,0 +1,3 @@
+INSERT INTO `csm_pg_pe` VALUES (545,11,52,'0000-00-00');
+INSERT INTO `csm_protection_element` VALUES (52,'edu.ualberta.med.biobank.model.Site/ID=1/Name=CBSR','','edu.ualberta.med.biobank.model.Site','id','1','',2,'2010-05-27');
+
