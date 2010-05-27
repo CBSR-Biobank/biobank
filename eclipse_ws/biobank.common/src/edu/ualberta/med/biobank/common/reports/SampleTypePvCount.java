38754f27a3e791bf6e4077e9717b4a87a88d41c5
Nelson Loyola <loyola@cs.ualberta.ca>
2010-05-27 14:12:46 -0600

    fix to SampleTypePvCount report

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/reports/SampleTypePvCount.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/reports/SampleTypePvCount.java
@@ -11,8 +11,12 @@ public class SampleTypePvCount extends QueryObject {
             "Lists the total number of each sample type per patient visit for a specified study.",
             "Select pv.patient.pnumber, pv.dateProcessed, pv.dateDrawn,  Alias.sampleType.name, count(*) from "
                 + PatientVisit.class.getName()
-                + " as pv join pv.aliquotCollection as Alias where pv.patient.study.nameShort LIKE ? "
-                + " and Alias.aliquotPosition not in (from edu.ualberta.med.biobank.model.AliquotPosition a where a.container.label not like '%SS%') and Alias.patientVisit.patient.study.site "
+                + " as pv join pv.aliquotCollection as Alias "
+                + "where pv.patient.study.nameShort LIKE ? "
+                + " and Alias.aliquotPosition not in "
+                + "(from edu.ualberta.med.biobank.model.AliquotPosition a "
+                + "where a.container.label like '%SS%') "
+                + "and Alias.patientVisit.patient.study.site "
                 + op
                 + siteId
                 + " GROUP BY pv, Alias.sampleType ORDER BY pv.patient.pnumber, pv.dateProcessed",
