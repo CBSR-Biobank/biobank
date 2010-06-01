292c4c2a7803cb6ab0419831fd3c8bf9e4c90560
Aaron <aaron.young@ualberta.ca>
2010-06-01 10:38:32 -0600

    merge conflicts

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/reports/AliquotRequest.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/reports/AliquotRequest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import edu.ualberta.med.biobank.common.BiobankCheckException;
 import edu.ualberta.med.biobank.common.wrappers.AliquotWrapper;
 import edu.ualberta.med.biobank.model.Aliquot;
+import edu.ualberta.med.biobank.model.AliquotPosition;
 import gov.nih.nci.system.applicationservice.ApplicationException;
 import gov.nih.nci.system.applicationservice.WritableApplicationService;
 import gov.nih.nci.system.query.hibernate.HQLCriteria;
