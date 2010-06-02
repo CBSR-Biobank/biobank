1fca623ce137bd4a4830c0687fe48d4e76fa274e
Nelson Loyola <loyola@cs.ualberta.ca>
2010-06-02 15:55:34 -0600

    working on uml model parser

--- eclipse_ws/biobank2.strfields/src/edu/ualberta/med/biobank/strfields/DataModelExtractor.java
+++ eclipse_ws/biobank2.strfields/src/edu/ualberta/med/biobank/strfields/DataModelExtractor.java
@@ -1,6 +1,9 @@
 package edu.ualberta.med.biobank.strfields;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -12,10 +15,27 @@ import org.w3c.dom.NodeList;
 
 public class DataModelExtractor {
 
+    private class DataModelClass {
+        @SuppressWarnings("unused")
+        String name;
+
+        Map<String, String> attrMap;
+
+        public DataModelClass(String name) {
+            this.name = name;
+            attrMap = new HashMap<String, String>();
+        }
+    }
+
     private static DataModelExtractor instance = null;
 
-    private DataModelExtractor() {
+    private Map<String, String> modelDataTypeMap;
 
+    private Map<String, DataModelClass> dataModelClassMap;
+
+    private DataModelExtractor() {
+        modelDataTypeMap = new HashMap<String, String>();
+        dataModelClassMap = new HashMap<String, DataModelClass>();
     }
 
     public static DataModelExtractor getInstance() {
@@ -25,43 +45,182 @@ public class DataModelExtractor {
         return instance;
     }
 
-    public void getDataModel(String modelFileName) throws Exception {
+    public void getModel(String modelFileName) throws Exception {
         File file = new File(modelFileName);
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(file);
         doc.getDocumentElement().normalize();
-        System.out.println("Root element "
-            + doc.getDocumentElement().getNodeName());
-        NodeList pkgNodeLst = doc.getElementsByTagName("UML:Package");
 
-        for (int s = 0; s < pkgNodeLst.getLength(); s++) {
-            Element el = (Element) pkgNodeLst.item(s);
-            if (!el.getAttribute("name").equals("Data Model"))
+        NodeList nodeLst = doc.getElementsByTagName("UML:Model");
+        for (int i = 0, n = nodeLst.getLength(); i < n; i++) {
+            Node node = nodeLst.item(i);
+            if (!((Element) node).getAttribute("name").equals("Model"))
                 continue;
 
-            NodeList dmNodeLst = el.getChildNodes();
-
-            for (int i = 0, n = dmNodeLst.getLength(); i < n; ++i) {
-                Node dmChildren = (Node) dmNodeLst.item(i);
-                if (!dmNodeLst.item(i).getNodeName().equals(
-                    "UML:Namespace.ownedElement"))
-                    continue;
-
-                NodeList opNodeList = dmChildren.getChildNodes();
-
-                for (int j = 0, m = opNodeList.getLength(); j < m; ++j) {
-                    Node opNodeChildList = (Node) opNodeList.item(j);
-                    if (!opNodeChildList.getNodeName().equals("UML:Class"))
-                        continue;
-
-                    System.out.println("node " + opNodeChildList.getNodeName()
-                        + " "
-                        + ((Element) opNodeChildList).getAttribute("name"));
-                }
-
-            }
+            getModel(node);
         }
 
+        nodeLst = doc.getElementsByTagName("UML:Package");
+        for (int i = 0, n = nodeLst.getLength(); i < n; i++) {
+            Node node = nodeLst.item(i);
+            if (!((Element) node).getAttribute("name").equals("Data Model"))
+                continue;
+
+            getDataModel(node);
+        }
+    }
+
+    public Set<String> getDmClassSet() throws Exception {
+        if (dataModelClassMap.size() == 0) {
+            throw new Exception("UML file not parsed yet");
+        }
+        return dataModelClassMap.keySet();
+    }
+
+    public Map<String, String> getDmClassAttrMap(String className)
+        throws Exception {
+        if (dataModelClassMap.size() == 0) {
+            throw new Exception("UML file not parsed yet");
+        }
+
+        DataModelClass dmClass = dataModelClassMap.get(className);
+        if (dmClass == null) {
+            throw new Exception("invalid data model class name: " + className);
+        }
+        return dmClass.attrMap;
+    }
+
+    private void getModel(Node node) throws Exception {
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals(
+                "UML:Namespace.ownedElement"))
+                continue;
+
+            getModelOwnedElement(childNode);
+        }
+    }
+
+    private void getModelOwnedElement(Node node) throws Exception {
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals("UML:DataType"))
+                continue;
+
+            getModelDataType(childNode);
+        }
+    }
+
+    private void getModelDataType(Node node) {
+        modelDataTypeMap.put(((Element) node).getAttribute("xmi.id"),
+            ((Element) node).getAttribute("name"));
+
+    }
+
+    private void getDataModel(Node node) throws Exception {
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals(
+                "UML:Namespace.ownedElement"))
+                continue;
+
+            getDmOwnedElement(childNode);
+        }
+    }
+
+    private void getDmOwnedElement(Node node) throws Exception {
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals("UML:Class"))
+                continue;
+
+            getDmClass(childNode);
+        }
+    }
+
+    private void getDmClass(Node node) throws Exception {
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals(
+                "UML:Classifier.feature"))
+                continue;
+
+            getDmClassifierFeature(childNode);
+        }
+    }
+
+    private void getDmClassifierFeature(Node node) throws Exception {
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals("UML:Attribute"))
+                continue;
+
+            getDmClassAttribute(childNode);
+        }
+    }
+
+    private void getDmClassAttribute(Node node) throws Exception {
+        Node grandParent = node.getParentNode().getParentNode();
+        if (grandParent == null) {
+            throw new Exception(
+                "UML:Attribute node does not have a grandparent");
+        }
+
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals(
+                "UML:StructuralFeature.type"))
+                continue;
+
+            getDmClassAttrStructFeature(childNode);
+        }
+    }
+
+    private void getDmClassAttrStructFeature(Node node) throws Exception {
+        Node classParent = node.getParentNode().getParentNode().getParentNode();
+        if (classParent == null) {
+            throw new Exception(
+                "UML:Attribute node does not have a class parent");
+        }
+
+        NodeList childNodeLst = node.getChildNodes();
+
+        for (int i = 0, n = childNodeLst.getLength(); i < n; ++i) {
+            Node childNode = (Node) childNodeLst.item(i);
+            if (!childNodeLst.item(i).getNodeName().equals("UML:DataType"))
+                continue;
+
+            String className = ((Element) classParent).getAttribute("name");
+            String attrName = ((Element) node.getParentNode())
+                .getAttribute("name");
+            String idref = ((Element) childNode).getAttribute("xmi.idref");
+            String modelDataType = modelDataTypeMap.get(idref);
+            if (modelDataType == null) {
+                throw new Exception("could not find data type for " + className
+                    + "." + attrName);
+            }
+
+            DataModelClass dmClass = dataModelClassMap.get(className);
+            if (dmClass == null) {
+                dmClass = new DataModelClass(className);
+                dataModelClassMap.put(className, dmClass);
+            }
+            dmClass.attrMap.put(attrName, modelDataType);
+        }
     }
 }
--- eclipse_ws/biobank2.strfields/src/edu/ualberta/med/biobank/strfields/StrFields.java
+++ eclipse_ws/biobank2.strfields/src/edu/ualberta/med/biobank/strfields/StrFields.java
@@ -1,19 +1,206 @@
 package edu.ualberta.med.biobank.strfields;
 
+import jargs.gnu.CmdLineParser;
+import jargs.gnu.CmdLineParser.Option;
+import jargs.gnu.CmdLineParser.OptionException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StrFields {
 
-    public static void main(String argv[]) {
-        if (argv.length != 1) {
-            System.out.println("missing parameter");
+    private static String USAGE = "Usage: strfields [options] UMLFILE HBMDIR OUTPATH\n\n"
+        + "Options\n" + "  -v, --verbose    Shows verbose output";
+
+    private static String HBM_FILE_EXTENSION = ".hbm.xml";
+
+    private static Pattern HBM_STRING_ATTR = Pattern
+        .compile("<.*type=\"string\".*column=\"([^\"]*)\"/");
+
+    private static Pattern HBM_STRING_LENGTH_ATTR = Pattern
+        .compile("length=\"(\\d+)\"");
+
+    private AppArgs appArgs;
+
+    public StrFields(AppArgs appArgs) {
+        this.appArgs = appArgs;
+
+        try {
+            DataModelExtractor.getInstance().getModel(appArgs.modelFileName);
+
+            if (appArgs.verbose) {
+                for (String className : DataModelExtractor.getInstance()
+                    .getDmClassSet()) {
+                    Map<String, String> attrMap = DataModelExtractor
+                        .getInstance().getDmClassAttrMap(className);
+                    for (String attrName : attrMap.keySet()) {
+                        String type = attrMap.get(attrName);
+                        if (!type.startsWith("VARCHAR"))
+                            continue;
+
+                        System.out.println(className + "." + attrName + ": "
+                            + type);
+                    }
+                }
+            }
+
+            for (String file : getHbmFiles(appArgs.hbmDir)) {
+                updateHbmFile(file);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * Returns all "*.hbm.xml" files found in directory hbmDir
+     */
+    private List<String> getHbmFiles(String hbmDir) {
+        File dir = new File(hbmDir);
+        String[] files = dir.list();
+        if (files == null) {
+            System.out.println("Error: no files found in directory " + hbmDir);
             System.exit(-1);
         }
 
+        List<String> hbmFiles = new ArrayList<String>();
+        for (String file : files) {
+            if (!file.endsWith(HBM_FILE_EXTENSION))
+                continue;
+            hbmFiles.add(file);
+        }
+        return hbmFiles;
+    }
+
+    private void updateHbmFile(String hbmFileName) throws Exception {
+        String className = toTitleCase(hbmFileName.replace(HBM_FILE_EXTENSION,
+            ""));
+
         try {
-            DataModelExtractor.getInstance().getDataModel(argv[0]);
+            Map<String, String> attrMap = DataModelExtractor.getInstance()
+                .getDmClassAttrMap(className);
+
+            File inFile = new File(appArgs.hbmDir + "/" + hbmFileName);
+            File outFile = File.createTempFile(className, HBM_FILE_EXTENSION);
+
+            BufferedReader reader = new BufferedReader(new FileReader(inFile));
+            // BufferedWriter writer = new BufferedWriter(new
+            // FileWriter(outFile));
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                Matcher stringAttrMatcher = HBM_STRING_ATTR.matcher(line);
+                if (stringAttrMatcher.matches()) {
+                    System.out.println(line);
+                }
+
+                // writer.write(line);
+                // writer.newLine();
+                line = reader.readLine();
+            }
+
         } catch (Exception e) {
-            e.printStackTrace();
+            if (appArgs.verbose) {
+                System.out.println("class " + className
+                    + " does not have a corresponding HBM file");
+            }
         }
 
     }
 
+    public static void main(String argv[]) {
+        try {
+            new StrFields(parseCommandLine(argv));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * Parses the command line arguments and returns them in an AppArgs object.
+     */
+    private static AppArgs parseCommandLine(String argv[])
+        throws URISyntaxException {
+        AppArgs appArgs = new AppArgs();
+
+        CmdLineParser parser = new CmdLineParser();
+        Option verboseOpt = parser.addBooleanOption('v', "verbose");
+
+        try {
+            parser.parse(argv);
+        } catch (OptionException e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+
+        Boolean verbose = (Boolean) parser.getOptionValue(verboseOpt);
+        if (verbose != null) {
+            appArgs.verbose = verbose.booleanValue();
+        }
+
+        String[] args = parser.getRemainingArgs();
+        if (args.length != 3) {
+            System.out.println("Error: invalid arguments\n" + USAGE);
+            System.exit(-1);
+        }
+
+        appArgs.modelFileName = args[0];
+        appArgs.hbmDir = args[1];
+        appArgs.outDir = args[2];
+
+        return appArgs;
+    }
+
+    @SuppressWarnings("unused")
+    private String convertToCamelCase(String str) {
+        StringBuffer sb = new StringBuffer();
+        String[] splitStr = str.split("_");
+        boolean firstTime = true;
+        for (String temp : splitStr) {
+            if (firstTime) {
+                sb.append(temp.toLowerCase());
+                firstTime = false;
+            } else {
+                sb.append(Character.toUpperCase(temp.charAt(0)));
+                sb.append(temp.substring(1).toLowerCase());
+            }
+        }
+        return sb.toString();
+    }
+
+    private String toTitleCase(String str) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0, n = str.length(); i < n; ++i) {
+            if (i == 0) {
+                sb.append(str.charAt(0));
+                continue;
+            }
+
+            char ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                sb.append("_" + ch);
+            } else {
+                sb.append(Character.toUpperCase(ch));
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
+class AppArgs {
+    boolean verbose = false;
+    String modelFileName = null;
+    String hbmDir = null;
+    String outDir = null;
 }
