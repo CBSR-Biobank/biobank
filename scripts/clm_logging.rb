a6c576f714b9c4e069f4e7562f6b12dff05c495f
Nelson Loyola <loyola@cs.ualberta.ca>
2010-06-16 08:47:55 -0600

    working on importing clm logging

--- scripts/clm_logging.rb
+++ scripts/clm_logging.rb
@@ -20,6 +20,20 @@ where attribute='inventoryId'
 QUERY_END
 
 
+  def initialize
+    unless ARGV.length == 1
+      print USAGE.gsub("{script_name}", File.basename(__FILE__))
+      exit
+    end
+
+    host = ARGV[0]
+    getDbConnection("biobank2", host)
+    res = @dbh.query(QUERY)
+    res.each_hash do |row|
+      print row['USERNAME'], ", ", row['CURRENT_VALUE'], ", ", Time.at(row['CREATED_ON'].to_f / 1000.0), " - (", row['CREATED_ON'], "), ", row['OPERATION'], "\n"
+    end
+  end
+
 end
 
-PvAttrDupFix.new
+ClmLogging.new
