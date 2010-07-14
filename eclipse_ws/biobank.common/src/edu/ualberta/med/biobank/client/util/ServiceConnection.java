2aff027b5613077df763b43861c75f2fc496ee9a
Delphine Degris-Dard <degrisda@ualberta.ca>
2010-07-14 12:15:48 -0600

    resolves #676

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/client/util/ServiceConnection.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/client/util/ServiceConnection.java
@@ -1,6 +1,8 @@
 package edu.ualberta.med.biobank.client.util;
 
-import java.net.URL;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 import edu.ualberta.med.biobank.server.applicationservice.BiobankApplicationService;
 import gov.nih.nci.system.applicationservice.WritableApplicationService;
@@ -8,26 +10,15 @@ import gov.nih.nci.system.client.ApplicationServiceProvider;
 
 public class ServiceConnection {
 
-    /**
-     * if not null, called to resolved the address
-     */
-    private static ResourceResolver resourceResolver;
-
     private static String TRUST_STORE_PROPERTY_NAME = "javax.net.ssl.trustStore";
 
     private static String KEYSTORE_FILE_PATH = "cert/all.keystore";
 
     public static BiobankApplicationService getAppService(String serverUrl,
         String userName, String password) throws Exception {
-        return getAppService(serverUrl, ServiceConnection.class
-            .getResource(KEYSTORE_FILE_PATH), userName, password);
-    }
-
-    public static BiobankApplicationService getAppService(String serverUrl,
-        URL trustStoreUrl, String userName, String password) throws Exception {
         if (serverUrl.startsWith("https")
             && System.getProperty(TRUST_STORE_PROPERTY_NAME) == null) {
-            setTrustStore(trustStoreUrl);
+            setTrustStore();
         }
         BiobankApplicationService appService = null;
         if (userName == null) {
@@ -41,30 +32,26 @@ public class ServiceConnection {
         return appService;
     }
 
-    public static void setTrustStore() throws Exception {
-        setTrustStore(ServiceConnection.class.getResource(KEYSTORE_FILE_PATH));
-    }
-
-    public static void setTrustStore(URL url) throws Exception {
-        if (url == null)
-            return;
-        if (resourceResolver != null) {
-            url = resourceResolver.resolveURL(url);
-        }
-        System.setProperty(TRUST_STORE_PROPERTY_NAME, url.getFile());
+    private static void setTrustStore() throws Exception {
+        // export the keystore into a temporary file (can't use it from inside a
+        // jar)
+        File tf = File.createTempFile("biobank2", ".keystore");
+        tf.deleteOnExit();
+        byte buffer[] = new byte[0x1000];
+        InputStream is = ServiceConnection.class
+            .getResourceAsStream(KEYSTORE_FILE_PATH);
+        FileOutputStream out = new FileOutputStream(tf);
+        int cnt;
+        while ((cnt = is.read(buffer)) != -1)
+            out.write(buffer, 0, cnt);
+        is.close();
+        out.close();
+        System.setProperty(TRUST_STORE_PROPERTY_NAME, tf.getAbsolutePath());
     }
 
     public static WritableApplicationService getAppService(String serverUrl)
         throws Exception {
         return getAppService(serverUrl, null, null);
-    }
-
-    public static void setResourceResolver(ResourceResolver resourceResolver) {
-        ServiceConnection.resourceResolver = resourceResolver;
-    }
-
-    public static ResourceResolver getResourceResolver() {
-        return resourceResolver;
     }
 
     public static void logout(WritableApplicationService appService) {
