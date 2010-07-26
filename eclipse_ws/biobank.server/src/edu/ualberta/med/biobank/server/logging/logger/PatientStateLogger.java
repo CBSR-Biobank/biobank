3d6cf07ea9941a4fcdbff1532a63a4f7162d80c2
Nelson Loyola <loyola@cs.ualberta.ca>
2010-07-26 11:02:21 -0600

    fix for log site field in stat logger

--- eclipse_ws/biobank.server/src/edu/ualberta/med/biobank/server/logging/logger/PatientStateLogger.java
+++ eclipse_ws/biobank.server/src/edu/ualberta/med/biobank/server/logging/logger/PatientStateLogger.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import edu.ualberta.med.biobank.model.Log;
 import edu.ualberta.med.biobank.model.Patient;
+import edu.ualberta.med.biobank.model.Study;
 
 public class PatientStateLogger extends BiobankObjectStateLogger {
 
@@ -14,6 +15,8 @@ public class PatientStateLogger extends BiobankObjectStateLogger {
     protected Log getLogObject(Object obj, Map<String, Object> statesMap) {
         if (obj instanceof Patient) {
             Log log = new Log();
+            Study study = (Study) statesMap.get("study");
+            log.setSite(study.getSite().getNameShort());
             log.setPatientNumber((String) statesMap.get("pNumber"));
             log.setType("Patient");
             return log;
