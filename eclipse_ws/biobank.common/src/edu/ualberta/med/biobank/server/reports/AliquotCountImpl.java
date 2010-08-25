1e1031ebd92ed0cb7d62bdcc0412d70f893a10cc
Jonathan Ferland <jferland@ualberta.ca>
2010-08-25 10:38:15 -0600

    Added better data generation and PatientVisitSummaryTest

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/server/reports/AliquotCountImpl.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/server/reports/AliquotCountImpl.java
@@ -8,7 +8,7 @@ public class AliquotCountImpl extends AbstractReport {
 
     private static final String QUERY = "Select Alias.sampleType.name, count(*) from "
         + Aliquot.class.getName()
-        + " as Alias where Alias.aliquotPosition not in (from "
+        + " as Alias where Alias.aliquotPosition.id not in (from "
         + AliquotPosition.class.getName()
         + " a where a.container.label like 'SS%') and Alias.linkDate between ? and ? and Alias.patientVisit.shipment.site "
         + SITE_OPERATOR + SITE_ID + " GROUP BY Alias.sampleType.name";
