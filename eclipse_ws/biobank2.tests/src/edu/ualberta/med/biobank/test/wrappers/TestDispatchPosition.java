34c260c9c9e0329876101cf76711cd1db6e3050c
Nelson Loyola <loyola@cs.ualberta.ca>
2010-08-27 14:31:24 -0600

    working on dispatch position tests

---
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/wrappers/TestDispatchPosition.java
@@ -0,0 +1,41 @@
+package edu.ualberta.med.biobank.test.wrappers;
+
+import org.junit.Test;
+
+public class TestDispatchPosition {
+
+    @Test
+    public void testGettersAndSetters() throws Exception {
+
+    }
+
+    @Test
+    public void testCompatreTo() throws Exception {
+
+    }
+
+    @Test
+    public void testReset() throws Exception {
+
+    }
+
+    @Test
+    public void testReload() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWrappedClass() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testPersist() throws Exception {
+
+    }
+}
