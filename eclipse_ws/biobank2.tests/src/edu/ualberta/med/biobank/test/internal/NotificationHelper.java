d281c2910c1ba82b8f5481d7632528f0a58c1b8e
Nelson Loyola <nloyola@gmail.com>
2010-08-30 09:29:11 -0600

    working on disptach container tests

---
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/internal/NotificationHelper.java
@@ -0,0 +1,14 @@
+package edu.ualberta.med.biobank.test.internal;
+
+import java.util.Date;
+
+import edu.ualberta.med.biobank.common.wrappers.NotificationWrapper;
+import edu.ualberta.med.biobank.common.wrappers.SiteWrapper;
+
+public class NotificationHelper {
+
+    public static NotificationWrapper newShipment(SiteWrapper site,
+        Date dateSent, String message) throws Exception {
+        return null;
+    }
+}
--- eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/wrappers/TestNotification.java
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/wrappers/TestNotification.java
@@ -2,10 +2,16 @@ package edu.ualberta.med.biobank.test.wrappers;
 
 import org.junit.Test;
 
-public class TestDispatchPosition {
+import edu.ualberta.med.biobank.common.wrappers.SiteWrapper;
+import edu.ualberta.med.biobank.test.TestDatabase;
+import edu.ualberta.med.biobank.test.internal.SiteHelper;
+
+public class TestNotification extends TestDatabase {
 
     @Test
     public void testGettersAndSetters() throws Exception {
+        String name = "testGettersAndSetters" + r.nextInt();
+        SiteWrapper site = SiteHelper.addSite(name);
 
     }
 
