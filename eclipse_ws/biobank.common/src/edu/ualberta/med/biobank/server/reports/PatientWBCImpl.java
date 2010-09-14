8d950b89a5a89555b73eea85ac94e7839ba9a88d
Aaron <aaron.young@ualberta.ca>
2010-09-14 16:12:42 -0600

    checking over reports

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/server/reports/PatientWBCImpl.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/server/reports/PatientWBCImpl.java
@@ -12,14 +12,16 @@ public class PatientWBCImpl extends AbstractReport {
     private static final String QUERY = "Select Alias.patient.study.nameShort, Alias.shipment.clinic.nameShort, "
         + "Alias.patient.pnumber, Alias.dateProcessed, aliquot.sampleType.name, aliquot.inventoryId, aliquot.aliquotPosition.container.label  from "
         + PatientVisit.class.getName()
-        + " as Alias left join Alias.aliquotCollection as aliquot where aliquot.aliquotPosition not in (from "
+        + " as Alias join Alias.aliquotCollection as aliquot where aliquot.aliquotPosition not in (from "
         + AliquotPosition.class.getName()
         + " a where a.container.label like '"
         + SENT_SAMPLES_FREEZER_NAME
         + "') and Alias.shipment.site "
         + SITE_OPERATOR
         + SITE_ID
-        + " and aliquot.sampleType.name LIKE '%DNA%' and aliquot.aliquotPosition.container.id in (select path1.container.id from "
+        + " and aliquot.sampleType.nameShort ='"
+        + FTA_CARD_SAMPLE_TYPE_NAME
+        + "' and aliquot.aliquotPosition.container.id in (select path1.container.id from "
         + ContainerPath.class.getName()
         + " as path1, "
         + ContainerPath.class.getName()
