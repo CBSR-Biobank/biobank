20943fe41d199f0b9e5b577f7d095f45ccddcc8e
Nelson Loyola <loyola@cs.ualberta.ca>
2010-10-07 09:30:10 -0600

    resolves #826: renamed labeling scheme Box81 to CBSR SBS

--- data/sql/containerLabelingScheme.sql
+++ data/sql/containerLabelingScheme.sql
@@ -4,5 +4,5 @@ INSERT INTO `CONTAINER_LABELING_SCHEME` (ID, NAME, MIN_CHARS, MAX_CHARS, MAX_ROW
 ( 2, "CBSR 2 char alphabetic",2,2,null,null,576),
 ( 3, "2 char numeric",2,2,null,null,99),
 ( 4, "Dewar",2,2,2,2,4),
-( 5, "Box81",2,2,9,9,81);
+( 5, "CBSR SBS",2,2,9,9,81);
 UNLOCK TABLES;
