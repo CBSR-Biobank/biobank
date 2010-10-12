f3da982b9090a288fdceb22d4fc15c452180bf13
Aaron <aaron.young@ualberta.ca>
2010-10-12 15:34:46 -0600

    resolves #842: upgrade script added to convert 130 database to no longer use the disabled activity status

---
+++ data/sql/upgrade_v130_131.sql
@@ -0,0 +1,10 @@
+update aliquot c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update container c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update clinic c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update study c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update site c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update container_type c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update sample_storage c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update study_pv_attr c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+update abstract_shipment c set c.activity_status_id = (select id from activity_status where name = 'Closed') where c.activity_status_id = (select id from activity_status where name = 'Disabled');
+delete from activity_status where name='Disabled';
