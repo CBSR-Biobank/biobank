f13a4523f00cff20f150644a560ff3bef9a5efa2
Jonathan Ferland <jferland@ualberta.ca>
2010-10-13 10:17:53 -0600

    Check if Log Dir exists, is dir and is writable.
    resolves #829

---
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/utils/FilePromptUtil.java
@@ -0,0 +1,56 @@
+package edu.ualberta.med.biobank.utils;
+
+import java.io.File;
+import java.text.MessageFormat;
+
+import edu.ualberta.med.biobank.BioBankPlugin;
+
+public class FilePromptUtil {
+    private static final String CREATE_PATH_TITLE = "Create Path";
+    private static final String CREATE_PATH_MESSAGE = "Path {0} does not exist. Would you like to create it?";
+    private static final String CREATE_PATH_ERROR_TITLE = "Error Creating Path";
+    private static final String CREATE_PATH_ERROR_MESSAGE = "An error occured. Could not create path {0}.";
+    private static final String PATH_ERROR_TITLE = "Path Error";
+    private static final String PATH_CANNOT_WRITE_MESSAGE = "An error occured. Unable to write to path {0}.";
+    private static final String PATH_NOT_DIRECTORY_MESSAGE = "An error occured. The path {0} is not a directory.";
+
+    /**
+     * Creates the given directory, if it does not exist, after prompting the
+     * user for permission. Checks if the <code>File</code> is a directory and
+     * is writable.
+     * 
+     * @param dir
+     * @return <code>true</code> if the directory exists and is writable.
+     */
+    // TODO: accept a list of checks to apply? Order matters.
+    public static boolean isWritableDir(File dir) {
+        if (!dir.exists()) {
+            boolean createPath = BioBankPlugin.openConfirm(CREATE_PATH_TITLE,
+                MessageFormat.format(CREATE_PATH_MESSAGE, dir));
+
+            if (!createPath) {
+                return false;
+            }
+
+            if (!dir.mkdirs()) {
+                BioBankPlugin.openAsyncError(CREATE_PATH_ERROR_TITLE,
+                    MessageFormat.format(CREATE_PATH_ERROR_MESSAGE, dir));
+                return false;
+            }
+        }
+
+        if (!dir.isDirectory()) {
+            BioBankPlugin.openAsyncError(CREATE_PATH_ERROR_TITLE,
+                MessageFormat.format(PATH_NOT_DIRECTORY_MESSAGE, dir));
+            return false;
+        }
+
+        if (!dir.canWrite()) {
+            BioBankPlugin.openAsyncError(PATH_ERROR_TITLE,
+                MessageFormat.format(PATH_CANNOT_WRITE_MESSAGE, dir));
+            return false;
+        }
+
+        return true;
+    }
+}
