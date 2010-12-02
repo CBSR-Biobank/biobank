011bcd308214889062f9818d88465aa70be15cfc
Nelson Loyola <loyola@cs.ualberta.ca>
2010-12-02 10:19:36 -0700

    fix for AllTests after merge

--- eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/AllTests.java
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/AllTests.java
@@ -11,19 +11,19 @@ import edu.ualberta.med.biobank.test.internal.DbHelper;
 import edu.ualberta.med.biobank.test.wrappers.TestActivityStatus;
 import edu.ualberta.med.biobank.test.wrappers.TestAliquot;
 import edu.ualberta.med.biobank.test.wrappers.TestClinic;
-import edu.ualberta.med.biobank.test.wrappers.TestShipment;
 import edu.ualberta.med.biobank.test.wrappers.TestContact;
 import edu.ualberta.med.biobank.test.wrappers.TestContainer;
 import edu.ualberta.med.biobank.test.wrappers.TestContainerLabelingScheme;
 import edu.ualberta.med.biobank.test.wrappers.TestContainerPath;
 import edu.ualberta.med.biobank.test.wrappers.TestContainerType;
-import edu.ualberta.med.biobank.test.wrappers.TestDispatchShipment;
+import edu.ualberta.med.biobank.test.wrappers.TestDispatch;
 import edu.ualberta.med.biobank.test.wrappers.TestModelWrapper;
 import edu.ualberta.med.biobank.test.wrappers.TestPatient;
 import edu.ualberta.med.biobank.test.wrappers.TestPatientVisit;
 import edu.ualberta.med.biobank.test.wrappers.TestPvSourceVessel;
 import edu.ualberta.med.biobank.test.wrappers.TestSampleStorage;
 import edu.ualberta.med.biobank.test.wrappers.TestSampleType;
+import edu.ualberta.med.biobank.test.wrappers.TestShipment;
 import edu.ualberta.med.biobank.test.wrappers.TestShippingMethod;
 import edu.ualberta.med.biobank.test.wrappers.TestSite;
 import edu.ualberta.med.biobank.test.wrappers.TestSourceVessel;
@@ -33,13 +33,13 @@ import gov.nih.nci.system.applicationservice.WritableApplicationService;
 
 @RunWith(Suite.class)
 @SuiteClasses({ TestActivityStatus.class, TestAliquot.class, TestClinic.class,
-    TestClinicShipment.class, TestContact.class, TestContainer.class,
+    TestShipment.class, TestContact.class, TestContainer.class,
     TestContainerLabelingScheme.class, TestContainerPath.class,
-    TestContainerType.class, TestDispatchShipment.class,
-    TestModelWrapper.class, TestPatient.class, TestPatientVisit.class,
-    TestPvSourceVessel.class, TestSampleStorage.class, TestSampleType.class,
-    TestShippingMethod.class, TestSite.class, TestSourceVessel.class,
-    TestStudy.class, TestStudySourceVessel.class })
+    TestContainerType.class, TestDispatch.class, TestModelWrapper.class,
+    TestPatient.class, TestPatientVisit.class, TestPvSourceVessel.class,
+    TestSampleStorage.class, TestSampleType.class, TestShippingMethod.class,
+    TestSite.class, TestSourceVessel.class, TestStudy.class,
+    TestStudySourceVessel.class })
 public class AllTests {
     public static WritableApplicationService appService = null;
 
