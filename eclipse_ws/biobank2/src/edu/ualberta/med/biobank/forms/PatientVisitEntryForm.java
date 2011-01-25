43958695123675e44ecd353f6d58d80af3f15853
Nelson Loyola <loyola@cs.ualberta.ca>
2011-01-25 14:12:16 -0700

    patient visit entry form sets activity status to active when editing a new patient visit

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/PatientVisitEntryForm.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/forms/PatientVisitEntryForm.java
@@ -95,7 +95,7 @@ public class PatientVisitEntryForm extends BiobankEntryForm {
     private ComboViewer activityStatusComboViewer;
 
     @Override
-    public void init() {
+    public void init() throws Exception {
         Assert.isTrue(adapter instanceof PatientVisitAdapter,
             "Invalid editor input: object of type "
                 + adapter.getClass().getName());
@@ -112,9 +112,12 @@ public class PatientVisitEntryForm extends BiobankEntryForm {
         String tabName;
         if (patientVisit.isNew()) {
             tabName = "New Patient Visit";
+            patientVisit.setActivityStatus(ActivityStatusWrapper
+                .getActiveActivityStatus(appService));
         } else {
             tabName = "Visit " + patientVisit.getFormattedDateProcessed();
         }
+
         setPartName(tabName);
     }
 
