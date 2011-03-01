5d53fb74c4d60a7d3a56bc310272ef9b077b28cd
Aaron <aaron.young@ualberta.ca>
2011-03-01 10:18:02 -0700

    new table

---
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/infotables/OriginInfoTable.java
@@ -0,0 +1,115 @@
+package edu.ualberta.med.biobank.widgets.infotables;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.swt.widgets.Composite;
+
+import edu.ualberta.med.biobank.common.wrappers.ClinicWrapper;
+import edu.ualberta.med.biobank.common.wrappers.OriginInfoWrapper;
+import edu.ualberta.med.biobank.common.wrappers.PatientWrapper;
+import edu.ualberta.med.biobank.common.wrappers.ShippingMethodWrapper;
+import edu.ualberta.med.biobank.widgets.BiobankLabelProvider;
+
+public class OriginInfoTable extends InfoTableWidget<OriginInfoWrapper> {
+
+    private class TableRowData {
+        OriginInfoWrapper shipment;
+        String dateReceived;
+        String waybill;
+        String shippingCompany;
+        Integer numPatients;
+
+        @Override
+        public String toString() {
+            return StringUtils.join(new String[] { dateReceived, waybill,
+                shippingCompany, numPatients.toString() }, "\t");
+        }
+    }
+
+    private static final String[] HEADINGS = new String[] { "Date received",
+        "Waybill", "Shipping company", "No. Patients" };
+
+    public OriginInfoTable(Composite parent, ClinicWrapper clinic) {
+        super(parent, clinic.getOriginInfoCollection(true), HEADINGS, 10);
+    }
+
+    @Override
+    protected BiobankLabelProvider getLabelProvider() {
+        return new BiobankLabelProvider() {
+            @Override
+            public String getColumnText(Object element, int columnIndex) {
+                TableRowData item = (TableRowData) ((BiobankCollectionModel) element).o;
+                if (item == null) {
+                    if (columnIndex == 0) {
+                        return "loading...";
+                    }
+                    return "";
+                }
+                switch (columnIndex) {
+                case 0:
+                    return item.dateReceived;
+                case 1:
+                    return item.waybill;
+                case 2:
+                    return item.shippingCompany;
+                case 3:
+                    return item.numPatients.toString();
+                default:
+                    return "";
+                }
+            }
+        };
+    }
+
+    /**
+     * Required since the shipping company object must be loaded for every
+     * shipment.
+     */
+    @Override
+    public Object getCollectionModelObject(OriginInfoWrapper shipinfo)
+        throws Exception {
+        TableRowData info = new TableRowData();
+        info.shipment = shipinfo;
+        info.dateReceived = shipinfo.getShipmentInfo()
+            .getFormattedDateReceived();
+        info.waybill = shipinfo.getShipmentInfo().getWaybill();
+        ShippingMethodWrapper company = shipinfo.getShipmentInfo()
+            .getShippingMethod();
+        if (company != null) {
+            info.shippingCompany = company.getName();
+        } else {
+            info.shippingCompany = new String();
+        }
+        List<PatientWrapper> patients = shipinfo.getPatientCollection();
+        if (patients == null) {
+            info.numPatients = 0;
+        } else {
+            info.numPatients = patients.size();
+        }
+        return info;
+    }
+
+    @Override
+    protected String getCollectionModelObjectToString(Object o) {
+        if (o == null)
+            return null;
+        return ((TableRowData) o).toString();
+    }
+
+    @Override
+    public OriginInfoWrapper getSelection() {
+        BiobankCollectionModel item = getSelectionInternal();
+        if (item == null)
+            return null;
+        TableRowData row = (TableRowData) item.o;
+        Assert.isNotNull(row);
+        return row.shipment;
+    }
+
+    @Override
+    protected BiobankTableSorter getComparator() {
+        return null;
+    }
+}
