57c6c5117521aab9d27f0d0afa9df98b9b6c6980
Aaron <aaron.young@ualberta.ca>
2011-03-01 16:32:47 -0700

    new specimenadapter

---
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/treeview/SpecimenAdapter.java
@@ -0,0 +1,84 @@
+package edu.ualberta.med.biobank.treeview;
+
+import java.util.Collection;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Tree;
+
+import edu.ualberta.med.biobank.common.wrappers.ModelWrapper;
+import edu.ualberta.med.biobank.common.wrappers.SpecimenWrapper;
+import edu.ualberta.med.biobank.forms.SpecimenEntryForm;
+import edu.ualberta.med.biobank.forms.SpecimenViewForm;
+import gov.nih.nci.system.applicationservice.WritableApplicationService;
+
+public class SpecimenAdapter extends AdapterBase {
+
+    public SpecimenAdapter(AdapterBase parent, SpecimenWrapper sample) {
+        super(parent, sample);
+    }
+
+    public SpecimenWrapper getSpecimen() {
+        return (SpecimenWrapper) modelObject;
+    }
+
+    @Override
+    public void addChild(AdapterBase child) {
+        Assert.isTrue(false, "Cannot add children to this adapter");
+    }
+
+    @Override
+    protected String getLabelInternal() {
+        SpecimenWrapper aliquot = getSpecimen();
+        Assert.isNotNull(aliquot, "sample is null");
+        return aliquot.getInventoryId();
+    }
+
+    @Override
+    public String getTooltipText() {
+        return getTooltipText("Aliquot");
+    }
+
+    @Override
+    public WritableApplicationService getAppService() {
+        return modelObject.getAppService();
+    }
+
+    @Override
+    public void popupMenu(TreeViewer tv, Tree tree, Menu menu) {
+        addViewMenu(menu, "Aliquot");
+    }
+
+    @Override
+    protected AdapterBase createChildNode() {
+        return null;
+    }
+
+    @Override
+    protected AdapterBase createChildNode(ModelWrapper<?> child) {
+        return null;
+    }
+
+    @Override
+    protected Collection<? extends ModelWrapper<?>> getWrapperChildren()
+        throws Exception {
+        return null;
+    }
+
+    @Override
+    protected int getWrapperChildCount() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public String getEntryFormId() {
+        return SpecimenEntryForm.ID;
+    }
+
+    @Override
+    public String getViewFormId() {
+        return SpecimenViewForm.ID;
+    }
+
+}
