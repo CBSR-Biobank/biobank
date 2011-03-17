d017f9b77860d4093749b0316d99843f22fe5206
Nelson Loyola <loyola@cs.ualberta.ca>
2011-03-17 14:56:54 -0600

    adding more database upgrade tests

---
+++ eclipse_ws/biobank2.tools/src/edu/ualberta/med/biobank/tools/upgradetest/DataDump.java
@@ -0,0 +1,53 @@
+package edu.ualberta.med.biobank.tools.upgradetest;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+
+import edu.ualberta.med.biobank.common.formatters.DateFormatter;
+
+public class DataDump {
+
+    protected Properties queryProps;
+
+    protected Connection dbconnection;
+
+    protected BufferedWriter bw;
+
+    public DataDump(Connection connection, Properties queryProps,
+        String filename) throws IOException {
+        this.dbconnection = connection;
+        this.queryProps = queryProps;
+
+        File f = new File(filename);
+        f.delete();
+
+        bw = new BufferedWriter(new FileWriter(filename, true));
+    }
+
+    public void dispose() throws IOException, SQLException {
+        bw.close();
+        dbconnection.close();
+    }
+
+    public static Date getDateFromStr(String str) throws ParseException {
+        Date date = DateFormatter.parseToDateTime(str);
+        if (date == null) {
+            date = DateFormatter.parseToDate(str);
+        }
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.set(Calendar.MILLISECOND, 0);
+        cal.set(Calendar.SECOND, 0);
+        return cal.getTime();
+    }
+
+}
---
+++ eclipse_ws/biobank2.tools/src/edu/ualberta/med/biobank/tools/upgradetest/DataDumpV140.java
@@ -0,0 +1,106 @@
+package edu.ualberta.med.biobank.tools.upgradetest;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Properties;
+
+public class DataDumpV140 extends DataDump {
+
+    public DataDumpV140(Connection dbconnection, Properties queryProps,
+        String filename) throws IOException {
+        super(dbconnection, queryProps, filename);
+    }
+
+    public void getAliquotedSpecimens() throws SQLException, IOException {
+        PreparedStatement ps = dbconnection.prepareStatement(queryProps
+            .getProperty("v140.getAliquotedSpecimens"));
+
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            StringBuilder row = new StringBuilder();
+            row.append(rs.getString(1)).append(",").append(rs.getString(2))
+                .append(",").append(rs.getString(3)).append(",")
+                .append(rs.getString(4)).append(",").append(rs.getDate(5))
+                .append(",").append(rs.getDouble(6)).append(",")
+                .append(rs.getString(7));
+            bw.write(row.toString());
+            bw.newLine();
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public void getSourceSpecimens() throws SQLException, IOException,
+        ParseException {
+        PreparedStatement ps = dbconnection.prepareStatement(queryProps
+            .getProperty("v140.getSourceSpecimens"));
+
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            StringBuilder row = new StringBuilder();
+            row.append(rs.getString(1)).append(",").append(rs.getString(2))
+                .append(",").append(rs.getString(3)).append(",")
+                .append(getDateFromStr(rs.getString(4))).append(",")
+                .append(getDateFromStr(rs.getString(5))).append(",")
+                .append(rs.getString(6));
+            bw.write(row.toString());
+            bw.newLine();
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public void getSiteStudies() throws SQLException, IOException,
+        ParseException {
+        PreparedStatement ps = dbconnection.prepareStatement(queryProps
+            .getProperty("v140.getSiteStudies"));
+
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            StringBuilder row = new StringBuilder();
+            row.append(rs.getString(1)).append(",").append(rs.getString(2));
+            bw.write(row.toString());
+            bw.newLine();
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public void getStudyContacts() throws SQLException, IOException,
+        ParseException {
+        PreparedStatement ps = dbconnection.prepareStatement(queryProps
+            .getProperty("v140.getStudyContacts"));
+
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            StringBuilder row = new StringBuilder();
+            row.append(rs.getString(1)).append(",").append(rs.getString(2))
+                .append(",").append(rs.getString(3));
+            bw.write(row.toString());
+            bw.newLine();
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+    public void getSiteContainers() throws SQLException, IOException,
+        ParseException {
+        PreparedStatement ps = dbconnection.prepareStatement(queryProps
+            .getProperty("v140.getSiteContainers"));
+
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            StringBuilder row = new StringBuilder();
+            row.append(rs.getString(1)).append(",").append(rs.getString(2));
+            bw.write(row.toString());
+            bw.newLine();
+        }
+        bw.newLine();
+        bw.flush();
+    }
+
+}
