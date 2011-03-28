9910560d13aa867a4d5caf02a3b6fbb1069a2ca7
Delphine Degris-Dard <degrisda@ualberta.ca>
2011-03-27 21:06:32 -0600

    working on #1042 and 1043

--- eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/TestDatabase.java
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/TestDatabase.java
@@ -2,7 +2,9 @@ package edu.ualberta.med.biobank.test;
 
 import edu.ualberta.med.biobank.common.VarCharLengths;
 import edu.ualberta.med.biobank.common.exception.BiobankCheckException;
+import edu.ualberta.med.biobank.common.exception.CheckFieldLimitsException;
 import edu.ualberta.med.biobank.common.wrappers.ModelWrapper;
+import edu.ualberta.med.biobank.server.applicationservice.exceptions.StringValueLengthServerException;
 import edu.ualberta.med.biobank.test.internal.ClinicHelper;
 import edu.ualberta.med.biobank.test.internal.ShippingMethodHelper;
 import edu.ualberta.med.biobank.test.internal.SiteHelper;
@@ -21,7 +23,6 @@ import java.util.Random;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.springframework.dao.InvalidDataAccessResourceUsageException;
 
 public class TestDatabase {
     protected static WritableApplicationService appService;
@@ -183,20 +184,36 @@ public class TestDatabase {
                 String attrName = getterInfo.getMethod.getName().substring(3);
                 attrName = attrName.substring(0, 1).toLowerCase()
                     + attrName.substring(1);
-                if (VarCharLengths.getMaxSize(w.getWrappedClass(), attrName) == null)
-                    continue;
-
+                // 512 char string
+                String longString = Utils.getRandomString(511, 512);
                 try {
-                    // 512 char string
-                    String longString = Utils.getRandomString(511, 512);
                     getterInfo.setMethod.invoke(w, longString);
                     w.persist();
-                    Assert.fail("VARCHAR limits not enforced on field: "
-                        + attrName);
-                } catch (InvalidDataAccessResourceUsageException e) {
+                    // no specific error thrown
                     Assert.assertTrue(true);
+                } catch (StringValueLengthServerException e) {
+                    if (VarCharLengths
+                        .getMaxSize(w.getWrappedClass(), attrName) == null) {
+                        System.out
+                            .println("StringValueLengthServerException thrown but "
+                                + "no corresponding key in VarCharLengths map for "
+                                + w.getWrappedClass() + "." + attrName);
+                        continue;
+                    }
+                    // FIXME VarCharLength map does not contain information for
+                    // internal properties like street1 for centers or sub
+                    // classes like clinic
+                    Assert
+                        .fail("VARCHAR limits not checked with 'checkFieldLimits method' on field: "
+                            + w.getWrappedClass().getName() + "." + attrName);
+                } catch (CheckFieldLimitsException cfle) {
+                    if (VarCharLengths
+                        .getMaxSize(w.getWrappedClass(), attrName) == null)
+                        Assert
+                            .fail("CheckFieldLimitsException should not be thrown");
+                } finally {
+                    w.reload();
                 }
-                w.reload();
             }
         }
 
