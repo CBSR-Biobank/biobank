44cc24535bfdf28bb602a680da4c0e20011d9fd8
Nelson Loyola <loyola@cs.ualberta.ca>
2011-05-03 11:39:19 -0600

    fixing tests in TestShipmentInfo

---
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/internal/ShipmentInfoHelper.java
@@ -0,0 +1,59 @@
+package edu.ualberta.med.biobank.test.internal;
+
+import java.util.Date;
+
+import edu.ualberta.med.biobank.common.wrappers.CenterWrapper;
+import edu.ualberta.med.biobank.common.wrappers.ClinicWrapper;
+import edu.ualberta.med.biobank.common.wrappers.ShipmentInfoWrapper;
+import edu.ualberta.med.biobank.common.wrappers.ShippingMethodWrapper;
+import edu.ualberta.med.biobank.common.wrappers.SpecimenWrapper;
+import edu.ualberta.med.biobank.test.Utils;
+import edu.ualberta.med.biobank.test.wrappers.TestCommon;
+
+public class ShipmentInfoHelper extends DbHelper {
+
+    public static ShipmentInfoWrapper newShipment(CenterWrapper<?> center,
+        ShippingMethodWrapper method, String waybill, Date dateReceived,
+        SpecimenWrapper... spcs) throws Exception {
+        ShipmentInfoWrapper shipInfo = new ShipmentInfoWrapper(appService);
+
+        for (SpecimenWrapper spc : spcs) {
+            spc.getOriginInfo().setShipmentInfo(shipInfo);
+            if (center != null) {
+                spc.setCurrentCenter(center);
+            }
+        }
+
+        shipInfo.setShippingMethod(method);
+        shipInfo.setWaybill(waybill);
+        if (dateReceived != null) {
+            shipInfo.setReceivedAt(dateReceived);
+        }
+
+        shipInfo.setPackedAt(Utils.getRandomDate());
+
+        return shipInfo;
+    }
+
+    public static ShipmentInfoWrapper newShipment(CenterWrapper<?> center,
+        ShippingMethodWrapper method) throws Exception {
+        return newShipment(center, method, TestCommon.getNewWaybill(r),
+            Utils.getRandomDate());
+    }
+
+    public static ShipmentInfoWrapper addShipment(CenterWrapper<?> center,
+        ShippingMethodWrapper method, String waybill, SpecimenWrapper... spcs)
+        throws Exception {
+        ShipmentInfoWrapper shipment = newShipment(center, method, waybill,
+            Utils.getRandomDate(), spcs);
+        shipment.persist();
+        return shipment;
+    }
+
+    public static ShipmentInfoWrapper addShipment(CenterWrapper<?> center,
+        ClinicWrapper clinic, ShippingMethodWrapper method,
+        SpecimenWrapper... spcs) throws Exception {
+        return addShipment(center, method, TestCommon.getNewWaybill(r), spcs);
+    }
+
+}
--- eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/wrappers/TestShipmentInfo.java
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/wrappers/TestShipmentInfo.java
@@ -2,43 +2,44 @@ package edu.ualberta.med.biobank.test.wrappers;
 
 import java.util.Arrays;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
+import edu.ualberta.med.biobank.common.exception.BiobankCheckException;
 import edu.ualberta.med.biobank.common.wrappers.ClinicWrapper;
+import edu.ualberta.med.biobank.common.wrappers.CollectionEventWrapper;
 import edu.ualberta.med.biobank.common.wrappers.ContactWrapper;
 import edu.ualberta.med.biobank.common.wrappers.PatientWrapper;
 import edu.ualberta.med.biobank.common.wrappers.ShippingMethodWrapper;
+import edu.ualberta.med.biobank.common.wrappers.SpecimenWrapper;
 import edu.ualberta.med.biobank.common.wrappers.StudyWrapper;
 import edu.ualberta.med.biobank.test.TestDatabase;
 import edu.ualberta.med.biobank.test.internal.ClinicHelper;
+import edu.ualberta.med.biobank.test.internal.CollectionEventHelper;
 import edu.ualberta.med.biobank.test.internal.ContactHelper;
 import edu.ualberta.med.biobank.test.internal.PatientHelper;
+import edu.ualberta.med.biobank.test.internal.SpecimenHelper;
 import edu.ualberta.med.biobank.test.internal.StudyHelper;
 
 public class TestShipmentInfo extends TestDatabase {
     @Test
     public void testPersistFailWaybillNull() throws Exception {
         String name = "testPersistFailWaybillNull" + r.nextInt();
-        ClinicWrapper clinic = ClinicHelper.addClinicWithShipments(name);
         StudyWrapper study = StudyHelper.addStudy(name);
-        ContactWrapper contact = ContactHelper.addContact(clinic, name);
-        study.addToContactCollection(Arrays.asList(contact));
-        study.persist();
+        ClinicWrapper clinic = ClinicHelper.addClinicWithShipments(name);
         PatientWrapper patient = PatientHelper.addPatient(name, study);
+        SpecimenWrapper spc = SpecimenHelper.newSpecimen(name);
 
-        // FIXME
-        // CollectionEventWrapper cevent = CollectionEventHelper
-        // .newCollectionEvent(clinic, ShippingMethodWrapper
-        // .getShippingMethods(appService).get(0), null, Utils
-        // .getRandomDate(), SpecimenHelper.newSpecimen(patient,
-        // Utils.getRandomDate(), 0.1));
-        //
-        // try {
-        // cevent.persist();
-        // Assert.fail("cevent with waybill null");
-        // } catch (BiobankCheckException bce) {
-        // Assert.assertTrue(true);
-        // }
+        CollectionEventWrapper cevent = CollectionEventHelper
+            .newCollectionEvent(clinic, patient, 1, spc);
+
+        try {
+            cevent.persist();
+            Assert.fail("cevent with waybill null");
+        } catch (BiobankCheckException bce) {
+            Assert.assertTrue(true);
+        }
     }
 
     @Test
