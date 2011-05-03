78392468e22a0026c7fc1704454aa8f47c590f31
Aaron <aaron.young@ualberta.ca>
2011-05-03 13:33:23 -0600

    resolves #1181: report not showing aliquots without position

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/server/reports/SampleTypePvCountImpl.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/server/reports/SampleTypePvCountImpl.java
@@ -2,6 +2,7 @@ package edu.ualberta.med.biobank.server.reports;
 
 import edu.ualberta.med.biobank.common.reports.BiobankReport;
 import edu.ualberta.med.biobank.model.CollectionEvent;
+import edu.ualberta.med.biobank.model.SpecimenPosition;
 
 /**
  * needs one parameters = study.nameShort
@@ -10,11 +11,15 @@ public class SampleTypePvCountImpl extends AbstractReport {
     private static final String QUERY = "SELECT ce.patient.pnumber,"
         + "     min(s.parentSpecimen.processingEvent.createdAt) as first_date_processed,"
         + "     min(s.topSpecimen.createdAt) as first_date_drawn, s.specimenType.name, count(*)"
-        + (" FROM " + CollectionEvent.class.getName() + " as ce ")
-        + "     join ce.allSpecimenCollection as s"
-        + (" WHERE s.specimenPosition.container.label not like '"
-            + SENT_SAMPLES_FREEZER_NAME + "'")
-        + "     and ce.patient.study.nameShort like ?"
+        + " FROM "
+        + CollectionEvent.class.getName()
+        + " as ce join ce.allSpecimenCollection as s"
+        + " left join s.specimenPosition as p WHERE (p is null or p not in (from "
+        + SpecimenPosition.class.getName()
+        + " a where a.container.label like '"
+        + SENT_SAMPLES_FREEZER_NAME
+        + "'"
+        + ")) and ce.patient.study.nameShort like ?"
         + " GROUP BY ce.patient.pnumber, s.specimenType "
         + " ORDER BY ce.patient.pnumber, min(s.parentSpecimen.processingEvent.createdAt)";
 
