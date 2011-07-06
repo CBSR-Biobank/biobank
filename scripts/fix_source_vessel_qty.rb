2517b7c5d8b2dfb78b574d5922c44e4aac38628e
Nelson Loyola <loyola@cs.ualberta.ca>
2011-07-06 13:10:10 -0600

    ready to run script on live database

--- scripts/fix_source_vessel_qty.rb
+++ scripts/fix_source_vessel_qty.rb
@@ -1,10 +1,23 @@
 #!/usr/bin/ruby -w
 
+#-------------------------------------------------------------------------------
+#
+# Fix for issue #1278.
+#
+# Creates as many source specimens in the BioBank 3 database as there were
+# source vessels for each patient visit. The quantity field in the
+# pv_source_vessel table tells us how many source vessels there are per
+# patient visit.
+#
+#-------------------------------------------------------------------------------
+
 $LOAD_PATH.unshift( File.join( File.dirname(__FILE__), 'lib' ) )
 
 require "script_base"
 require 'time'
 require 'pp'
+require 'rubygems'
+require 'choice'
 
 class FixSourceVesselQty < ScriptBase
 
@@ -38,24 +51,51 @@ BIOBANK_QRY_END
 
   SPC_INSERT_BASE_QRY = <<SPC_INSERT_QRY
 insert into  specimen (inventory_id,created_at,quantity,activity_status_id,
-collection_event_id,original_collection_event_id,specimen_type_id,
-parent_specimen_id,top_specimen_id,origin_info_id) values
+collection_event_id,original_collection_event_id,processing_event_id,
+specimen_type_id,parent_specimen_id,top_specimen_id,origin_info_id,
+current_center_id) values
 SPC_INSERT_QRY
 
-  FIELDS = [ 'QUANTITY', 'ACTIVITY_STATUS_ID', 'COLLECTION_EVENT_ID', 'ORIGINAL_COLLECTION_EVENT_ID',
-             'SPECIMEN_TYPE_ID', 'PARENT_SPECIMEN_ID', 'TOP_SPECIMEN_ID', 'ORIGIN_INFO_ID' ]
+  FIELDS = [ 'QUANTITY', 'ACTIVITY_STATUS_ID', 'COLLECTION_EVENT_ID',
+             'ORIGINAL_COLLECTION_EVENT_ID', 'PROCESSING_EVENT_ID',
+             'SPECIMEN_TYPE_ID', 'PARENT_SPECIMEN_ID', 'TOP_SPECIMEN_ID',
+             'ORIGIN_INFO_ID', 'CURRENT_CENTER_ID' ]
 
   def initialize
+    Choice.options do
+      header 'options:'
+
+      separator 'Optional:'
+
+      option :sql do
+        short '-s'
+        long '--sql'
+        desc 'use sql insert statements to add missing source speciemens to database'
+      end
+
+      separator 'Common:'
+
+      option :help do
+        short '-h'
+        long '--help'
+        desc 'Show this message.'
+      end
+    end
+
+    print "inserting missing source specimens in database\n" if (Choice.choices[:sql])
+
     getDbConnection("biobank2", 'localhost')
     res = @dbh.query(BIOBANK2_QRY)
     pvsvs = Array.new
     res.each_hash do |row|
+
       invalid_date = false
-      begin
-        created_at = Time.parse(row['created_at']).utc.strftime("%Y-%m-%d %T")
-      rescue ArgumentError
+      year = row['created_at'][0..4].to_i
+      if (year < 1970)
         created_at = row['created_at']
         invalid_date = false
+      else
+        created_at = Time.parse(row['created_at']).utc.strftime("%Y-%m-%d %T")
       end
 
       pvsvs << {
@@ -72,7 +112,7 @@ SPC_INSERT_QRY
     #pp pvsvs
 
     @dbh2 = Mysql.real_connect('localhost', "dummy", "ozzy498", "biobank")
-    #print "ALTER TABLE specimen MODIFY COLUMN ID INT(11) NOT NULL auto_increment;\n"
+    @dbh2.query("ALTER TABLE specimen MODIFY COLUMN ID INT(11) NOT NULL auto_increment") if (Choice.choices[:sql])
 
     pvsvs.each do |pvsv|
       qry = BIOBANK_QRY.gsub("{study_name}", pvsv['study']);
@@ -85,14 +125,15 @@ SPC_INSERT_QRY
         qry = qry.gsub("{created_at}", pvsv['created_at']);
       end
 
-      print qry, "\n"
+      #print qry, "\n"
+
       res = @dbh2.query(qry)
       if (res.num_rows < 1)
         print "no result for: ",
         [ pvsv['study'],  pvsv['pnumber'],  pvsv['created_at_unformatted'], pvsv['created_at'],
           pvsv['specimen_type_name'], pvsv['quantity'] ].join(", "), "\n"
       elsif (res.num_rows == pvsv['quantity'])
-        print "fixed: ",
+        print "already fixed: ",
         [ pvsv['study'],  pvsv['pnumber'],  pvsv['created_at_unformatted'], pvsv['created_at'],
           pvsv['specimen_type_name'], pvsv['quantity'] ].join(", "), "\n"
       elsif (res.num_rows < pvsv['quantity'])
@@ -122,6 +163,7 @@ SPC_INSERT_QRY
           qry << values.join(",")
           qry << ")"
           #print qry, ";\n";
+          @dbh2.query(qry) if (Choice.choices[:sql])
         end
       else
         raise "#{res.num_rows}: nothing to be done for #{pvsv['pnumber']}, specimen type #{ pvsv['specimen_type_name']} with date #{pvsv['created_at']} \n"
@@ -129,7 +171,7 @@ SPC_INSERT_QRY
       res.free
     end
 
-#    print "ALTER TABLE specimen MODIFY COLUMN ID INT(11) NOT NULL;\n"
+    @dbh2.query("ALTER TABLE specimen MODIFY COLUMN ID INT(11) NOT NULL") if (Choice.choices[:sql])
   end
 end
 
