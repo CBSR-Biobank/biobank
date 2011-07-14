77ddef93c92aa82795bfcfad78c5944f0abd2986
Aaron <aaron.young@ualberta.ca>
2011-07-14 15:59:41 -0600

    rg persist checks and request addresses

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/wrappers/ResearchGroupWrapper.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/wrappers/ResearchGroupWrapper.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.ualberta.med.biobank.common.exception.BiobankException;
+import edu.ualberta.med.biobank.common.peer.CenterPeer;
 import edu.ualberta.med.biobank.common.peer.ResearchGroupPeer;
 import edu.ualberta.med.biobank.common.wrappers.base.ResearchGroupBaseWrapper;
+import edu.ualberta.med.biobank.model.Center;
 import edu.ualberta.med.biobank.model.ResearchGroup;
 import edu.ualberta.med.biobank.model.Study;
 import gov.nih.nci.system.applicationservice.ApplicationException;
@@ -45,6 +47,31 @@ public class ResearchGroupWrapper extends ResearchGroupBaseWrapper {
         return getCountResult(appService, new HQLCriteria(RG_COUNT_QRY));
     }
 
+    private static String AVAIL_STUDIES = "select s from "
+        + Study.class.getName() + " s where s not in (select r."
+        + ResearchGroupPeer.STUDY.getName() + " from "
+        + ResearchGroup.class.getName() + " r)";
+
+    public static List<StudyWrapper> getAvailStudies(
+        WritableApplicationService appService) throws ApplicationException {
+        HQLCriteria criteria = new HQLCriteria(AVAIL_STUDIES);
+        List<Study> studies = appService.query(criteria);
+        List<StudyWrapper> wrappers = new ArrayList<StudyWrapper>();
+        for (Study s : studies) {
+            wrappers.add(new StudyWrapper(appService, s));
+        }
+        return wrappers;
+    }
+
+    @Override
+    protected void persistChecks() throws BiobankException,
+        ApplicationException {
+        checkNoDuplicates(Center.class, CenterPeer.NAME.getName(), getName(),
+            "A center with name");
+        checkNoDuplicates(Center.class, CenterPeer.NAME_SHORT.getName(),
+            getNameShort(), "A center with short name");
+    }
+
     @Override
     public long getCollectionEventCountForStudy(StudyWrapper study)
         throws ApplicationException, BiobankException {
@@ -63,27 +90,6 @@ public class ResearchGroupWrapper extends ResearchGroupBaseWrapper {
     public Long getPatientCount() throws Exception {
         // TODO Auto-generated method stub
         return null;
-    }
-
-    public Boolean getSendsShipments() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    private static String AVAIL_STUDIES = "select s from "
-        + Study.class.getName() + " s where s not in (select r."
-        + ResearchGroupPeer.STUDY.getName() + " from "
-        + ResearchGroup.class.getName() + " r)";
-
-    public static List<StudyWrapper> getAvailStudies(
-        WritableApplicationService appService) throws ApplicationException {
-        HQLCriteria criteria = new HQLCriteria(AVAIL_STUDIES);
-        List<Study> studies = appService.query(criteria);
-        List<StudyWrapper> wrappers = new ArrayList<StudyWrapper>();
-        for (Study s : studies) {
-            wrappers.add(new StudyWrapper(appService, s));
-        }
-        return wrappers;
     }
 
 }
