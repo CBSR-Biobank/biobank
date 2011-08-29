348624d926364409227d68508c4091b38a51181b
Nelson Loyola <loyola@cs.ualberta.ca>
2011-08-29 13:32:55 -0600

    fixed externalized string warnings and errors

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/utils/Messages.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/widgets/utils/Messages.java
@@ -4,7 +4,6 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
     private static final String BUNDLE_NAME = "edu.ualberta.med.biobank.widgets.utils.messages"; //$NON-NLS-1$
-    public static String BiobankClipboard_copy_label;
     static {
         // initialize resource bundle
         NLS.initializeMessages(BUNDLE_NAME, Messages.class);
