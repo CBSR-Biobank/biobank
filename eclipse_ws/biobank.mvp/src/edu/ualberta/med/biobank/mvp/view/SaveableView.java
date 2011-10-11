a72f8c1ac62d6707afba50e26a483633d394a4f5
Jonathan Ferland <jferland@ualberta.ca>
2011-10-11 12:31:45 -0600

    Added TODO: SaveableView should have way to disable save button

--- eclipse_ws/biobank.mvp/src/edu/ualberta/med/biobank/mvp/view/SaveableView.java
+++ eclipse_ws/biobank.mvp/src/edu/ualberta/med/biobank/mvp/view/SaveableView.java
@@ -3,5 +3,7 @@ package edu.ualberta.med.biobank.mvp.view;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 
 public interface SaveableView extends View {
+    // TODO: should probably use an interface that allows this "button" to be
+    // enabled or disabled
     HasClickHandlers getSave();
 }
