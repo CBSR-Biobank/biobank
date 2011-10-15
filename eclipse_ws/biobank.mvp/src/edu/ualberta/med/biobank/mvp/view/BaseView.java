cfb42430bb1061195de3740e945aaa1f138631cd
Jonathan Ferland <jmferland@gmail.com>
2011-10-15 17:49:12 -0600

    Add Explanation of Why Create(parent) VS asComposite for Views

--- eclipse_ws/biobank.mvp/src/edu/ualberta/med/biobank/mvp/view/BaseView.java
+++ eclipse_ws/biobank.mvp/src/edu/ualberta/med/biobank/mvp/view/BaseView.java
@@ -1,5 +1,21 @@
 package edu.ualberta.med.biobank.mvp.view;
 
+import com.google.gwt.canvas.dom.client.Context2d.Composite;
+
 public interface BaseView {
+    /**
+     * With Eclipse RCP, {@link BaseView}-s cannot extend {@link Composite} and
+     * implement the view because the {@link Composite#setParent()} method does
+     * not necessarily work on all operating systems. So, instead,
+     * {@link BaseView}-s must implement this interface and contain an inner
+     * class that is created via the
+     * {@link BaseView#create(org.eclipse.swt.widgets.Composite)} method.
+     * <p>
+     * Event IEditorPart uses {@link IEditorPart#createPartControl(Composite)}
+     * to create the inner part of the editor, instead of taking a Composite as
+     * an argument.
+     * 
+     * @param parent
+     */
     void create(org.eclipse.swt.widgets.Composite parent);
 }
