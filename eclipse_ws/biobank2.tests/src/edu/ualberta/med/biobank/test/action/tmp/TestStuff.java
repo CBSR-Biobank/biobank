cf55bd9807497b1c8c0debabfe3d3200aa680f0e
Jonathan Ferland <jmferland@gmail.com>
2011-12-15 20:05:50 -0700

    Hibernate Listener Testing

--- eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/action/tmp/TestStuff.java
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/action/tmp/TestStuff.java
@@ -1,15 +1,18 @@
 package edu.ualberta.med.biobank.test.action.tmp;
 
-import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
-import org.hibernate.EmptyInterceptor;
 import org.hibernate.FlushMode;
 import org.hibernate.PropertyValueException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.event.PreUpdateEvent;
+import org.hibernate.event.PreUpdateEventListener;
 import org.hibernate.exception.ConstraintViolationException;
-import org.hibernate.type.Type;
 
 import edu.ualberta.med.biobank.model.ActivityStatus;
 import edu.ualberta.med.biobank.model.Address;
@@ -20,24 +23,42 @@ public class TestStuff {
     private final Session session;
 
     private TestStuff() {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
-        // new Configuration().configure().getEventListeners().set
-        session = sessionFactory.openSession(new Interceptor());
+        Configuration configuration = new Configuration().configure();
+        configuration.getEventListeners().setPreUpdateEventListeners(new PreUpdateEventListener[] {new Handler()});
+//        configuration.setProperty("hibernate.show_sql", "true");
+//        configuration.setProperty("hibernate.format_sql", "true");
+//        configuration.setProperty("hibernate.use_sql_comments", "true");
+        
+        sessionFactory = configuration.buildSessionFactory();
+        
+        session = sessionFactory.openSession();
         session.setFlushMode(FlushMode.COMMIT);
     }
 
     public void run() {
+        Site s1 = null;
+        
+        List<?> results =  session.createQuery("select o from " + Site.class.getName() + " o where name = 'a'").list();
+        if (!results.isEmpty()) {
+            s1 = (Site) results.get(0);
+        } else {            
+            s1 = new Site();
+            s1.setAddress(new Address());
+            s1.setName("a");
+            s1.setNameShort("b");
+        }
+
         ActivityStatus active =
             (ActivityStatus) session
                 .createQuery(
                     "select o from " + ActivityStatus.class.getName()
                         + " o where name = 'Active'").list().get(0);
-
-        Site s1 = new Site();
-        s1.setName("a");
-        s1.setNameShort("b");
-        s1.setAddress(new Address());
+        
+        Address address = s1.getAddress();
+        address.setCity(new BigInteger(10, new Random()).toString(32));
+        
         s1.setActivityStatus(active);
+        s1.setNameShort(new BigInteger(130, new Random()).toString(32));
 
         System.out.println("start");
 
@@ -61,23 +82,17 @@ public class TestStuff {
     public static void main(String[] args) {
         new TestStuff().run();
     }
-
-    public static class Interceptor extends EmptyInterceptor {
+    
+    public static class Handler implements PreUpdateEventListener {
         private static final long serialVersionUID = 1L;
 
         @Override
-        public boolean onSave(Object entity, Serializable id, Object[] state,
-            String[] propertyNames, Type[] types) {
-            System.out.println("onSave");
-            return true;
-        }
-
-        @Override
-        public boolean onFlushDirty(Object entity, Serializable id,
-            Object[] currentState, Object[] previousState,
-            String[] propertyNames, Type[] types) {
-            System.out.println("onFlushDirty");
-            return true;
+        public boolean onPreUpdate(PreUpdateEvent event) {
+            System.out.println(Arrays.toString(event.getPersister().getPropertyNames()));
+            System.out.println(Arrays.toString(event.getState()));
+            System.out.println(Arrays.toString(event.getOldState()));
+            
+            return false;
         }
     }
 }
