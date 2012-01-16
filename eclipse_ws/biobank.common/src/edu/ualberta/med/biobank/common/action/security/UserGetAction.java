aa702caa317334288a630714e8783fe45dfb4d62
Nelson Loyola <loyola@cs.ualberta.ca>
2012-01-16 15:48:30 -0700

    action tests: better way for determining if super admin user exists

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/action/security/UserGetAction.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/action/security/UserGetAction.java
@@ -34,9 +34,8 @@ public class UserGetAction implements Action<UserGetResult> {
         @SuppressWarnings("unchecked")
         List<User> list = c.list();
         if (list.size() == 0) {
-            return null;
-            //throw new ActionException("Problem getting user with login=" //$NON-NLS-1$
-            // + login);
+            throw new ActionException("Problem getting user with login=" //$NON-NLS-1$
+                + login);
         }
         return new UserGetResult(list.get(0));
     }
