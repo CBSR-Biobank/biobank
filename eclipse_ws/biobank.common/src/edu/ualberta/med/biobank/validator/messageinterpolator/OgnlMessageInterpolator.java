44175eee5bc278acd38c65df45eb7bd6914e0ce0
Jonathan Ferland <jmferland@gmail.com>
2012-02-05 12:48:41 -0700

    New Root Object so can have validateValue AND attribute properties

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/validator/messageinterpolator/OgnlMessageInterpolator.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/validator/messageinterpolator/OgnlMessageInterpolator.java
@@ -1,21 +1,21 @@
 package edu.ualberta.med.biobank.validator.messageinterpolator;
 
 import java.util.Locale;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.validation.MessageInterpolator;
 
 import ognl.Ognl;
-import ognl.OgnlContext;
 import ognl.OgnlException;
 
 import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
 
 /**
  * Enables the OGNL evaluation of anything of the format
- * <code>$&#123;ognl_expression&#125;</code>, where the validated value is the
- * root object of the OGNL expression.
+ * <code>$&#123;ognl_expression&#125;</code>, where the root object for
+ * evaluation is a {@link RootObject}.
  * 
  * @author Jonathan Ferland
  */
@@ -48,19 +48,19 @@ public class OgnlMessageInterpolator implements MessageInterpolator {
     @Override
     public String interpolate(String message, Context context, Locale locale) {
         String tmp = delegate.interpolate(message, context, locale);
-        return interpolateMessage(tmp, context.getValidatedValue(), locale);
+        return interpolateMessage(tmp, context, locale);
     }
 
     /**
      * Interpolate the validated value in the given message.
      * 
      * @param message the message where validated value have to be interpolated
-     * @param validatedValue the value of the object being validated
+     * @param context the context of the object being validated
      * @param locale the {@code Locale} to use for message interpolation
      * 
      * @return the interpolated message
      */
-    private String interpolateMessage(String message, Object validatedValue,
+    private String interpolateMessage(String message, Context context,
         Locale locale) {
         String interpolatedMessage = message;
         Matcher matcher = VARIABLE_START_PATTERN.matcher(message);
@@ -99,8 +99,7 @@ public class OgnlMessageInterpolator implements MessageInterpolator {
                 String variable = message.substring(matcher.start(), lastIndex);
                 String ognlExpression = extractContents(variable);
 
-                String evaluation =
-                    evaluateOgnl(ognlExpression, validatedValue);
+                String evaluation = evaluateOgnl(ognlExpression, context);
 
                 String escapedVariable = Pattern.quote(variable);
                 String escapedEvaluation = Matcher.quoteReplacement(evaluation);
@@ -147,17 +146,34 @@ public class OgnlMessageInterpolator implements MessageInterpolator {
         return charIndex > 0 && string.charAt(charIndex - 1) == '\\';
     }
 
-    private String evaluateOgnl(String expression, Object validatedValue) {
+    private String evaluateOgnl(String expression, Context context) {
         String result = null;
 
         try {
-            OgnlContext context = new OgnlContext();
-            Object value = Ognl.getValue(expression, context, validatedValue);
+            RootObject root = new RootObject();
+            root.validatedValue = context.getValidatedValue();
+            root.attributes = context.getConstraintDescriptor().getAttributes();
+
+            Object value = Ognl.getValue(expression, root);
 
             result = value.toString();
         } catch (OgnlException e) {
+            // TODO: something better?
         }
 
         return result;
+    }
+
+    static class RootObject {
+        private Object validatedValue;
+        private Map<String, Object> attributes;
+
+        public Object getValidatedValue() {
+            return validatedValue;
+        }
+
+        public Map<String, Object> getAttributes() {
+            return attributes;
+        }
     }
 }
