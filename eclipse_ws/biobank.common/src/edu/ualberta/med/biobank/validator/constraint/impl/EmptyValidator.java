36dab73afff184dd040528e8fe043a0f906346e8
Jonathan Ferland <jferland@ualberta.ca>
2012-02-06 14:13:11 -0700

    Fix collection size implementation

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/validator/constraint/impl/EmptyValidator.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/validator/constraint/impl/EmptyValidator.java
@@ -1,15 +1,12 @@
 package edu.ualberta.med.biobank.validator.constraint.impl;
 
-import java.io.Serializable;
+import java.text.MessageFormat;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.hibernate.EntityMode;
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.engine.SessionImplementor;
+import org.hibernate.Query;
 import org.hibernate.metadata.ClassMetadata;
 
 import edu.ualberta.med.biobank.validator.SessionAwareConstraintValidator;
@@ -17,6 +14,9 @@ import edu.ualberta.med.biobank.validator.constraint.Empty;
 
 public class EmptyValidator extends SessionAwareConstraintValidator<Object>
     implements ConstraintValidator<Empty, Object> {
+    private static final String SIZE_QUERY =
+        "SELECT {0}.size FROM {1} o WHERE o = ?";
+
     private String property;
 
     @Override
@@ -30,13 +30,13 @@ public class EmptyValidator extends SessionAwareConstraintValidator<Object>
         if (value == null) {
             return true;
         }
-        
-        boolean empty = countRows(value) == 0;
-        
+
+        boolean empty = getCollectionSize(value) == 0;
+
         if (!empty) {
             overrideEmptyMessageTemplate(value, context);
         }
-        
+
         return empty;
     }
 
@@ -64,22 +64,22 @@ public class EmptyValidator extends SessionAwareConstraintValidator<Object>
         }
     }
 
-    private int countRows(Object value) {
+    private int getCollectionSize(Object value) {
         ClassMetadata meta = getSession().getSessionFactory()
             .getClassMetadata(value.getClass());
 
         // Don't check the property directy, but use HQL instead since it could
-        // be enormous.
+        // be enormous. UNLESS, see the following link:
+        // http://stackoverflow.com/questions/2913160/hibernate-count-collection-size-without-initializing
+        // BUT, we would still have a problem if the database is out of sync
+        // from the local collection.
 
-        String idName = meta.getIdentifierPropertyName();
-        Serializable id = meta.getIdentifier(value,
-            (SessionImplementor) getSession());
+        String hql = MessageFormat.format(SIZE_QUERY,
+            property, meta.getMappedClass(EntityMode.POJO).getName());
 
-        DetachedCriteria criteria = DetachedCriteria.forClass(value.getClass());
-        criteria.add(Restrictions.eq(idName, id))
-            .setProjection(Projections.count(property));
+        Query query = getSession().createQuery(hql).setParameter(0, value);
 
-        List<?> results = criteria.getExecutableCriteria(getSession()).list();
+        List<?> results = query.list();
         Number count = (Number) results.iterator().next();
 
         return count.intValue();
