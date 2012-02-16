38843c6373c393086ba57e4cede98ff9f97c7935
Aaron <aaron.young@ualberta.ca>
2012-02-16 14:54:25 -0700

    fixes to reloadhandler and plugin.xml

--- eclipse_ws/biobank.gui.common/src/edu/ualberta/med/biobank/gui/common/forms/Actions.java
+++ eclipse_ws/biobank.gui.common/src/edu/ualberta/med/biobank/gui/common/forms/Actions.java
@@ -1,9 +1,12 @@
 package edu.ualberta.med.biobank.gui.common.forms;
 
 public class Actions {
-    public static final String GUI_COMMON_RESET = "edu.ualberta.med.biobank.gui.common.commands.reset"; //$NON-NLS-1$
+    public static final String GUI_COMMON_RELOAD =
+        "edu.ualberta.med.biobank.gui.common.commands.reload"; //$NON-NLS-1$
 
-    public static final String GUI_COMMON_CONFIRM = "edu.ualberta.med.biobank.gui.common.commands.confirm"; //$NON-NLS-1$
+    public static final String GUI_COMMON_CONFIRM =
+        "edu.ualberta.med.biobank.gui.common.commands.confirm"; //$NON-NLS-1$
 
-    public static final String GUI_COMMON_CANCEL = "edu.ualberta.med.biobank.gui.common.commands.cancel"; //$NON-NLS-1$
+    public static final String GUI_COMMON_CANCEL =
+        "edu.ualberta.med.biobank.gui.common.commands.cancel"; //$NON-NLS-1$
 }
