05e1bc734a393ab9d32f5dd91ba2fdbd23d6075a
Nelson Loyola <loyola@cs.ualberta.ca>
2012-02-17 11:31:17 -0700

    database upgrade changes after merging dev-jon

--- data/sql/upgrade_v310_v320_security_transfer.sql
+++ data/sql/upgrade_v310_v320_security_transfer.sql
@@ -3,15 +3,19 @@
 -- convert users from csm to users from biobank:
 -- add + 10 because groups (that are also principals) should be added. So far only one is added, but just in case we add more, add 10
 
-set @asactive = null;
-
-select id from activity_status where name='Active' into @asactive;
-
 insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk_emails, full_name, email, need_pwd_change, activity_status_id)
-       select user_id + 10, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), email_id, 0, @asactive
+       select user_id + 10, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), null, 0, 1
        from csm_user
        where login_name != 'administrator'
-       and login_name != 'bbadmin';
+       and login_name != 'bbadmin'
+       and length(email_id) = 0;
+
+insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk_emails, full_name, email, need_pwd_change, activity_status_id)
+       select user_id + 10, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), email_id, 0, 1
+       from csm_user
+       where login_name != 'administrator'
+       and login_name != 'bbadmin'
+       and length(email_id) != 0;
 
 set @group_super_admin = null;
 
