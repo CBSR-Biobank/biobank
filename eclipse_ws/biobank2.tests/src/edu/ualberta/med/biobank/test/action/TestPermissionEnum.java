efecf69af9a1ca77ad161efb5ae3df30d98cbf95
Jonathan Ferland <jmferland@gmail.com>
2012-02-21 14:52:52 -0700

    Added a couple tests to check if the correct identifier is being used when saving enums

---
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/action/TestPermissionEnum.java
@@ -0,0 +1,41 @@
+package edu.ualberta.med.biobank.test.action;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.ualberta.med.biobank.model.PermissionEnum;
+import edu.ualberta.med.biobank.model.Role;
+
+public class TestPermissionEnum extends TestAction {
+    @Test
+    public void testPersistedId() {
+        String name = getMethodNameR();
+
+        Role role = new Role();
+        role.setName(name);
+
+        Integer roleId = (Integer) session.save(role);
+
+        for (PermissionEnum permissionEnum : PermissionEnum.values()) {
+            role.getPermissionCollection().clear();
+            role.getPermissionCollection().add(permissionEnum);
+
+            session.update(role);
+            session.flush();
+
+            Integer permissionEnumId =
+                (Integer) session
+                    .createSQLQuery(
+                        "select permission_id from role_permission where id = ?")
+                    .setParameter(0, roleId)
+                    .list().iterator().next();
+
+            Assert.assertTrue(
+                "stored id " + permissionEnumId
+                    + " does not match PermissionEnum.getId() "
+                    + permissionEnum.getId() + " of PermissionEnum "
+                    + permissionEnum.name(),
+                permissionEnumId.intValue() == permissionEnum.getId());
+        }
+    }
+}
