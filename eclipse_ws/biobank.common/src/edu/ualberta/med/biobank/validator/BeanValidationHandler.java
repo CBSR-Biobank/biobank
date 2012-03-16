8c7466e018f537a2269ca4aff017632634d8d0c0
Jonathan Ferland <jmferland@gmail.com>
2012-03-15 21:58:42 -0600

    BeanValidationHandler refactoring for validating collections

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/validator/BeanValidationHandler.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/validator/BeanValidationHandler.java
@@ -51,6 +51,8 @@ public class BeanValidationHandler implements PreInsertEventListener,
     private final ValidatorFactory factory;
     private final MessageInterpolator messageInterpolator =
         new OgnlMessageInterpolator();
+
+    // TODO: I really hope this doesn't hold onto TONS of objects. Investigate!
     private ConcurrentHashMap<EntityPersister, Set<String>> associationsPerEntityPersister =
         new ConcurrentHashMap<EntityPersister, Set<String>>();
 
@@ -98,26 +100,20 @@ public class BeanValidationHandler implements PreInsertEventListener,
             .println("Recreate. Role: " + event.getCollection().getRole());
     }
 
-    private void validate(AbstractPreDatabaseOperationEvent event,
-        Class<?>[] groups) {
-        // TODO: write something to track event source (Session) and only
-        // validate once? Not sure this is a good idea if validators affect each
-        // other, or the same object is saved multiple times... can't guarantee
-        // that won't happen.
-        validate(event.getEntity(), event, groups);
+    private void validate(Object entity, String property) {
+
     }
 
-    private <T> void validate(T object,
-        AbstractPreDatabaseOperationEvent event, Class<?>[] groups) {
-
-        EntityMode mode = event.getSession().getEntityMode();
-
-        if (object == null || mode != EntityMode.POJO) {
-            return;
-        }
-
+    private void validate(AbstractPreDatabaseOperationEvent event,
+        Class<?>[] groups) {
         EntityPersister persister = event.getPersister();
         EventSource session = event.getSession();
+        Validator validator = getValidator(persister, session);
+        validate(validator, event.getEntity(), session, groups);
+    }
+
+    private Validator getValidator(EntityPersister persister,
+        EventSource session) {
         SessionFactoryImplementor sessionFactory = session.getFactory();
 
         TraversableResolver tr = new HibernateTraversableResolver(
@@ -132,6 +128,16 @@ public class BeanValidationHandler implements PreInsertEventListener,
             .messageInterpolator(messageInterpolator)
             .getValidator();
 
+        return validator;
+    }
+
+    private <T> void validate(Validator validator, T object,
+        EventSource session, Class<?>[] groups) {
+        EntityMode mode = session.getEntityMode();
+
+        if (object == null || mode != EntityMode.POJO) return;
+        if (groups.length == 0) return;
+
         FlushMode oldMode = session.getFlushMode();
         try {
             // If the session is used to query data, then another flush could be
@@ -139,48 +145,50 @@ public class BeanValidationHandler implements PreInsertEventListener,
             // an infinite loop. Avoid this.
             session.setFlushMode(FlushMode.MANUAL);
 
-            if (groups.length > 0) {
-                final Set<ConstraintViolation<T>> constraintViolations =
-                    validator.validate(object, groups);
-                if (constraintViolations.size() > 0) {
-                    // TODO: include the bean being validated and the type of
-                    // action that was trying to be performed?
-                    // TODO: put some of this code in another separate class
-                    // that can be called directly (not through a listener)
-                    // through a specific action.
-                    // TODO: add a tag for validations to be performed on the
-                    // server (only) and not locally...
-                    Set<ConstraintViolation<?>> localizedViolations =
-                        new HashSet<ConstraintViolation<?>>(
-                            constraintViolations.size());
-
-                    StringBuilder builder = new StringBuilder();
-                    builder.append("validation failed for groups: ");
-                    builder.append(Arrays.toString(groups));
-
-                    for (ConstraintViolation<T> violation : constraintViolations) {
-                        // if (log.isTraceEnabled()) {
-                        // log.trace(violation.toString());
-                        // }
-
-                        ConstraintViolation<T> localizedViolation =
-                            new LocalizedConstraintViolation<T>(violation);
-
-                        localizedViolations.add(localizedViolation);
-
-                        builder.append("\r\n");
-                        builder.append(violation.getLeafBean().getClass()
-                            .getName());
-                        builder.append(":");
-                        builder.append(violation.getMessage());
-                    }
-
-                    throw new ConstraintViolationException(
-                        builder.toString(), localizedViolations);
-                }
-            }
+            final Set<ConstraintViolation<T>> constraintViolations =
+                validator.validate(object, groups);
+            handleViolations(constraintViolations, groups);
         } finally {
             session.setFlushMode(oldMode);
         }
+    }
+
+    private <T> void handleViolations(
+        Set<ConstraintViolation<T>> constraintViolations, Class<?>[] groups) {
+        if (constraintViolations.isEmpty()) return;
+
+        // TODO: include the bean being validated and the type of
+        // action that was trying to be performed?
+        // TODO: put some of this code in another separate class
+        // that can be called directly (not through a listener)
+        // through a specific action.
+        // TODO: add a tag for validations to be performed on the
+        // server (only) and not locally...
+        Set<ConstraintViolation<?>> localizedViolations =
+            new HashSet<ConstraintViolation<?>>(
+                constraintViolations.size());
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("validation failed for groups: ");
+        builder.append(Arrays.toString(groups));
+
+        for (ConstraintViolation<T> violation : constraintViolations) {
+            // if (log.isTraceEnabled()) {
+            // log.trace(violation.toString());
+            // }
+
+            ConstraintViolation<T> localizedViolation =
+                new LocalizedConstraintViolation<T>(violation);
+
+            localizedViolations.add(localizedViolation);
+
+            builder.append("\r\n");
+            builder.append(violation.getLeafBean().getClass().getName());
+            builder.append(":");
+            builder.append(violation.getMessage());
+        }
+
+        throw new ConstraintViolationException(builder.toString(),
+            localizedViolations);
     }
 }
