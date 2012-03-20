0a9304856d0663628a59f075b5b33e332142666b
Aaron <aaron.young@ualberta.ca>
2012-03-20 15:11:01 -0600

    resolves #1561: actions added, and removed empty file

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/action/site/SiteAddCommentAction.java
+++
@@ -1,6 +0,0 @@
-package edu.ualberta.med.biobank.common.action.site;
-
-// TODO: this action will be called by SiteSaveAction, or independently (so that a comment can be added every time a site is created or editted).
-public class SiteAddCommentAction {
-
-}
