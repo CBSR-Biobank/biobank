9b8e5472b07de7d2789e74044027624e98a19f74
Nelson Loyola <loyola@cs.ualberta.ca>
2012-03-21 11:48:47 -0600

    resolves #1577: querying the user for activity logs location moved to when opening link assign and specimen assign

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/handlers/SpecimenLinkHandler.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/handlers/SpecimenLinkHandler.java
@@ -1,12 +1,20 @@
 package edu.ualberta.med.biobank.handlers;
 
+import java.io.File;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 
+import edu.ualberta.med.biobank.BiobankPlugin;
 import edu.ualberta.med.biobank.SessionManager;
 import edu.ualberta.med.biobank.common.permission.specimen.SpecimenAssignPermission;
 import edu.ualberta.med.biobank.common.wrappers.UserWrapper;
+import edu.ualberta.med.biobank.dialogs.startup.ActivityLogLocationDialog;
 import edu.ualberta.med.biobank.forms.linkassign.SpecimenLinkEntryForm;
+import edu.ualberta.med.biobank.gui.common.BgcPlugin;
+import edu.ualberta.med.biobank.preferences.PreferenceConstants;
 import edu.ualberta.med.biobank.treeview.processing.SpecimenLinkAdapter;
 
 public class SpecimenLinkHandler extends LinkAssignCommonHandler {
@@ -15,11 +23,40 @@ public class SpecimenLinkHandler extends LinkAssignCommonHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
+        if (!checkActivityLogSavePathValid()) {
+            BgcPlugin.openAsyncError("Activity Log Location",
+                "Invalid path selected. Cannot proceed with link assign.");
+            return null;
+        }
+
         openLinkAssignPerspective(SpecimenLinkEntryForm.ID,
             new SpecimenLinkAdapter(null, 0,
-                Messages.SpecimenLinkHandler_specimen_link_label,
-                false));
+                Messages.SpecimenLinkHandler_specimen_link_label, false));
         return null;
+    }
+
+    public static boolean checkActivityLogSavePathValid() {
+        IWorkbenchWindow window =
+            PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+
+        boolean logSave =
+            BiobankPlugin.getDefault().getPreferenceStore().getBoolean(
+                PreferenceConstants.LINK_ASSIGN_ACTIVITY_LOG_INTO_FILE);
+
+        File dir = new File(BiobankPlugin.getDefault().getPreferenceStore()
+            .getString(PreferenceConstants.LINK_ASSIGN_ACTIVITY_LOG_PATH));
+
+        // check if the activity logs path is set...
+        if (logSave && (!dir.isDirectory() || !dir.canWrite())) {
+            ActivityLogLocationDialog logsDlg = new ActivityLogLocationDialog(
+                window.getShell());
+            logsDlg.open();
+        }
+
+        dir = new File(BiobankPlugin.getDefault().getPreferenceStore()
+            .getString(PreferenceConstants.LINK_ASSIGN_ACTIVITY_LOG_PATH));
+
+        return (!logSave || (dir.isDirectory() && dir.canWrite()));
     }
 
     @Override
