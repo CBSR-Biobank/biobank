983507a824f0de1404bc3b84e91f716ad878392e
Jonathan Ferland <jferland@ualberta.ca>
2012-03-27 13:31:11 -0600

    Work on a copy of the Membership so as to not modify if canceled

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/dialogs/user/MembershipEditWizard.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/dialogs/user/MembershipEditWizard.java
@@ -3,9 +3,11 @@ package edu.ualberta.med.biobank.dialogs.user;
 import org.eclipse.jface.wizard.Wizard;
 
 import edu.ualberta.med.biobank.common.action.security.ManagerContext;
+import edu.ualberta.med.biobank.model.Domain;
 import edu.ualberta.med.biobank.model.Membership;
 
 public class MembershipEditWizard extends Wizard {
+    private final Membership originalMembership;
     private final Membership membership;
     private final ManagerContext context;
 
@@ -13,8 +15,14 @@ public class MembershipEditWizard extends Wizard {
     private MembershipRolesPage rolesPage;
 
     public MembershipEditWizard(Membership membership, ManagerContext context) {
-        this.membership = membership;
+        this.originalMembership = membership;
+
+        this.membership = new Membership();
+        copyMembership(originalMembership, membership);
+
         this.context = context;
+
+        updateWindowTitle();
     }
 
     @Override
@@ -33,6 +41,36 @@ public class MembershipEditWizard extends Wizard {
 
     @Override
     public boolean performFinish() {
+        copyMembership(membership, originalMembership);
         return true;
+    }
+
+    /**
+     * Copy the properties we plan to modify.
+     * 
+     * @param src source {@link Membership}
+     * @param dst destination {@link Membership}
+     */
+    private void copyMembership(Membership src, Membership dst) {
+        dst.setDomain(new Domain(src.getDomain()));
+
+        dst.setUserManager(src.isUserManager());
+        dst.setEveryPermission(src.isEveryPermission());
+
+        dst.getPermissions().clear();
+        dst.getPermissions().addAll(src.getPermissions());
+
+        dst.getRoles().clear();
+        dst.getRoles().addAll(src.getRoles());
+    }
+
+    private void updateWindowTitle() {
+        String title = null;
+        if (membership.isNew()) {
+            title = "Add a New Membership";
+        } else {
+            title = "Editing a Membership";
+        }
+        setWindowTitle(title);
     }
 }
