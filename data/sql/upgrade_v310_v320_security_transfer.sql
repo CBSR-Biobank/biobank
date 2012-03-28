459f362e8efe442261e6c039d5a2f85c74c146f0
Jonathan Ferland <jferland@ualberta.ca>
2012-03-27 19:42:33 -0600

    Changes to reflect groups.sql changes

--- data/sql/upgrade_v310_v320_security_transfer.sql
+++ data/sql/upgrade_v310_v320_security_transfer.sql
@@ -2,6 +2,8 @@
 
 -- convert users from csm to users from biobank:
 -- add + 10 because groups (that are also principals) should be added. So far only one is added, but just in case we add more, add 10
+set autocommit = 0;
+start transaction;
 
 insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk_emails, full_name, email, need_pwd_change, activity_status_id)
        select user_id + 10, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), null, 0, 1
@@ -19,7 +21,7 @@ insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk
 
 set @group_super_admin = null;
 
-select id from principal where name='Super Administrators' into @group_super_admin;
+select id from principal where name='Global Administrators' into @group_super_admin;
 
 -- Old super admin group_id was 5 in csm database. Add previous super admin users in new super admin group:
 insert into group_user(user_id, group_id)
@@ -46,4 +48,4 @@ delete from csm_protection_element where object_id = 'edu.ualberta.med.biobank.m
 delete from csm_role where role_id = 7;
 delete from csm_role where role_id = 9;
 
-
+commit;
