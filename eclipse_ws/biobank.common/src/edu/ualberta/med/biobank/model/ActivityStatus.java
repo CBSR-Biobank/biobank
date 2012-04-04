4b3d3bdb8efc44ca9fe461b35b7982fe9361f821
Jonathan Ferland <jferland@ualberta.ca>
2012-04-04 13:56:47 -0600

    Add Tasks and Jars for I18N

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/model/ActivityStatus.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/model/ActivityStatus.java
@@ -5,21 +5,25 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
+
 import edu.ualberta.med.biobank.common.util.NotAProxy;
 
 public enum ActivityStatus implements NotAProxy, Serializable {
+
     // TODO: a key for translation? Even if comes from server is enum, so will
     // use local translation.
     // TODO: comment on NOT CHANGING THE ID!!!
-    ACTIVE(1, "Active"),
-    CLOSED(2, "Closed"),
+    ACTIVE(1, Loader.i18n.tr("Active")),
+    CLOSED(2, Loader.i18n.tr("Closed")),
     // TODO: why can't there be a closed and flagged item or an active but
     // flagged
     // item? Especially for users that are mean to be enabled or disabled.
     // When is ActivityStatus.FLAGGED even used in the source code? Can a
     // flagged
     // user log in? What is the point of flagged? Shouldn't it be separate?
-    FLAGGED(4, "Flagged");
+    FLAGGED(4, Loader.i18n.tr("Flagged"));
 
     private static final List<ActivityStatus> VALUES_LIST = Collections
         .unmodifiableList(Arrays.asList(values()));
@@ -51,5 +55,9 @@ public enum ActivityStatus implements NotAProxy, Serializable {
             }
         }
         return null;
+    }
+
+    public static class Loader {
+        private static final I18n i18n = I18nFactory.getI18n(Loader.class);
     }
 }
