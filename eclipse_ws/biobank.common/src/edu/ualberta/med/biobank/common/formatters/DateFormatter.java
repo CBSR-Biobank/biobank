024c8f3cf2600148ae58dfebe180bda74b3adbbc
Nelson Loyola <loyola@cs.ualberta.ca>
2012-04-25 10:43:44 -0600

    resolves #1499: BBPSP consent

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/formatters/DateFormatter.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/formatters/DateFormatter.java
@@ -3,6 +3,7 @@ package edu.ualberta.med.biobank.common.formatters;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -100,6 +101,27 @@ public class DateFormatter {
         } catch (ParseException e) {
             return null;
         }
+    }
+
+    public static Date dateNoSeconds(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    public static boolean compareDatesToMinutes(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+
+        cal1.setTime(date1);
+        cal2.setTime(date2);
+
+        return cal1.get(Calendar.YEAR) == cal1.get(Calendar.YEAR)
+            && cal1.get(Calendar.DAY_OF_YEAR) == cal1.get(Calendar.DAY_OF_YEAR)
+            && cal1.get(Calendar.HOUR_OF_DAY) == cal1.get(Calendar.HOUR_OF_DAY)
+            && cal1.get(Calendar.MINUTE) == cal1.get(Calendar.MINUTE);
     }
 
 }
