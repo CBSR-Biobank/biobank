46a34eacc03829ece2ae360e9e5bf374b8dcba27
Jonathan Ferland <jferland@ualberta.ca>
2012-04-30 10:59:23 -0600

    Resovle Several User Management Delete Issues
    - delete CSM user after since our user delete is more easily rolled back
    - don't fail a delete if the csm user doesn't exist
    - remove a user from all its groups before deleting since that is the managing side
    
    resolves #1696

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/action/security/UserDeleteAction.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/action/security/UserDeleteAction.java
@@ -6,8 +6,10 @@ import edu.ualberta.med.biobank.common.action.EmptyResult;
 import edu.ualberta.med.biobank.common.action.exception.ActionException;
 import edu.ualberta.med.biobank.common.permission.Permission;
 import edu.ualberta.med.biobank.common.permission.security.UserManagerPermission;
+import edu.ualberta.med.biobank.model.Group;
 import edu.ualberta.med.biobank.model.User;
 import edu.ualberta.med.biobank.server.applicationservice.BiobankCSMSecurityUtil;
+import gov.nih.nci.security.exceptions.CSObjectNotFoundException;
 import gov.nih.nci.system.applicationservice.ApplicationException;
 
 public class UserDeleteAction implements Action<EmptyResult> {
@@ -33,13 +35,27 @@ public class UserDeleteAction implements Action<EmptyResult> {
             throw new ActionException("insufficient power");
         }
 
+        // the group side is the managing side, so we must remove the user from
+        // the group and then save it
+        for (Group group : user.getGroups()) {
+            group.getUsers().remove(user);
+        }
+
+        // delete and flush the user first as this is easier to revert than
+        // deleting the CSM user.
+        context.getSession().delete(user);
+        context.getSession().flush();
+
         try {
             BiobankCSMSecurityUtil.deleteUser(user);
         } catch (ApplicationException e) {
-            throw new ActionException("unable to delete underlying CSM user", e);
+            // don't throw an exception if the user object cannot even be found.
+            if (!(e.getCause() instanceof CSObjectNotFoundException)) {
+                throw new ActionException(
+                    "unable to delete underlying CSM user", e);
+            }
         }
 
-        context.getSession().delete(user);
         return new EmptyResult();
     }
 }
