e4d0e6bcce1d54ddbe03dce9aa34867262851f9a
Jonathan Ferland <jferland@ualberta.ca>
2012-05-03 14:48:35 -0600

    Serialize and Deserialize action input and output to better simulate while testing
    resolves #1572

--- eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/action/LocalActionExecutor.java
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/action/LocalActionExecutor.java
@@ -1,5 +1,11 @@
 package edu.ualberta.med.biobank.test.action;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
@@ -33,33 +39,71 @@ public class LocalActionExecutor implements IActionExecutor {
     @Override
     public <T extends ActionResult> T exec(Action<T> action)
         throws ActionException {
+        action = simulateWire(action);
+
         // to behave like we sent this action to the server, we must open a new
         // session and close it before and after this action is executed, every
         // time. The User object should also be loaded fresh from the database.
         Session session = sessionProvider.openSession();
-        Transaction transaction = session.beginTransaction();
+
+        Transaction tx = session.beginTransaction();
         try {
-            User user = (User) session.createCriteria(User.class)
-                .add(Restrictions.eq("id", userId))
-                .list().iterator().next();
+            User user = (User) session
+                .createCriteria(User.class)
+                .add(Restrictions.idEq(userId))
+                .uniqueResult();
 
             ActionContext context = new ActionContext(user, session, null);
 
-            if (!action.isAllowed(context))
-                throw new AccessDeniedException();
+            if (!action.isAllowed(context)) throw new AccessDeniedException();
 
             T result = action.run(context);
 
-            transaction.commit();
+            result = simulateWire(result);
+
+            tx.commit();
 
             return result;
         } catch (RuntimeException e) {
-            transaction.rollback();
-
+            tx.rollback();
             throw e;
         } finally {
             session.clear();
             session.close();
         }
+    }
+
+    /**
+     * Serialize and deserialize the given object to pretend like it was
+     * transfered across the wire to a client or server.
+     * 
+     * @param object
+     * @return
+     */
+    private static <T> T simulateWire(T object) {
+        T obj = null;
+        try {
+            // Write the object out to a byte array
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(bos);
+            out.writeObject(object);
+            out.flush();
+            out.close();
+
+            // Make an input stream from the byte array and read
+            // a copy of the object back in.
+            ObjectInputStream in = new ObjectInputStream(
+                new ByteArrayInputStream(bos.toByteArray()));
+
+            @SuppressWarnings("unchecked")
+            T tmp = (T) in.readObject();
+
+            obj = tmp;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+        }
+        return obj;
     }
 }
