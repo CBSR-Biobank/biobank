70896a54a829e5b9ccfe47b729a1dfff2d84c45e
Aaron <aaron.young@ualberta.ca>
2012-05-10 13:15:46 -0600

    resolves #1727: null site adapters don't try to create children

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/treeview/admin/SiteAdapter.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/treeview/admin/SiteAdapter.java
@@ -33,8 +33,8 @@ public class SiteAdapter extends AdapterBase {
         super(parent, site);
         if (site != null && site.getId() != null) {
             nodeIdOffset *= site.getId();
+            createNodes();
         }
-        createNodes();
     }
 
     @Override
