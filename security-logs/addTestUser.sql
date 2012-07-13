b2bda6a33d75fcf5b1529f24ef7ec1ea4f22ade3
Nelson Loyola <loyola@cs.ualberta.ca>
2012-07-13 09:50:49 -0600

    this script is no longer CBSR site dependant

--- security-logs/addTestUser.sql
+++ security-logs/addTestUser.sql
@@ -1,9 +1,11 @@
 INSERT INTO csm_user (USER_ID, LOGIN_NAME, MIGRATED_FLAG, FIRST_NAME, LAST_NAME, PASSWORD, UPDATE_DATE)
-       select (select coalesce(MAX(id), 0)+Max(user_id)+1
-       from principal, csm_user), 'testuser', 0 , 'testuser', 'testuser', 'orDBlaojDQE=', sysdate();
+       select coalesce(MAX(id), 0)+Max(user_id)+1, 'testuser', 0 , 'testuser', 'testuser', 'orDBlaojDQE=', sysdate()
+       from principal, csm_user;
 
-insert into principal (discriminator, id, version, full_name, login, recv_bulk_emails, need_pwd_change, activity_status_id)
-       select 'User', user_id, 0, concat(first_name, ' ', last_name), login_name, 0, 0, 1 from csm_user where login_name = 'testuser';
+insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk_emails, full_name, email, need_pwd_change,activity_status_id)
+       select coalesce(MAX(id), 0)+1, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), email_id, 0, 1
+       from csm_user,principal
+       where login_name = 'testuser';
 
 insert into csm_user_group_role_pg (user_id, role_id, protection_group_id, update_date)
        select user_id, 8, 1, sysdate()
@@ -15,6 +17,5 @@ insert into group_user(user_id, group_id)
        select u.id, g.id
        from principal u, principal g
        where u.login='testuser'
-       and g.name='Global Administrators'
-       and u.discriminator = 'User'
-       and g.discriminator = 'BbGroup';
+       and g.name='Global Administrators';
+
