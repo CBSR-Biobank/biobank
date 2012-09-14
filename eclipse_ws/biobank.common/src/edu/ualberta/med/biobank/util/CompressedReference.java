4a59e782d8aea789b97a22b4670a88e6981da17b
Jonathan Ferland <jferland@ualberta.ca>
2012-09-14 11:51:29 -0600

    Slightly changed CompressedReference

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/util/CompressedReference.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/util/CompressedReference.java
@@ -3,76 +3,90 @@ package edu.ualberta.med.biobank.util;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 /**
- * Taken from
- * http://theplateisbad.blogspot.ca/2011/04/java-in-memory-compression.html.
+ * @author Jonathan Ferland
  * 
- * @author loyola
- * 
- * @param <T>
+ * @param <T> the compressed object type
+ * @see http://theplateisbad.blogspot.ca/2011/04/java-in-memory-compression.html
  */
-public class CompressedReference<T extends Serializable> implements
-    Serializable {
+public class CompressedReference<T extends Serializable>
+    implements Serializable {
     private static final long serialVersionUID = 7967994340450625830L;
 
-    private byte[] theCompressedReferent = null;
+    private final byte[] zip;
 
     public CompressedReference(T referent) {
         try {
-            compress(referent);
+            zip = compress(referent);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new IllegalArgumentException(e);
         }
     }
 
     public int size() {
-        return theCompressedReferent.length;
+        return zip.length;
     }
 
-    public T get() {
+    /**
+     * Will decompress the compressed byte array every time this method is
+     * called. No internal caching.
+     * 
+     * @return the decompressed object.
+     * @throws IOException if an IO problem decompressing
+     * @throws ClassNotFoundException if the resulting decompressed class
+     *             definition cannot be found
+     */
+    public T get() throws IOException, ClassNotFoundException {
+        return decompress();
+    }
+
+    @SuppressWarnings("resource")
+    private byte[] compress(T referent) throws IOException {
+        OutputStream out = null;
+
         try {
-            return decompress();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            ObjectOutputStream oos;
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            out = new GZIPOutputStream(out);
+            out = oos = new ObjectOutputStream(out);
+
+            oos.writeObject(referent);
+            oos.flush();
+
+            byte[] compressed = bos.toByteArray();
+            return compressed;
+        } finally {
+            if (out != null) {
+                out.close();
+            }
         }
-        return null;
     }
 
-    private void compress(T referent) throws IOException {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        GZIPOutputStream zos = new GZIPOutputStream(bos);
-        ObjectOutputStream ous = new ObjectOutputStream(zos);
-
-        ous.writeObject(referent);
-
-        zos.finish();
-        bos.flush();
-        theCompressedReferent = bos.toByteArray();
-        bos.close();
-    }
-
-    @SuppressWarnings("unchecked")
     private T decompress() throws IOException, ClassNotFoundException {
-        T tmpObject = null;
-        ByteArrayInputStream bis =
-            new ByteArrayInputStream(theCompressedReferent);
-        GZIPInputStream zis = new GZIPInputStream(bis);
-        ObjectInputStream ois = new ObjectInputStream(zis);
-        tmpObject = (T) ois.readObject();
+        InputStream in = null;
 
-        ois.close();
+        try {
+            ObjectInputStream ois;
 
-        return tmpObject;
+            in = new ByteArrayInputStream(zip);
+            in = new GZIPInputStream(in);
+            in = ois = new ObjectInputStream(in);
+
+            @SuppressWarnings("unchecked")
+            T decompressed = (T) ois.readObject();
+            return decompressed;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
     }
 }
