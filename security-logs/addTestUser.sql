a89fe09c6165d18a7eb3ddb42263a83eb0d489ca
Nelson Loyola <loyola@ualberta.ca>
2012-09-27 09:45:40 -0600

    fix to SQL script that adds test user

--- security-logs/addTestUser.sql
+++ security-logs/addTestUser.sql
@@ -1,9 +1,8 @@
-INSERT INTO csm_user (USER_ID, LOGIN_NAME, MIGRATED_FLAG, FIRST_NAME, LAST_NAME, PASSWORD, UPDATE_DATE)
-       select coalesce(MAX(id), 0)+Max(user_id)+1, 'testuser', 0 , 'testuser', 'testuser', 'orDBlaojDQE=', sysdate()
-       from principal, csm_user;
+INSERT INTO csm_user (LOGIN_NAME, MIGRATED_FLAG, FIRST_NAME, LAST_NAME, PASSWORD, UPDATE_DATE)
+       VALUES ('testuser', 0 , 'testuser', 'testuser', 'orDBlaojDQE=', sysdate());
 
 insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk_emails, full_name, email, need_pwd_change,activity_status_id)
-       select coalesce(MAX(id), 0)+1, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), email_id, 0, 1
+       select csm_user.user_id, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), email_id, 0, 1
        from csm_user,principal
        where login_name = 'testuser';
 
