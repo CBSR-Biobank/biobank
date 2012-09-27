f847cee8b1e95b1fa0f0fcdd17c2ba2c5734f4e9
Nelson Loyola <loyola@ualberta.ca>
2012-09-27 13:36:10 -0600

    another fix to test user add script

--- security-logs/addTestUser.sql
+++ security-logs/addTestUser.sql
@@ -1,10 +1,12 @@
 INSERT INTO csm_user (LOGIN_NAME, MIGRATED_FLAG, FIRST_NAME, LAST_NAME, PASSWORD, UPDATE_DATE)
        VALUES ('testuser', 0 , 'testuser', 'testuser', 'orDBlaojDQE=', sysdate());
 
-insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk_emails, full_name, email, need_pwd_change,activity_status_id)
+insert into principal (id, version, discriminator, login, csm_user_id, recv_bulk_emails,
+            full_name, email, need_pwd_change,activity_status_id)
        select coalesce(MAX(id), 0)+1, 0, 'User', login_name, user_id, 1, concat(first_name, ' ', last_name), email_id, 0, 1
        from csm_user,principal
-       where login_name = 'testuser';
+       where login_name = 'testuser'
+       group by principal.id;
 
 insert into csm_user_group_role_pg (user_id, role_id, protection_group_id, update_date)
        select user_id, 8, 1, sysdate()
