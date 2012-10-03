c37d7a69c081ce6bac215b29ee58cea87ad6fc43
Jonathan Ferland <jferland@ualberta.ca>
2012-10-03 11:22:06 -0600

    Specify column ordering for some entity/report related inserts

--- data/sql/upgrade_v310_v320.sql
+++ data/sql/upgrade_v310_v320.sql
@@ -1,11 +1,14 @@
 -- start bug#1405 fix
 -- add properties for columns and filters for collectionEvent-s and topSpecimen-s
 
-INSERT INTO entity_property VALUES (32, 'processingEvent.worksheet', 1, 1, 0);
+INSERT INTO entity_property (id, property, property_type_id, entity_id, version) VALUES 
+  (32, 'processingEvent.worksheet', 1, 1, 0);
 
-INSERT INTO entity_column VALUES (31, 'Processing Event Worksheet', 32, 0);
+INSERT INTO entity_column (id, name, entity_property_id, version) VALUES
+  (31, 'Processing Event Worksheet', 32, 0);
 
-INSERT INTO entity_filter VALUES (31, 1, 'Processing Event Worksheet', 32, 0);
+INSERT INTO entity_filter (id, filter_type, name, entity_property_id, version) VALUES
+  (31, 1, 'Processing Event Worksheet', 32, 0);
 
 -- end bug#1405 fix
 
