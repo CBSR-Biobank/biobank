f42c10616c5ba62bffdde8e94c8ce069cfe9570e
Jonathan Ferland <jferland@ualberta.ca>
2012-11-01 15:14:38 -0600

    Playing with Spring3/Hibernate4 Config

--- biobank-model/src/test/java/edu/ualberta/med/biobank/DbTest.java
+++ biobank-model/src/test/java/edu/ualberta/med/biobank/DbTest.java
@@ -6,97 +6,82 @@ import junit.framework.Assert;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import edu.ualberta.med.biobank.model.HibernateConfig;
 import edu.ualberta.med.biobank.model.provider.EntityProcessor;
 import edu.ualberta.med.biobank.model.provider.EntityProvider;
 import edu.ualberta.med.biobank.model.provider.Mother;
 
-public class DbTest
-    extends BaseTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = HibernateConfig.class)
+public class DbTest 
+	extends BaseTest {
+	protected SessionFactory sessionFactory;
+	private Mother mother;
+	
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
-    protected static final SessionFactory sessionFactory;
+	@Before
+	public void setUp() throws Exception {
+		mother = new Mother(getMethodNameR());
 
-    static {
-        // configure() configures settings from hibernate.cfg.xml found in the
-        // resources directory
-        Configuration configuration = new Configuration().configure();
-        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(
-            configuration.getProperties()).buildServiceRegistry();
+		// automatically save objects whenever they're created
+		mother.setEntityProcessor(new EntityProcessor<Object>() {
+			@Override
+			public void process(Object entity) {
+				Session session = sessionFactory.getCurrentSession();
+				session.save(entity);
+				// session.flush();
+			}
+		});
+	}
 
-        sessionFactory = configuration.buildSessionFactory(reg);
-    }
+	protected <T> EntityProvider<T> getProvider(Class<T> klazz) {
+		return mother.getProvider(klazz);
+	}
 
-    private Mother mother;
-    protected Session session;
+	/**
+	 * Check an assertion when a certain type of entity is updated and when it
+	 * is inserted.
+	 * 
+	 * @param klazz
+	 *            the type of entity to insert and update
+	 * @param processor
+	 *            run on the entity instance to "make it wrong"
+	 * @param assertion
+	 *            describes the expected ConstraintViolationException
+	 */
+	protected <T> void persistEntity(Class<T> klazz,
+			EntityProcessor<T> processor, ConstraintViolationAssertion assertion) {
+		T entity = getProvider(klazz).create();
+		
+		Session session = sessionFactory.getCurrentSession();
 
-    @Before
-    public void setUp() throws Exception {
-        session = sessionFactory.openSession();
-        mother = new Mother(getMethodNameR());
+		try {
+			processor.process(entity);
+			session.update(entity);
+			session.flush();
+			Assert.fail("update should not be allowed");
+		} catch (ConstraintViolationException e) {
+			assertion.withRootBean(entity).assertIn(e);
+		}
 
-        // automatically save objects whenever they're created
-        mother.setEntityProcessor(new EntityProcessor<Object>() {
-            @Override
-            public void process(Object entity) {
-                session.save(entity);
-                // session.flush();
-            }
-        });
-
-        session.beginTransaction();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        Transaction tx = session.getTransaction();
-        if (tx != null && tx.isActive()) {
-            tx.rollback();
-        }
-
-        mother = null;
-        session.close();
-    }
-
-    protected <T> EntityProvider<T> getProvider(Class<T> klazz) {
-        return mother.getProvider(klazz);
-    }
-
-    /**
-     * Check an assertion when a certain type of entity is updated and when it
-     * is inserted.
-     * 
-     * @param klazz the type of entity to insert and update
-     * @param processor run on the entity instance to "make it wrong"
-     * @param assertion describes the expected ConstraintViolationException
-     */
-    protected <T> void persistEntity(Class<T> klazz,
-        EntityProcessor<T> processor,
-        ConstraintViolationAssertion assertion) {
-        T entity = getProvider(klazz).create();
-
-        try {
-            processor.process(entity);
-            session.update(entity);
-            session.flush();
-            Assert.fail("update should not be allowed");
-        } catch (ConstraintViolationException e) {
-            assertion.withRootBean(entity).assertIn(e);
-        }
-
-        try {
-            entity = getProvider(klazz).create();
-            processor.process(entity);
-            session.save(entity);
-            session.flush();
-            Assert.fail("insert should not be allowed");
-        } catch (ConstraintViolationException e) {
-            assertion.withRootBean(entity).assertIn(e);
-        }
-    }
+		try {
+			entity = getProvider(klazz).create();
+			processor.process(entity);
+			session.save(entity);
+			session.flush();
+			Assert.fail("insert should not be allowed");
+		} catch (ConstraintViolationException e) {
+			assertion.withRootBean(entity).assertIn(e);
+		}
+	}
 }
