bd25d3ec4c1c0e3e8aca104a4812a36a3464e241
Nelson Loyola <nloyola@gmail.com>
2012-11-07 15:00:53 -0700

    1) Made changes to SpecimenReadPermission for maintainability. 2) Wrote action tests to test the above changes in TestSpecimen. 3) Commented out test code in DispatchCreateScanDialog. 4) PalletScanManagement.launchScanAndProcessResult() now handles AccessDeniedException. 5) PalletWell.merge() now handles AccessDeniedException and displays the first specimen inventory ID that user does not have access to read.

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/permission/specimen/SpecimenReadPermission.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/common/permission/specimen/SpecimenReadPermission.java
@@ -11,39 +11,41 @@ import edu.ualberta.med.biobank.model.Study;
 
 public class SpecimenReadPermission implements Permission {
     private static final long serialVersionUID = 1L;
+    
+    private static final String HQL_QRY = 
+        "FROM "+ Specimen.class.getName() + " spec " 
+            + "INNER JOIN FETCH spec.currentCenter " 
+            + "INNER JOIN FETCH spec.collectionEvent ce " 
+            + "INNER JOIN FETCH ce.patient p " 
+            + "INNER JOIN FETCH p.study " 
+            + "WHERE spec.inventoryId=?";
 
-    private Integer specimenId = null;
-    private String inventoryId = null;
+    private final Integer specimenId;
+    private final String inventoryId;
 
     public SpecimenReadPermission(Integer specimenId) {
         this.specimenId = specimenId;
+        this.inventoryId = null;
     }
 
     public SpecimenReadPermission(String inventoryId) {
         this.inventoryId = inventoryId;
+        this.specimenId = null;
     }
 
     @Override
     public boolean isAllowed(ActionContext context) {
         Specimen specimen = null;
-        if (specimenId != null)
+        if (specimenId != null) {
             specimen = context.get(Specimen.class, specimenId);
-        else {
-            Query q =
-                context
-                    .getSession()
-                    .createQuery(
-                        "from " //$NON-NLS-1$
-                            + Specimen.class.getName()
-                            + " spec inner join fetch spec.currentCenter inner join fetch " //$NON-NLS-1$
-                            + "spec.collectionEvent ce inner join fetch" //$NON-NLS-1$
-                            + " ce.patient p inner join fetch p.study where spec.inventoryId=?"); //$NON-NLS-1$
+        } else {
+            Query q = context.getSession().createQuery(HQL_QRY);
             q.setParameter(0, inventoryId);
-            if (q.list().size() > 0)
-                specimen = (Specimen) q.list().get(0);
+            specimen = (Specimen) q.uniqueResult();
         }
 
         if (specimen == null) return true;
+        
         Center center = specimen.getCurrentCenter();
         Study study = specimen.getCollectionEvent().getPatient().getStudy();
 
--- eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/action/TestSpecimen.java
+++ eclipse_ws/biobank2.tests/src/edu/ualberta/med/biobank/test/action/TestSpecimen.java
@@ -1,7 +1,9 @@
 package edu.ualberta.med.biobank.test.action;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import junit.framework.Assert;
 
@@ -12,10 +14,13 @@ import edu.ualberta.med.biobank.common.action.collectionEvent.CollectionEventGet
 import edu.ualberta.med.biobank.common.action.collectionEvent.CollectionEventGetInfoAction.CEventInfo;
 import edu.ualberta.med.biobank.common.action.collectionEvent.CollectionEventSaveAction;
 import edu.ualberta.med.biobank.common.action.collectionEvent.CollectionEventSaveAction.SaveCEventSpecimenInfo;
+import edu.ualberta.med.biobank.common.action.search.SpecimenByInventorySearchAction;
 import edu.ualberta.med.biobank.common.action.specimen.SpecimenGetInfoAction;
 import edu.ualberta.med.biobank.common.action.specimen.SpecimenGetInfoAction.SpecimenBriefInfo;
 import edu.ualberta.med.biobank.common.action.specimen.SpecimenInfo;
 import edu.ualberta.med.biobank.model.ActivityStatus;
+import edu.ualberta.med.biobank.model.Site;
+import edu.ualberta.med.biobank.model.Specimen;
 import edu.ualberta.med.biobank.test.action.helper.CollectionEventHelper;
 import edu.ualberta.med.biobank.test.action.helper.SiteHelper.Provisioning;
 
@@ -46,11 +51,9 @@ public class TestSpecimen extends TestAction {
 
         // Save a new cevent
         final Integer ceventId = exec(
-            new CollectionEventSaveAction(null, provisioning.patientIds
-                .get(0),
+            new CollectionEventSaveAction(null, provisioning.patientIds.get(0),
                 getR().nextInt(20) + 1, ActivityStatus.ACTIVE, null,
-                new ArrayList<SaveCEventSpecimenInfo>(specs.values()),
-                null))
+                new ArrayList<SaveCEventSpecimenInfo>(specs.values()), null))
             .getId();
 
         CEventInfo ceventInfo =
@@ -64,5 +67,30 @@ public class TestSpecimen extends TestAction {
 
         Assert.assertEquals(specimenInfo.specimen,
             specimenBriefInfo.getSpecimen());
+    }
+
+    // search for an invalid specimenId
+    @Test
+    public void searchByInventoryIdActionBadId() {
+        String badInventoryId = new UUID(128, 256).toString();
+        
+        final List<Integer> actionResult = exec(
+            new SpecimenByInventorySearchAction(badInventoryId, provisioning.siteId)).getList();
+        Assert.assertEquals(0, actionResult.size());
+    }
+
+    @Test
+    public void searchByInventoryIdAction() {
+        session.beginTransaction();
+        factory.createClinic();
+        Site site = factory.createSite();
+        factory.createStudy();
+        Specimen spc = factory.createParentSpecimen();
+        session.getTransaction().commit();
+        
+        final List<Integer> actionResult = exec(new SpecimenByInventorySearchAction(
+            spc.getInventoryId(), site.getId())).getList();
+        Assert.assertEquals(1, actionResult.size());
+        Assert.assertEquals(spc.getId(), actionResult.get(0));
     }
 }
