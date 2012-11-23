20c0ec62a14a420f7da60a83d958b9ea15cb9967
Nelson Loyola <loyola@cs.ualberta.ca>
2012-11-22 17:57:47 -0700

    working on flyway java migrations

---
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/migration/V1_2__another_test.java
@@ -0,0 +1,19 @@
+package edu.ualberta.med.biobank.migration;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.googlecode.flyway.core.api.migration.spring.SpringJdbcMigration;
+
+@SuppressWarnings("nls")
+public class V1_2__another_test implements SpringJdbcMigration {
+
+    @Override
+    public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
+        //jdbcTemplate.execute("ALTER TABLE origin_info DROP FOREIGN KEY FKE92E7A275598FA35");
+        //jdbcTemplate.execute("ALTER TABLE origin_info DROP INDEX FKE92E7A275598FA35");
+        //jdbcTemplate.execute("ALTER TABLE origin_info ADD INDEX FKE92E7A274D7A8883 (RECEIVER_SITE_ID)");
+        //jdbcTemplate.execute("ALTER TABLE origin_info ADD CONSTRAINT FKE92E7A274D7A8883 FOREIGN KEY FKE92E7A274D7A8883 (RECEIVER_SITE_ID) REFERENCES center (ID) ON UPDATE NO ACTION ON DELETE NO ACTION");
+        jdbcTemplate.execute("INSERT INTO contact  ");
+    }
+
+}
