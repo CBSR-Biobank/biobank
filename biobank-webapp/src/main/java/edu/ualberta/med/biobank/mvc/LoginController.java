dc555167b1b03bfe796babf9da19052a180ecd1e
Nelson Loyola <loyola@ualberta.ca>
2012-12-14 15:44:17 -0700

    working on spring security with openid

--- biobank-webapp/src/main/java/edu/ualberta/med/biobank/mvc/LoginController.java
+++ biobank-webapp/src/main/java/edu/ualberta/med/biobank/mvc/LoginController.java
@@ -1,7 +1,7 @@
 package edu.ualberta.med.biobank.mvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,12 +10,19 @@ public class LoginController {
 
     @RequestMapping(value="/login", method = RequestMethod.GET)
     public String handleRequest(){
-        return "login"; //$NON-NLS-1$
+        return "openidlogin.html"; //$NON-NLS-1$
+    }
+
+    /** Login form with error. */
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true); //$NON-NLS-1$
+        return "openidlogin.html"; //$NON-NLS-1$
     }
 
     @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logout(ModelMap model) {
-        return "login";
+    public String logout() {
+        return "openidlogin.html"; //$NON-NLS-1$
 
     }
 
---
+++ biobank-webapp/src/main/java/edu/ualberta/med/biobank/openid/CustomUserDetails.java
@@ -0,0 +1,43 @@
+package edu.ualberta.med.biobank.openid;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class CustomUserDetails extends User {
+    private static final long serialVersionUID = 1L;
+
+    private String email;
+    private String name;
+    private boolean newUser;
+
+    public CustomUserDetails(String username, Collection<GrantedAuthority> authorities) {
+        super(username, "unused", authorities);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isNewUser() {
+        return newUser;
+    }
+
+    public void setNewUser(boolean newUser) {
+        this.newUser = newUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
