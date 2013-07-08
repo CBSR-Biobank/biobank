351324a07616b4481626763e506c325a632ffe08
Nelson Loyola <loyola@ualberta.ca>
2013-07-08 14:53:02 -0600

    fixed bug in schema upgrade script

--- eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/migration/V1_3__Biobank_v370.java
+++ eclipse_ws/biobank.common/src/edu/ualberta/med/biobank/migration/V1_3__Biobank_v370.java
@@ -14,9 +14,10 @@ public class V1_3__Biobank_v370 implements SpringJdbcMigration {
         jdbcTemplate.execute("CREATE TABLE `batch_operation_event_attr` ("
             + " `EVENT_ATTR_ID` int(11) NOT NULL,"
             + " `BATCH_OPERATION_ID` int(11) NOT NULL,"
-            + " PRIMARY KEY (PROCESSING_EVENT_ID, BATCH_OPERATION_ID)"
-            + "  KEY `FKF1184A93D3BA0590` (`BATCH_OPERATION_ID`),"
-            + "  CONSTRAINT `FKF1184A93D3BA0590` FOREIGN KEY (`BATCH_OPERATION_ID`) REFERENCES `batch_operation` (`ID`)"
+            + " PRIMARY KEY (EVENT_ATTR_ID, BATCH_OPERATION_ID),"
+            + " KEY `FKF1184A93D3BA0590` (`BATCH_OPERATION_ID`),"
+            + " CONSTRAINT `FKF1184A93D3BA0590` FOREIGN KEY (`BATCH_OPERATION_ID`) "
+            + " REFERENCES `batch_operation` (`ID`)"
             + ") ENGINE=InnoDB");
     }
 }
