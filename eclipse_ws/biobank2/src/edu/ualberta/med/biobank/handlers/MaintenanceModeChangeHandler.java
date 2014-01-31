7f3327e9ae08392710d7456b80d10684a44cee25
Nelson Loyola <nloyola@gmail.com>
2014-01-31 16:07:50 -0700

    Working on #1841: Allow server to be placed in maintenance mode
    Disabled functionality on client to modify the server's maintenance mode.

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/handlers/MaintenanceModeChangeHandler.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/handlers/MaintenanceModeChangeHandler.java
@@ -3,51 +3,46 @@ package edu.ualberta.med.biobank.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.PlatformUI;
-import org.xnap.commons.i18n.I18n;
-import org.xnap.commons.i18n.I18nFactory;
-
-import edu.ualberta.med.biobank.SessionManager;
-import edu.ualberta.med.biobank.gui.common.BgcPlugin;
-import edu.ualberta.med.biobank.server.applicationservice.BiobankApplicationService.MaintenanceMode;
-import gov.nih.nci.system.applicationservice.ApplicationException;
 
 public class MaintenanceModeChangeHandler extends AbstractHandler {
 
-    private static final I18n i18n = I18nFactory.getI18n(DecodeImageHandler.class);
+    // private static final I18n i18n = I18nFactory.getI18n(DecodeImageHandler.class);
 
-    @SuppressWarnings("nls")
-    private static final String TITLE = i18n.tr("Server Maintenance Mode");
+    // @SuppressWarnings("nls")
+    // private static final String TITLE = i18n.tr("Server Maintenance Mode");
 
-    @SuppressWarnings("nls")
+    // @SuppressWarnings("nls")
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        final MaintenanceMode serverMaintenanceMode = SessionManager.getAppService().maintenanceMode();
+        // comment this out for now, maintenance mode on the server can be changed with an ANT
+        // command
 
-        String message;
-        MaintenanceMode newMode;
-
-        if (serverMaintenanceMode == MaintenanceMode.PREVENT_USER_LOGIN) {
-            message = i18n.tr("Maintenance mode is enabled. Do you want to disable it?");
-            newMode = MaintenanceMode.NONE;
-        } else {
-            message = i18n.tr("Maintenance mode is disabled. Do you want to enable it?");
-            newMode = MaintenanceMode.PREVENT_USER_LOGIN;
-        }
-
-        boolean confirm = MessageDialog.openConfirm(
-            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-            TITLE,
-            message);
-
-        if (confirm) {
-            try {
-                SessionManager.getAppService().maintenanceMode(newMode);
-            } catch (ApplicationException e) {
-                BgcPlugin.openAccessDeniedErrorMessage(e);
-            }
-        }
+        // final MaintenanceMode serverMaintenanceMode =
+        // SessionManager.getAppService().maintenanceMode();
+        //
+        // String message;
+        // MaintenanceMode newMode;
+        //
+        // if (serverMaintenanceMode == MaintenanceMode.PREVENT_USER_LOGIN) {
+        // message = i18n.tr("Maintenance mode is enabled. Do you want to disable it?");
+        // newMode = MaintenanceMode.NONE;
+        // } else {
+        // message = i18n.tr("Maintenance mode is disabled. Do you want to enable it?");
+        // newMode = MaintenanceMode.PREVENT_USER_LOGIN;
+        // }
+        //
+        // boolean confirm = MessageDialog.openConfirm(
+        // PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+        // TITLE,
+        // message);
+        //
+        // if (confirm) {
+        // try {
+        // SessionManager.getAppService().maintenanceMode(newMode);
+        // } catch (ApplicationException e) {
+        // BgcPlugin.openAccessDeniedErrorMessage(e);
+        // }
+        // }
         return null;
     }
 
