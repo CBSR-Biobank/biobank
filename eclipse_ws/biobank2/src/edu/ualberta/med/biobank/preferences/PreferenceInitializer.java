fc788d48a2b0d344ade9d0ab527d0c5b987d3d29
Nelson Loyola <nloyola@gmail.com>
2016-09-15 09:55:39 +0200

    Added new servers to PreferenceInitializer
    Added the CCNA and CNTRP servers to the SERVER_LIST. These will now be
    shown in the Login dialog box.

--- eclipse_ws/biobank2/src/edu/ualberta/med/biobank/preferences/PreferenceInitializer.java
+++ eclipse_ws/biobank2/src/edu/ualberta/med/biobank/preferences/PreferenceInitializer.java
@@ -13,16 +13,15 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
     @Override
     public void initializeDefaultPreferences() {
-        IPreferenceStore store = BiobankPlugin.getDefault() .getPreferenceStore();
+        IPreferenceStore store = BiobankPlugin.getDefault().getPreferenceStore();
         store.setDefault(PreferenceConstants.GENERAL_CONFIRM, "CONFIRM"); //$NON-NLS-1$
         store.setDefault(PreferenceConstants.GENERAL_CANCEL, "CANCEL"); //$NON-NLS-1$
         store.setDefault(PreferenceConstants.LINK_ASSIGN_ACTIVITY_LOG_INTO_FILE, true);
 
         store.setDefault(PreferenceConstants.LINK_ASSIGN_ACTIVITY_LOG_PATH, StringUtil.EMPTY_STRING);
-        store.setDefault( PreferenceConstants.LINK_ASSIGN_ACTIVITY_LOG_ASK_PRINT, true);
+        store.setDefault(PreferenceConstants.LINK_ASSIGN_ACTIVITY_LOG_ASK_PRINT, true);
         store.setDefault(PreferenceConstants.SCANNER_DPI, 300);
 
-        store.setDefault(PreferenceConstants.SERVER_LIST,
-            "biobank.cbsr.ualberta.ca\ncbsr-training.med.ualberta.ca\naicml-med.cs.ualberta.ca"); //$NON-NLS-1$
+        store.setDefault(PreferenceConstants.SERVER_LIST, "biobank.cbsr.ualberta.ca\nccnabiobank.cbsr.ualberta.ca\ncntrpbiobank.cbsr.ualberta.ca\ncbsr-training.med.ualberta.ca\naicml-med.cs.ualberta.ca"); //$NON-NLS-1$
     }
 }
