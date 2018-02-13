40c6ca6e88fc7bb1d9bf4db98533d1eb21687553
Nelson Loyola <nloyola@gmail.com>
2018-02-13 10:11:36 -0700

    Added discarded specimens at LTRI

--- scripts/ltriSpecimenPositions.php
+++ scripts/ltriSpecimenPositions.php
@@ -14,6 +14,61 @@ if (count($argv) < 2) {
 $csvname = $argv[1];
 $outdir = $argv[2] ?? ".";
 
+$discardedSpecimens = [
+  "0116251646",
+  "0116251645",
+  "0116251644",
+  "0188050920",
+  "0188050921",
+  "0188050922",
+  "0188050923",
+  "0188050924",
+  "0188050925",
+  "0188036870",
+  "0188036869",
+  "0188036868",
+  "0116251649",
+  "0116251648",
+  "0116251647",
+  "0188125320",
+  "0188125321",
+  "0188125322",
+  "0188125323",
+  "0188125324",
+  "0188125325",
+  "0188049734",
+  "0188049733",
+  "0188049732",
+  "G200361757",
+  "G200865391",
+  "G200865353",
+  "G200361753",
+  "G200891229",
+  "G200865162",
+  "G200886151",
+  "OBSMS0000048LEU",
+  "OBSMS0001841LEU",
+  "OBSMS0001849LEU",
+  "OBSMS0002162LEU",
+  "G200886101",
+  "G200361762",
+  "G200865333",
+  "G200865349",
+  "G200006464",
+  "G200006453",
+  "G200002989",
+  "G200002939",
+  "G200002709",
+  "G200002735",
+  "G200002837",
+  "G200002560",
+  "G200882903",
+  "G200882913",
+  "G200850018",
+  "G200850016",
+  "G200886127"
+];
+
 function getNewOutputfile($outdir, $count) {
   $date = date("Y-m-d");
   $outfilename = $outdir . "/ltri-specimen-positions-{$date}_" . sprintf("%02d", $count) . ".csv";
@@ -27,40 +82,49 @@ function getNewOutputfile($outdir, $count) {
 }
 
 function convertAndGenerate($csvname, $outdir) {
-    $handle = fopen($csvname, "r");
-    if ($handle === FALSE) {
-      echo "cannot open CSV file {$csvname}";
-      return;
-    }
+  global $discardedSpecimens;
 
-    $header = [
-      "inventoryId",
-      "currentPalletLabel",
-      "palletProductBarcode",
-      "rootContainerType",
-      "palletLabel",
-      "palletPosition",
-      "comment"
-    ];
+  $handle = fopen($csvname, "r");
+  if ($handle === FALSE) {
+    echo "cannot open CSV file {$csvname}";
+    return;
+  }
 
-    $rowCount = 0;
-    $outfilecount = 1;
-    $outhandle = getNewOutputfile($outdir, $outfilecount);
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-      if ($rowCount === 0) {
-        fputcsv($outhandle, $header);
-        $rowCount++;
-        continue;
-      }
-      fputcsv($outhandle, [ $data[3], "", $data[6], "", "", $data[0], "" ], ",", "\"");
+  $header = [
+    "inventoryId",
+    "currentPalletLabel",
+    "palletProductBarcode",
+    "rootContainerType",
+    "palletLabel",
+    "palletPosition",
+    "comment"
+  ];
+
+  $rowCount = 0;
+  $outfilecount = 1;
+  $outhandle = getNewOutputfile($outdir, $outfilecount);
+  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    if ($rowCount === 0) {
+      fputcsv($outhandle, $header);
       $rowCount++;
-      if (($rowCount % 1000) === 0) {
-        $outfilecount++;
-        $outhandle = getNewOutputfile($outdir, $outfilecount);
-        fputcsv($outhandle, $header);
-      }
+      continue;
     }
-    fclose($handle);
+
+    $inventoryId = $data[3];
+    if (in_array($inventoryId, $discardedSpecimens)) {
+      echo "ignoring discarded specimen {$inventoryId}\n";
+      continue;
+    }
+
+    fputcsv($outhandle, [ $inventoryId, "", $data[6], "", "", $data[0], "" ], ",", "\"");
+    $rowCount++;
+    if (($rowCount % 1000) === 0) {
+      $outfilecount++;
+      $outhandle = getNewOutputfile($outdir, $outfilecount);
+      fputcsv($outhandle, $header);
+    }
+  }
+  fclose($handle);
 }
 
 convertAndGenerate($csvname, $outdir);
